c6e2c0a389315f9463ba18122cf79b1bebe4aaab
EduardoGB <e.gbendito@gmail.com>
2021-09-17 15:00:46 +0000

    start and correct some datasets

--- scripts/fertilizer/doi_10.7910_DVN_LJPW4O.R
+++ scripts/fertilizer/doi_10.7910_DVN_LJPW4O.R
@@ -29,7 +29,7 @@ carob_script <- function(path) {
 
 ## download and read data 
 
-	ff  <- carobiner::get_data(uri, path, group) # WHICH VERSION OF THE DATSET IS DOWNLOADED???
+	ff  <- carobiner::get_data(uri, path, group)
 	js <- carobiner::get_metadata(dataset_id, path, group, major=1, minor=5) 
 	dset$license <- carobiner::get_license(js)
 
@@ -38,41 +38,34 @@ carob_script <- function(path) {
 
 	d <- as.data.frame(readxl::read_excel(f))
 	d$country <- "Uganda"
-	d$region <- ""
 	d$adm1 <- "Wakiso"
 	d$adm2 <- "Jinja"
 	d$adm3 <- ifelse(d$Site == "Kawanda", "Nabweru", "Busukuma")
 	d$trial_id <- paste0(dataset_id, '-', d$Site)
 	d$latitude <- ifelse(d$Site == "Kawanda", 0.4172778, 0.5256090)
 	d$longitude <- ifelse(d$Site == "Kawanda", 32.5355326, 32.6136960)
-	d$start_date <- as.numeric("2013") # Correct date format (to date)
-	d$end_date <- as.numeric("2014")  # Correct date format (to date)
+	d$start_date <- as.numeric("2013")
+	d$end_date <- as.numeric("2014")
 	d$on_farm <- "no"
 	d$is_survey <- "no"
 	d$crop <- "maize"
+	# d$treatment <- "Soil nutrient available"
 	
 	# Merge with measured biomass ("2a Dry matter measurements.xlsx")
 	biomass <- as.data.frame(readxl::read_excel(ff[basename(ff) == "2a Dry matter measurements.xlsx"]))
-
-### RH: season "1" and "2" is not informative 
-###  
 	biomass$Season <- ifelse(biomass$`Days after planting (dap)` == 30, 1, 2)
-  # biomass <- biomass[order(biomass[,"Site"], biomass[,"Season"], biomass[,"Block"], biomass[,"Treatment"], biomass[,"Plot"]), ]
+	biomass$season <- ifelse(biomass$`Days after planting (dap)` == 30, "dry", "rainy")
   biomass <- biomass[order(biomass[,"Site"], biomass[,"Season"], biomass[,"Block"], biomass[,"Treatment"]), ]
-  # biomass1 <- aggregate(biomass$`Dry weight with roots (g)`,by=list(biomass$Site,biomass$Season,biomass$Block,biomass$Treatment,biomass$Plot),data=biomass,FUN=mean)
-
-## RH instead of
-#  biomass1 <- aggregate(biomass$`Dry weight with roots (g)`,by=list(biomass$Site,biomass$Season,biomass$Block,biomass$Treatment),data=biomass,FUN=mean)
-#	colnames(biomass1) <- colnames(biomass)[c(1,8,3,4,7)]
-## do this: 
 	biomass1 <- aggregate(biomass[, "Dry weight with roots (g)", drop=FALSE], 
-						  biomass[, c("Site", "Season", "Block", "Treatment")], FUN=mean)
+						  biomass[, c("Site", "Season", "season", "Block", "Treatment")], FUN=mean)
 	d1 <- merge(d, biomass1, by = intersect(names(d), names(biomass1)), all.x = TRUE)
-	d1$grain_weight <- d$`Grain yield (kg/plot -5.625m2)`*1000
+	# Biomass is not making too much sense compared to yield
+	d1$biomass_total <- ((d1$`Dry weight with roots (g)` * 8) * (100/5.625))/1000 # 5a Plant growth data.xlsx --> (2 rows/plot * 20 plants/row) / 5 plants sampled
 	d1$Plot <- biomass$Plot
 	
 	###########
 	# Missing information on amount of manure applied.
+	# Have sent an email to Job Kihara to find out about the manure applied (2021/09/17)
 	###########
 	# # Merge with Manure applied ("1a Cattle manure lab analysis.xlsx")
 	# OM <- as.data.frame(readxl::read_excel(ff[basename(ff) == "1a Cattle manure lab analysis.xlsx"], skip = 5))
@@ -98,20 +91,15 @@ carob_script <- function(path) {
 	soil2 <- aggregate(soil1[, c(3,5,6,9,10,11)], list(Site = soil1$Site, Block = soil1$Block, Plot = soil1$Plot), mean, na.rm = TRUE)
 	soil2 <- soil2[order(soil2[,"Site"], soil2[,"Block"], soil2[,"Plot"]), ]
 	d2 <- merge(d1, soil2, by = intersect(names(d1), names(soil2)), all.x = TRUE)
-
-## RH: yield is the grain yield (for cereals)
-## 	d2$yield <- (d2$`Grain yield (kg/plot -5.625m2)` + d2$`Stover yield (kg/plot - 5.625m2)`)* (100/5.625)
-	
-## RH
 	d2$yield <- d2$`Grain yield (kg/plot -5.625m2)` * (100/5.625)
 	d2$residue_yield <- d2$`Stover yield (kg/plot - 5.625m2)` * (100/5.625)
 
 	
 	# process file(s)
 	d <- carobiner::change_names(d2,
-	                             c("Site","Dry weight with roots (g)", "pH", "N (%)", "K (ppm)", "P (ppm)", "Sand (%)", "Clay (%)"),
-	                             c("site","biomass_total", "soil_pH", "soil_N", "soil_K", "soil_P_total", "soil_sand", "soil_clay"))
-	d <- d[,c(1,8,9,11:29)]
+	                             c("country", "adm1", "adm2", "adm3", "latitude", "longitude", "Site", "start_date", "end_date", "season", "on_farm", "is_survey", "crop", "biomass_total", "yield", "residue_yield", "pH", "N (%)", "K (ppm)", "P (ppm)", "Sand (%)", "Clay (%)"),
+	                             c("country", "adm1", "adm2", "adm3", "latitude", "longitude", "site", "start_date", "end_date", "season", "on_farm", "is_survey", "crop", "biomass_total", "yield", "residue_yield", "soil_pH", "soil_N", "soil_K", "soil_P_total", "soil_sand", "soil_clay"))
+	d <- d[,c("country", "adm1", "adm2", "adm3", "latitude", "longitude", "site", "start_date", "end_date", "season", "on_farm", "is_survey", "crop", "biomass_total", "yield", "residue_yield", "soil_pH", "soil_N", "soil_K", "soil_P_total", "soil_sand", "soil_clay")]
 	d$trial_id <- paste0(d$trial_id, "-", ifelse(d$site == "Kawanda", seq(1,length(which(d$site == "Kawanda"))), seq(1,length(which(d$site == "Namulonge")))))
 	d$dataset_id <- dataset_id
 
