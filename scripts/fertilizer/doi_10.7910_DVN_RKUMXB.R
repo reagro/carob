de48c9e052835a8f108f0fe3f614bce1542a1bc7
EduardoGB <e.gbendito@gmail.com>
2022-04-24 19:43:40 +0000

    For fertilizer/doi_10.7910_DVN_UNLRGC.R and fertilizer/doi_10.7910_DVN_RKUMXB.R Changes in the license: No license specified in metadata. Only Terms of use available. It is registered as "CC BY 4.0"

--- scripts/fertilizer/doi_10.7910_DVN_RKUMXB.R
+++ scripts/fertilizer/doi_10.7910_DVN_RKUMXB.R
@@ -53,7 +53,9 @@ carob_script <- function(path) {
 	f <- ff[basename(ff) == "02. ET_data_June2017.csv"]
 	## read the json for version, license, terms of use  
 	js <- carobiner::get_metadata(dataset_id, path, group, major=2, minor=1)
-	dset$license <- carobiner::get_license(js)
+	# No License specified in metadata. Only Terms of use available. It is registered as "CC BY 4.0"
+	# dset$license <- carobiner::get_license(js)
+	dset$license <- "CC BY 4.0"
 
 ## Select and fix column names
 	ft <- c("DATASOURCE", "reference", "SITE", "location", "ADMIN_REGION", "adm1", "CODE", "trial_id", "CodeSE", "drop", "X", "longitude", "Y", "latitude", "CoordType", "drop", "CROPTYPE", "crop", "VARIETY", "variety", "VARIETYTYPE", "variety_type", "TRIALTYPE", "trial_type", "SOILTYPE", "soil_type", "Sand", "soil_sand", "Clay", "soil_clay", "SOC", "soil_SOC", "pH", "soil_pH", "Avail_P", "soil_P_available", "CroppingSystem", "crop_system", "Organicresource", "OM_used", "Inoculation", "innoculated", "OrgR_type", "OM_type", "OrgR_Amount", "OM_applied", "OrganicN", "OM_N", "OrganicK", "OM_K", "OrganicP", "OM_P", "Prev_crop", "previous_crop", "YEAR", "year", "Season", "season", "Response", "response", "N", "N", "N_Timing", "drop", "N_splits", "N_splits", "P", "P", "P_Appl", "drop", "P_Source", "fertilizer_type_1", "K", "K", "Other_Nutrient", "Other_Nutrient", "NutrientSource", "fertilizer_type_2", "Nutrientamount", "Nutrientamount", "AvailableSoilNutrient_OtherthanNPK", "drop", "TrtDesc", "drop", "Treatment_yld", "yield", "Control_Yld", "Control_Yld", "Absolute_Ctrl_Yld", "Absolute_Ctrl_Yld", "Error", "uncertainty", "ErrorType", "uncertainty_type", "Replications", "drop", "Treatments", "drop", "SDEV", "drop", "Application_ForOtherNutrients", "drop", "Rainfall", "drop", "WateringRegime", "irrigated", "Tillage", "tillage", "COMMENTS", "comments", "RR", "drop")
