c693cbc3aa82f7b42ec0433a7bca96287d7b99b6
mukami3juma <rachelmukami18@gmail.com>
2022-09-07 02:58:24 +0300

    adding fertilizer amounts to scripts/variety_performance/doi_10.25502_X2H1-AT51_D.R

--- scripts/variety_performance/doi_10.25502_X2H1-AT51_D.R
+++ scripts/variety_performance/doi_10.25502_X2H1-AT51_D.R
@@ -37,12 +37,12 @@ Malawi, Rwanda, Mozambique, Kenya & Zimbabwe) as tier one countries.
   ff <- carobiner::get_data(uri, path, group)
   js <- carobiner::get_metadata(dataset_id, path, group, major=1, minor=0)
   dset$license <- carobiner::get_license(js)
-  
+
   # processing activities.csv
   f <- ff[basename(ff) == "activities.csv"]
   d <- data.frame(read.csv2(f, sep = "," ))
   d$start_date <- as.Date(d$planting,"%m/%d/%Y")
-  d <- d[,c("trial_id","start_date")]
+  d<- d[,c("trial_id","start_date")]
   
   # processing crop_observations.csv
   f1 <- ff[basename(ff) == "crop_observations.csv"]
@@ -56,71 +56,70 @@ Malawi, Rwanda, Mozambique, Kenya & Zimbabwe) as tier one countries.
   d1$biomass_total <- d1$calc_weight_a_ground_biomass_kg
   d1$grain_weight <- d1$dry_weight_100_seed_g * 10
   d1 <- d1[,c("trial_id","rep","treatment","variety","start_date","end_date","yield","residue_yield","biomass_total","grain_weight")]
-  
-  # merging activities.csv,field_history.csv and general.csv into d5 due to common trial_id and dropping irrelevant variables
+
+  # processing field_history.csv
   f2 <- ff[basename(ff) == "field_history.csv"]
-  d2 <- data.frame(read.csv2(f2, sep = "," ))
+  d2 <- data.frame(read.csv(f2))
+  d2 <- d2[3]
+  
+  # processing general.csv
   f3 <- ff[basename(ff) == "general.csv"]
   d3 <- data.frame(read.csv2(f3, sep = "," ))
-  d4 <- merge(d,d2, by = c("trial_id"), all = TRUE)
-  d5 <- merge(d4,d3,by = c("trial_id","instanceid"), all = TRUE)
-  drop <- c("SN.x","SN.y","id.x","id.y","instanceid")
-  d5 <- d5[ , !(names(d5) %in% drop)]
-  d5$adm1 <- d5$district_county
-  d5$adm2 <- d5$village
-  d5$location <- d5$site
-  d5$crop <- d5$type_of_experiment
-  d5$crop <- ifelse(d5$crop == "commonbean_babytrial"|d5$crop == "common bean_input"|d5$crop == "commonbean_input"|d5$crop == "Commonbean_input"
-                    |d5$crop == "common bean_var"|d5$crop == "Commonbean_variety"|d5$crop == "commonbean_variety","common bean",
-                    ifelse(d5$crop == "Chickpea_input"|d5$crop == "Chickpea_variety","chickpea",
-                           ifelse(d5$crop == "Fababean_input"|d5$crop == "Fababean_variety","faba bean","soybean")))
-  d5 <- d5[,c("trial_id","country","adm1","adm2","location","crop","start_date")]
+  d3$adm1 <- d3$district_county
+  d3$adm2 <- d3$village
+  d3$location <- d3$site
+  d3$crop <- d3$type_of_experiment
+  d3$crop <- ifelse(d3$crop == "commonbean_babytrial"|d3$crop == "common bean_input"|d3$crop == "commonbean_input"|d3$crop == "Commonbean_input"
+                    |d3$crop == "common bean_var"|d3$crop == "Commonbean_variety"|d3$crop == "commonbean_variety","common bean",
+                    ifelse(d3$crop == "Chickpea_input"|d3$crop == "Chickpea_variety","chickpea",
+                           ifelse(d3$crop == "Fababean_input"|d3$crop == "Fababean_variety","faba bean","soybean")))
+  d3 <- d3[,c("trial_id","country","adm1","adm2","location","crop")]
   
   # processing nutr_deficiency_pest_disease.csv
-  f6 <- ff[basename(ff) == "nutr_deficiency_pest_disease.csv"]
-  d6 <- data.frame(read.csv2(f6, sep = "," ))
-  d6$rep <- d6$replication_no
-  d6 <- d6[,c("trial_id","rep")]
+  f4 <- ff[basename(ff) == "nutr_deficiency_pest_disease.csv"]
+  d4 <- data.frame(read.csv2(f4, sep = "," ))
+  d4$rep <- d4$replication_no
+  d4 <- d4[,c("trial_id","rep")]
   
   # processing pesticide_biocide_use.csv
-  f7 <- ff[basename(ff) == "pesticide_biocide_use.csv"]
-  d7 <- data.frame(read.csv2(f7, sep = "," ))
-  d7 <- d7[3]
+  f5 <- ff[basename(ff) == "pesticide_biocide_use.csv"]
+  d5 <- data.frame(read.csv2(f5, sep = "," ))
+  d5<- d5[3]
   
   # processing rainfall.csv
-  f8 <- ff[basename(ff) == "rainfall.csv"]
-  d8 <- data.frame(read.csv2(f8, sep = "," ))
-  d8$rain <- d8$rain_mm
-  d8 <- d8[,c("trial_id","rain")]
+  f6 <- ff[basename(ff) == "rainfall.csv"]
+  d6 <- data.frame(read.csv2(f6, sep = "," ))
+  d6$rain <- d6$rain_mm
+  d6 <- d6[,c("trial_id","rain")]
   
   # processing soil_data.csv
-  f9 <- ff[basename(ff) == "soil_data.csv"]
-  d9 <- data.frame(read.csv2(f9, sep = "," ))
-  d9$trial_id <- d9$farm_id
-  d9$soil_pH <- d9$ph
-  d9$soil_SOC <- d9$tc_perc
-  d9$soil_N <- d9$n_perc
-  d9$soil_sand <- d9$sand_perc
-  d9$soil_clay <- d9$clay_perc
-  d9 <- d9[,c("trial_id","soil_pH","soil_SOC","soil_N","soil_sand","soil_clay")]
-  
-  # compiling into a single final dataset
-  z <- carobiner::bindr(d1,d5,d6,d7,d8,d9)
-  z
-  colnames(z)
-  z$country <- replace(z$country,1:nrow(z),"Ethiopia")
-  unique(z$country)
-  z$latitude <- 9.14500
-  z$longitude <- 40.48967
-  z$on_farm <- "yes"
-  z$dataset_id <- dataset_id
-  z$crop[is.na(z$crop)] <- "common bean" # assuming all null values in crops are common beans
- 
-  
-  z <- z[,c("dataset_id","trial_id","on_farm","country","adm1","adm2","location","crop","variety","start_date",
-  "end_date","rep","treatment","rain","soil_pH","soil_SOC","soil_N","soil_sand","soil_clay","grain_weight",
-  "biomass_total","yield","residue_yield","latitude","longitude")]
+  f7 <- ff[basename(ff) == "soil_data.csv"]
+  d7<- data.frame(read.csv2(f7, sep = "," ))
+  d7$trial_id <- d7$farm_id
+  d7$soil_pH <- d7$ph
+  d7$soil_SOC <- d7$tc_perc
+  d7$soil_N <- d7$n_perc
+  d7$soil_sand <- d7$sand_perc
+  d7$soil_clay <- d7$clay_perc
+  d7 <- d7[,c("trial_id","soil_pH","soil_SOC","soil_N","soil_sand","soil_clay")]
 
-  carobiner::write_files(dset, z, path, dataset_id, group)
+  # compiling into a single final dataset
+  f <- carobiner::bindr(d,d1,d2,d3,d4,d5,d6,d7)
+  f$dataset_id <- dataset_id
+  f$country <- replace(f$country,1:nrow(f),"Ethiopia")
+  f$latitude <- 9.14500
+  f$longitude <- 40.48967
+  f$on_farm <- "yes"
+  f$crop[is.na(f$crop)] <- "common bean" 
+  
+  # Fertilizer rates: DAP will be applied using a rate of 25 kg DAP per hectare.
+  f$fertilizer_type[f$treatment %in% c("25kgDAP","25kgDAP&Inoculant")] <- "DAP"
+  f$P_fertilizer[f$fertilizer_type %in% c("25kgDAP","25kgDAP&Inoculant")] <- 25 # ignored the -P and +P treatments as there's no explanation as to what they actually mean
+  
+  f <- f[,c("dataset_id","adm1","adm2","location","trial_id","country","crop","variety","on_farm","start_date",
+            "end_date","rep","treatment","rain","soil_pH","soil_SOC","soil_N","soil_sand","soil_clay","grain_weight",
+            "fertilizer_type","P_fertilizer","biomass_total","yield","residue_yield","latitude","longitude")]
+  
+  carobiner::write_files(dset, f, path, dataset_id, group)
   TRUE
 }
