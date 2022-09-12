d03969f64ac4bad322962ff1d1eb68f1207dea1b
smkuhlani <siyabusa@gmail.com>
2022-09-12 18:28:26 +0300

    6 additional nut-fert data ses

---
+++ scripts/fertilizer/doi_10.21421_D2_01WXFG.R
@@ -0,0 +1,64 @@
+# R script for "carob"
+
+## ISSUES
+#Not showing license, showing question mark
+
+#remotes::install_github("reagro/carobiner")
+library(carobiner)
+path<-"/Users/siyabusa/OneDrive - CGIAR/EiA/Scripts/carob"
+carob_script <- function(path) {
+  
+  "Description
+Title: Response of Groundnut to plant density and phosphorous application in the sudan savanna zone of Minjibir, Nigeria. 
+Abstract: Despite the recent release of several improved varieties of groundnut in Nigeria the productivities have not increase significantly due to lack of commensurate recommendation in agronomic practices. Two groundnut varieties were evaluated for their response to different plant density and phosphorus application in two locations in the Sudan Savanna zone of Nigeria in 2012 and 2013. The groundnut were planted at density of 44444, 66667, and 133333 hills ha-1 with average of two plants per hill. Phosphorus was applied at rate of 0 or 20 kg P ha-1 . P fertilizer application increased pod and haulm yields by 26% and 16% respectively in Minjibir. It increased pod and haulm yields by 62% and 27% respectively in Wudil. Pod and haulm yields, harvest index, revenue, profit and cost benefit ratio increased with increasing plant density. Samnut-24 produced pod yields that were significantly higher than Samnut-22 across treatments. Pod yields at density of 133,333 hills ha-1 was 31% higher than at 66667 and 40% than at 44,444 hills ha-1. Application of fertilizer increased profit by 22% and 49% in Minjibir and Wudil respectively. Planting at density of 133,333 hill ha-1 increased profit by 19% and 27% over 66,667 and 444444 hill ha-1 respectively in Minjibir, while it increase profit by 9% in Wudil. Cultivation of Samnut-24 at high density with 
+  phosphorus application will make groundnut production a more profitable venture in Sudan Savanna zone of Minjibir, Nigeria."
+  
+  ## Process 
+ 
+  uri <- "doi:10.21421/D2/01WXFG"
+  dataset_id <- agro::get_simple_URI(uri)
+  group <- "fertilizer"
+  
+  dset <- data.frame(
+    dataset_id = dataset_id,
+    group=group,
+    uri=uri,
+    publication="",
+    carob_contributor="Siyabusa Mkuhlani",
+    experiment_type="fertilizer",
+    has_weather=FALSE,
+    has_management=TRUE
+  )
+  
+  ## treatment level data 
+  ff  <- carobiner::get_data(uri, path, group)
+  
+  ## read the json for version, license, terms of use  
+  js <- carobiner::get_metadata(dataset_id, path, major=1, minor=0, group)
+  dset$license <- carobiner::get_license(js) 
+  
+  ## the AFSIS data 
+  f <- ff[basename(ff) == "Data file of Groundnut to plant density and phosphorous application in Minjibir 2012-13.xlsx"]
+  d <- suppressMessages(as.data.frame(readxl::read_excel(f)))
+  
+  names(d)
+  e<-d[,c(1,2,4,5,6,7,14,15)]
+  names(e)
+  colnames(e)<-c('season','location','rep','variety_type','treatment','spacing','yield','residue_yield')
+  
+  e$country<- "Nigeria"
+  e$crop<-"groundnut"
+  e$dataset_id <- dataset_id
+  e$trial_id <- paste0("gnut_dens_phosph_", e$location)
+  e$adm1<-'Kano'
+  e$on_farm<-FALSE
+  e$is_survey<-FALSE
+  e$P_fertilizer[e$treatment=='F1']<-'0'
+  e$P_fertilizer[e$treatment=='F2']<-'20'
+  names(e)
+  e<-e[c("dataset_id","country", "adm1", 'location', "trial_id", "season","on_farm", "is_survey", "rep", "crop", "variety_type","yield", "residue_yield")]  
+ #Still need to decode the treatment. Which is a level of fertilizer
+ carobiner::write_files(dset, e, path, dataset_id, group)
+}
+
+
---
+++ scripts/fertilizer/doi_10.21421_D2_YDFJOB.R
@@ -0,0 +1,65 @@
+# R script for "carob"
+
+## ISSUES
+#Not showing license, showing question mark
+
+#remotes::install_github("reagro/carobiner")
+library(carobiner)
+path<-"/Users/siyabusa/OneDrive - CGIAR/EiA/Scripts/carob"
+carob_script <- function(path) {
+  
+  "Description
+Title:Sorghum productivity and water use under Phosphorus fertilization in the Sudan savanna of Nigeria 
+Abstract: Low soil fertility and water shortage are major constraints to food production and food security in semi-arid environments. Field experiments were conducted during two growing seasons (2014 and 2015) in Nigeria. The study examined the effects of Phosphorus (P) applications on crop transpiration (ETc) water use efficiency (WUE)and agronomy phosphorus use efficiency (APUE) and sorghum productivity. The experiments were arranged in split plot design with five (5) P-fertilizerlevels(0, 15, 30, 45 and 60 kg P205ha-1) as the main plot and threevarieties (CSR01, ICSV400 and local) as sub-plot in four replications.Results showed significant differences (P<0.05) amongthe P levels and sorghum varieties for grain yield in both locations and seasons. P increased grain yield by 19-39% over control treatment.The highest mean yield of 3156 kg ha-1 at Minjibir and 2929 kg ha-1 at BUK indicate optimum yield was recorded at the 45 kgP205ha-1 application rate and significantly higher than P rates at 0, 15 and 30kgha-1 respectively.Grain yield WUE washighly significantamongP-fertilizer levels and varieties, however, no significant differences between P-fertilizer rates for biomass WUE.P-application increased grain WUE of sorghum by 20-39%, the ICSV400 estimated the mean highest value of 9.3 and 8.6 kg ha-1mm-1 over CSR-01 and local at both locations.The study observed that the application of P could be an effective fertilization strategy to enhance sorghum yield and water use in low-rainfall cropping system 
+  and drought prone environment."
+  
+  ## Process 
+ 
+  uri <- "doi:10.21421/D2/YDFJOB"
+  dataset_id <- agro::get_simple_URI(uri)
+  group <- "fertilizer"
+  
+  dset <- data.frame(
+    dataset_id = dataset_id,
+    group=group,
+    uri=uri,
+    publication="",
+    carob_contributor="Siyabusa Mkuhlani",
+    experiment_type="fertilizer",
+    has_weather=FALSE,
+    has_management=TRUE
+  )
+  
+  ## treatment level data 
+  ff  <- carobiner::get_data(uri, path, group)
+  
+  ## read the json for version, license, terms of use  
+  js <- carobiner::get_metadata(dataset_id, path, major=1, minor=0, group)
+  dset$license <- carobiner::get_license(js) #Cant get the license right??
+  
+  ## the AFSIS data 
+  f <- ff[basename(ff) == "Data file of Sorghum productivity and water use under phosphorous fertilization.xlsx"]
+  d <- suppressMessages(as.data.frame(readxl::read_excel(f)))
+
+  ##Convert First Row to Header
+  e<-d[,c(1,2,4,5,6,14,15,16)]
+  colnames(e)<-c('season','adm1','rep','P_fertilizer','variety_type','yield','biomass_stems','grain_weight')
+  e$country<- "Nigeria"
+  e$crop<-"sorghum"
+  e$dataset_id <- dataset_id
+  e$trial_id <- paste0('P_fert_', e$adm1)
+  e$on_farm<-FALSE
+  e$is_survey<-FALSE
+  
+  #Replace values in a data frame
+  e["location"][e["location"]=="BUK"]<-"Bayero"
+  
+  e$adm1[e$location=='Minjibir']<-'Kano'
+  e$adm1[e$location=='Bayero']<-'Kano'
+  
+  e<-e[c("dataset_id","country", "adm1",'location',"trial_id", "season","on_farm", "is_survey", "rep", "crop", "variety_type","residue_yield", "yield", "grain_weight", "P_fertilizer")]
+   
+ carobiner::write_files(dset, e, path, dataset_id, group)
+}
+
+
