4e232611eef8abee98a0aa229f2aae3804bd73b7
rhijmans <r.hijmans@gmail.com>
2023-02-23 19:25:13 -0800

    fix

--- scripts/fertilizer/doi_10.25502_a7ex-ea51_d.R
+++ scripts/fertilizer/doi_10.25502_a7ex-ea51_d.R
@@ -6,6 +6,7 @@
 #################################################################################
 
 carob_script <- function(path){
+
 uri <- "doi.org/10.25502/a7ex-ea51/d"
 dataset_id <- carobiner::simple_uri(uri)
 group <- "fertilizer"
@@ -27,7 +28,7 @@ dset <- data.frame(
   of Tropical Agriculture (IITA). 
   https://doi.org/10.25502/A7EX-EA51/D",
   carob_contributor = "Effie Ochieng",
-  experiment_type = "variety_trials",
+  experiment_type = "on farm",
   has_weather =  FALSE,
   has_management = FALSE
 )
@@ -46,77 +47,73 @@ dset <- data.frame(
  f1 <- ff[basename(ff) == "general.csv"]
  d1 <- data.frame(read.csv2(f1, sep = ","))
  
- f2 <- ff[basename(ff) == "soil_properties.csv"]
- d2 <- data.frame(read.csv2(f2, sep = ","))
- 
-#create a "variable 'crop' and fill it using the varieties, data set had no crop variable
- d$crop  <-
- d$crop[d$variety == "Peka 6"] <- "soybean"
- d$crop[d$variety == "RWA 1668"] <- "common bean"
- d$crop[d$variety == "Peka6"] <- "soybean"
- d$crop[d$variety == "Sc. Saga"] <- "soybean"
- d$crop[d$variety == "Sc. Square"] <- "soybean"
- d$crop[d$variety == "Sc. Squille"] <- "soybean"
- d$crop[d$variety == "SB8"] <- "soybean"
- d$crop[d$variety == "SB24"] <- "soybean"
- d$crop[d$variety == "Gasirida "] <- "common bean"
- d$crop[d$variety == "RWR 1668"] <- "common bean"
- 
- #process data sets separately identifying the variables of interest
- d$trial_id <- d$experiment_id
- d$rep <- d$replication_no
- d$on_farm <-TRUE
- d$start_date <-ifelse(d$planting_date_yyyy == 0, NA, paste(d$planting_date_yyyy, sprintf("%02d", d$planting_date_mm), sprintf("%02d", d$planting_date_dd), sep = "-"))
- d$end_date <- ifelse(d$date_harvest_yyyy == 0, NA, paste(d$date_harvest_yyyy, sprintf("%02d", d$date_harvest_mm), sprintf("%02d", d$date_harvest_dd), sep = "-"))
- d$treatment <- paste(d$main_treatment,d$sub_treatment_inoc,d$sub_treatment_fert, sep = "-")
- d$biomass_roots <- as.numeric(d$root_dry_weight_roots_no_nodules)
- d$biomass_total <- as.numeric((d$above_ground_dry_biomass + d$root_dry_weight_roots_no_nodules + d$nodule_dry_weight))
- d$yield <- as.numeric(d$grain_yield_ha_calc)
+# f2 <- ff[basename(ff) == "soil_properties.csv"]
+# d2 <- data.frame(read.csv2(f2, sep = ","))
 
- # adding the fertilizer information
- d$fertilizer_combination <- d$sub_treatment_inoc
- d$P_fertilizer[d$fertilizer_combination == "DAP "| d$fertilizer_combination == "DAP"|
-                  d$fertilizer_combination == "TSP" | d$fertilizer_combination == "TSP/KCL"] <- 30
- 
- d$K_fertilizer[d$fertilizer_combination == "TSP/KCL"]<- 30
-
- #subset the processed variables
- d <- d[, c("trial_id", "rep", "on_farm","start_date","end_date", "treatment", "biomass_roots","biomass_total", "yield", "crop","P_fertilizer","K_fertilizer")]
- 
  d1$trial_id <- d1$experiment_id
  d1$country <-"Rwanda"
  d1$adm2 <- d1$mandate_area_name
  d1$site <- d1$action_site
- 
-
-#subset the processed variables
  d1 <- d1 [, c("trial_id", "country", "site","adm2")]
+
+## Fix 
+#  d1$latitude <- 
+#  d1$longitude <- 
  
- d2$trial_id <- d2$experiment_id
+ 
+ ##d2$trial_id <- d2$experiment_id
  # EGB: pH is NULL
  # d2$soil_pH <- as.numeric(d2$ph)
- d2$soil_K <- as.numeric(d2$k)
- d2$soil_sand <- as.numeric(d2$sand)
- d2$soil_clay <- as.numeric(d2$clay)
- d2$soil_SOC <- as.numeric(d2$tot_carbon)
- d2$soil_N <- as.numeric(d2$tot_nitrogen)
+ ## RH these are also all zero!
+ ##d2$soil_K <- as.numeric(d2$k)
+ ##d2$soil_sand <- as.numeric(d2$sand)
+ ##d2$soil_clay <- as.numeric(d2$clay)
+ ##d2$soil_SOC <- as.numeric(d2$tot_carbon)
+ ##d2$soil_N <- as.numeric(d2$tot_nitrogen)
  
  #subset the processed variables
- # d2 <- d2[, c("trial_id", "soil_pH", "soil_K", "soil_sand","soil_clay","soil_SOC","soil_N")]
- d2 <- d2[, c("trial_id", "soil_K", "soil_sand","soil_clay","soil_SOC","soil_N")]
-
- # combining the processed data sets to one
- s <- merge(d, d1, by = "trial_id")
- q <- merge(s, d2, by = "trial_id")
- #add the gps information
- q$latitude <-  -1.94028
- q$longitude <- 29.87389
+ ##d2 <- d2[, c("trial_id", "soil_K", "soil_sand","soil_clay","soil_SOC","soil_N")]
  
- q$dataset_id <- dataset_id
+ 
+#create a "variable 'crop' and fill it using the varieties, data set had no crop variable
+ e <- data.frame(variety = trimws(d$variety))
+ e$variety[e$variety==""] <- NA
+
+## RH: there are many records for which the crop is not know (and neither is the variety)
+## That needs to be resolved
+ e$crop <- NA
+ e$crop[d$variety %in% c("Peka 6", "Peka6", "Sc. Saga", "Sc. Square", "Sc. Squille", "SB8", "SB24")] <- "soybean"
+ e$crop[d$variety %in% c("RWA 1668", "Gasirida", "RWR 1668")] <- "common bean"
+ 
+ #process data sets separately identifying the variables of interest
+ e$trial_id <- d$experiment_id
+ e$rep <- d$replication_no
+ e$on_farm <-TRUE
+ e$start_date <- as.character(as.Date(paste(d$planting_date_yyyy, d$planting_date_mm, d$planting_date_dd, sep = "-")))
+ e$end_date <- as.character(as.Date(paste(d$date_harvest_yyyy, d$date_harvest_mm, d$date_harvest_dd, sep = "-")))
+ e$treatment <- paste(d$main_treatment, d$sub_treatment_inoc, d$sub_treatment_fert, sep = "-")
+ e$biomass_roots <- as.numeric(d$root_dry_weight_roots_no_nodules)
+ e$biomass_total <- as.numeric((d$above_ground_dry_biomass + d$root_dry_weight_roots_no_nodules + d$nodule_dry_weight))
+ e$yield <- as.numeric(d$grain_yield_ha_calc)
+
+ # adding the fertilizer information
+ fert <- trimws(d$sub_treatment_inoc)
+
+ e$P_fertilizer[fert %in% c("DAP", "TSP", "TSP/KCL")] <- 30
+ e$K_fertilizer[fert == "TSP/KCL"]<- 30
+ ## fix
+ ## d$N_fertilizer[fert %in% c("DAP")] <- ????
+ 
+ #subset the processed variables
+# d <- d[, c("trial_id", "rep", "on_farm","start_date","end_date", "treatment", "biomass_roots","biomass_total", "yield", "crop","P_fertilizer","K_fertilizer")]
+ 
+ # combining the processed data sets to one
+ e <- merge(e, d1, by = "trial_id")
+ 
+ e$dataset_id <- dataset_id
  
  # all scripts should end like this
- carobiner::write_files(dset, q, path, dataset_id, group)
- TRUE
+ carobiner::write_files(dset, e, path, dataset_id, group)
 }
 
 
