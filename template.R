05bfa780663f22a7acfef48cd623fa63fe2b2c4d
rhijmans <r.hijmans@gmail.com>
2023-02-25 18:56:11 -0800

    temp

--- template.R
+++ template.R
@@ -44,17 +44,69 @@ carob_script <- function(path) {
 
 	f <- ff[basename(ff) == "_____________"]
 
-	d <- read.csv(f)
-	d <- readxl::read_excel(f) |> as.data.frame()
+	r <- read.csv(f)
+	r <- readxl::read_excel(f) |> as.data.frame()
+
 	
-	# process file(s)
-	d <- carobiner::change_names(d, from, to)
+## process file(s)
+
+## use a subset
+	d <- carobiner::change_names(r, from, to)
+
+	
+#### about the data #####
+## (TRUE/FALSE)
+
 	d$dataset_id <- dataset_id
+	d$on_farm <- 
+	d$is_survey <- 
+	d$irrigated <- 
+## the treatment code	
+	d$treatment <- 
 
-#   d$longitude <- 
-#   d$latitude <- 
+
+##### Location #####
+## make sure that the names are normalized (proper capitalization, spelling, no additional white space).
+## you can use carobiner::fix_name()
+	d$country <- 
+	d$site <- 
+	d$adm1 <- 
+	d$adm2 <- 
+	d$elevation <- NA
+## each site must have corresponding longitude and latitude
+	d$longitude <- 
+	d$latitude <- 
 
 
+
+##### Crop #####
+## normalize variety names
+	d$crop <- 
+	d$variety <- 
+
+##### Time #####
+## time can be year (four characters), year-month (7 characters) or date (10 characters).
+## use 	as.character(as.Date()) for dates to assure the correct format.
+	d$start_date <- as.character(as.Date(   ))
+	d$end_date  <- as.character(as.Date(    ))
+
+##### Fertilizers #####
+## note that we use P and K, not P2O5 and K2O
+## P <- P2O5 / 2.29
+## K <- K2O / 1.2051
+   d$P_fertilizer <- 
+   d$K_fertilizer <-
+   d$N_fertilizer <- 
+## normalize names 
+   d$fertlizer_type <- 
+   d$inoculated <- 
+   
+
+##### Yield #####
+
+	d$yield <- 
+	d$biomass_total <- 
+	
 # all scripts must end like this
 	carobiner::write_files(dset, d, path, dataset_id, group)
 }
