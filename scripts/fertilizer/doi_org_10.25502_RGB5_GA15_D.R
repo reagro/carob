979ad828d3e6dd951e56435cd3e852fc434d161f
rhijmans <r.hijmans@gmail.com>
2023-02-27 21:09:04 -0800

    overhaul, more needed

--- scripts/fertilizer/doi_org_10.25502_RGB5_GA15_D.R
+++ scripts/fertilizer/doi_org_10.25502_RGB5_GA15_D.R
@@ -33,16 +33,13 @@ carob_script <- function(path) {
   )
   
   ## download and read data 
-  
-  ff  <- carobiner::get_data(uri, path, group)
+  ff <- carobiner::get_data(uri, path, group)
   js <- carobiner::get_metadata(dataset_id, path, group, major=2, minor=1)
   dset$license <- carobiner::get_license(js)
   
-  
-  
   f1 <- ff[basename(ff) == "BUK_T1_VT_yieldatharvest_summ.csv"] # dataset from team1
-  f2 <- ff[basename(ff) == "BUK_T3_VT_yieldatharvest_final.csv"]# dataset from team2
-  f3 <- ff[basename(ff) == "BUK-T2_VT_yieldatharvest_summ.csv"]# dataset from team3
+  f2 <- ff[basename(ff) == "BUK-T2_VT_yieldatharvest_summ.csv"] # dataset from team2
+  f3 <- ff[basename(ff) == "BUK_T3_VT_yieldatharvest_final.csv"]# dataset from team3
   # read the dataset
   d1 <- read.csv(f1)
   d2 <- read.csv(f2)
@@ -50,116 +47,77 @@ carob_script <- function(path) {
   #d <- readxl::read_excel(f) |> as.data.frame()
   
   # process file(s)
-  #d <- carobiner::change_names(d, from, to)
-  d1$dataset_id <- dataset_id
-  d2$dataset_id <- dataset_id
+
+##RH add plant density
   
-  #process team1 dataset
-  
-  d1$treatment<- d1$Section_B.repeat.trt_name
-  d1$yield <- d1$yld.plot.full.pd
-  
-  #d1$plant_density<-d1$
-  
-  d1$N_fertilizer<-ifelse(d1$Section_B.repeat.trt_name=="Control",0,100)
-                          
-  
-  d1$K_fertilizer<-ifelse(d1$Section_B.repeat.trt_name=="Control",0,60)
-                          
-  
-  d1$P_fertilizer<-ifelse(d1$Section_B.repeat.trt_name=="OCPF1",0,
-                          ifelse(d1$Section_B.repeat.trt_name=="OCPF2",20,0))
-  
-  #d1$Zn_fertilizer<-ifelse(d3$TrtDesc=="NPK+MN",3,0)
-  
-  #d1$S_fertilizer<-ifelse(d3$TrtDesc=="NPK+MN",5,0)
-  
-  d1=transform(d1,N_splits=ifelse(d1$N_fertilizer>0,3,0))
-  
-  d1<- d1[,c("dataset_id","treatment","yield","N_fertilizer","K_fertilizer","P_fertilizer","N_splits")]
-  
-  #process Team2 data 
-  
-  d2$yield<-d2$yld.full.pd
-  d2$treatment<-d2$trt_name
-  
-  d2$N_fertilizer<-ifelse(d2$trt_name=="Control",0,100)
-  
-  
-  d2$K_fertilizer<-ifelse(d2$trt_name=="Control",0,60)
-  
-  
-  d2$P_fertilizer<-ifelse(d2$trt_name=="OCPF1",0,
-                          ifelse(d2$trt_name=="OCPF2",20,0))
-  
-  #d1$Zn_fertilizer<-ifelse(d3$TrtDesc=="NPK+MN",3,0)
-  
-  #d1$S_fertilizer<-ifelse(d3$TrtDesc=="NPK+MN",5,0)
-  
-  d2=transform(d2,N_splits=ifelse(d2$N_fertilizer>0,3,0))
-  
-  d2 <- d2[,c("dataset_id","yield","treatment","N_fertilizer","K_fertilizer","P_fertilizer","N_splits")]
-  
-  #process Team3 data 
-  
-  d3$yield<-d3$yld.corr.pd..kg.ha.
-  
-  d3$treatment<-d3$trt_name
-  
-  d3$N_fertilizer<-ifelse(d3$trt_name=="Control",0,100)
-  
-  
-  d3$K_fertilizer<-ifelse(d3$trt_name=="Control",0,60)
-  
-  
-  d3$P_fertilizer<-ifelse(d3$trt_name=="OCPF1",0,
-                          ifelse(d3$trt_name=="OCPF2",20,0))
-  
-  #d1$Zn_fertilizer<-ifelse(d3$TrtDesc=="NPK+MN",3,0)
-  
-  #d1$S_fertilizer<-ifelse(d3$TrtDesc=="NPK+MN",5,0)
+	d1 <- d1[, c("Section_B.repeat.trt_name", "X_parent_index", "yld.plot.kg.ha")] 
+	colnames(d1) <- c("treatment", "rep", "yield")
+	d2 <- d2[, c("trt_name", "parent_index", "plot.yld..kg.ha.")] 
+	colnames(d2) <- c("treatment", "rep", "yield")
+	d3 <- d3[, c("trt_name", "parent_index", "yld.plot")] 
+	colnames(d3) <- c("treatment", "rep", "yield")
+	d <- rbind(d1, d2, d3)
+	
+				
+## RH: that is *not* the right number for yield
+#  d1$yield <- d1$yld.plot.full.pd
+# RH: I think it should be
+#  d1$yield <- d1$yld.plot.kg.ha 
  
-  d3=transform(d3,N_splits=ifelse(d3$N_fertilizer>0,3,0))
-   
-  d3<-d3[,c("treatment","N_fertilizer","K_fertilizer","P_fertilizer","N_splits")]
+  #d1$plant_density<-d1$
+
+
+
+## RH: I see:  
+
+#table(d$treatment)
+#Control     NPK   OCPF1   OCPF2 
+#    250     250     250     250 
+
+## the suggests that the below is not correct. Or at least it should be commented on
+## What are the contents of OCPF1 and OCPF2?
+## is it true that both contain 100 kg/ha of N and 60 kg/ha of K, but zero P??
+## None of that makes sense given that the NPK appears to be 15-15-15
+## on the face of it, that makes no sense
+## same applies to d2 and d3
+
+  d$N_fertilizer <- ifelse(d$treatment == "Control", 0, 100)
+  d$K_fertilizer <- ifelse(d$treatment == "Control", 0, 60)
+  d$P_fertilizer <- ifelse(d$treatment == "OCPF1", 0,
+                        ifelse(d$treatment == "OCPF2", 20, 0))
   
-  #merge all the data
-  list = list(d1,d2,d3)
-  d=Reduce(function(x, y) merge(x, y, all=TRUE), list)
+# RH where does this come from? Would be good to comment on.
+  d$N_splits <- ifelse(d$N_fertilizer > 0, 3, 0)
   
-  # add column
-  # replication column rep
-  for (i in 1:nrow(d)){# 
-    if(i==1){ # first replication value
-      d$rep[i]<-1
-      r<-1
-    }else if( d$treatment[i]==d$treatment[i-1]){ # give the same replication value for  the same treatment
-      d$rep[i]<-r
-      
-      
-    }
-    else {
-      d$rep[i]<-r+1
-      r<-1+r
-    }
-  }
   
-  d$country<- "Nigeria"
-  d$crop<- "maize"
-  d$latitude<-12.000000
-  d$longitude<-8.516667
-  d$start_date<-as.character(2018)
-  d$end_date<-as.character(2019)
-  d$location<-"Bayero"
-  d$adm1<-"kano"
+## RH: wow, why would you do that???
+## merge by what? (always specify the variables used to join the data ? 
+## These are three different datasets that need to be rbind-ed, I think.
+#merge all the data
+#  list <- list(d1, d2, d3)
+#  d <- Reduce(function(x, y) merge(x, y, all=TRUE), list)
+ 
+  d$dataset_id <- dataset_id
+  d$country <- "Nigeria"
+  d$crop <- "maize"
+
+#RH how do you know this? are these all in one location??
+  d$latitude <- 12.000000
+  d$longitude <- 8.516667
+
+  d$start_date <- "2018"
+  d$end_date <- "2019"
+  d$location <- "Bayero"
+  d$adm1 <- "Kano"
+ 
   d$trial_id <- paste0(dataset_id, '-', d$Location)
+
   # data type
-  d$yield<-as.numeric(d$yield)
-  d$rep<-as.integer(d$rep)
   # fill whitespace in observation 
-  d<- replace(d,d=='',NA)
+  ## do not blindly do things like this
+  # d <- replace(d, d=="",NA)
+
   # all scripts must end like this
-  carobiner::write_files(dset, d, path, dataset_id, group)
-  #TRUE
-  }
+ carobiner::write_files(dset, d, path, dataset_id, group)
+}
 
