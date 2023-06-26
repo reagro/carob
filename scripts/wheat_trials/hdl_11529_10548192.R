c8697d86492d36c50164f61ab6d37f443cbd64e6
asila <musilisila@gmail.com>
2023-06-26 14:29:06 +0300

    added more datasets

---
+++ scripts/wheat_trials/hdl_11529_10548192.R
@@ -0,0 +1,48 @@
+# R script for "carob"
+
+## ISSUES
+# ....
+# specify path parameter
+
+carob_script <- function(path) {
+
+"
+Description:
+CIMMYT annually distributes improved germplasm developed by its researchers and partners in international nurseries trials and experiments. The High Temperature Wheat Yield Trial (HTWYT) is a replicated yield trial that contains spring bread wheat (Triticum aestivum) germplasm adapted to Mega-environment 1 (ME1) which represents high temperature areas. (2017)
+"
+	dataset_id <- carobiner::simple_uri(uri)
+	group <- "wheat_trials"
+	## dataset level data 
+	dset <- data.frame(
+	   dataset_id = dataset_id,
+	   group=group,
+	   project=NA,
+	   uri=uri,
+	   ## if there is a paper, include the paper's doi here
+	   ## also add a RIS file in references folder (with matching doi)
+	   publication = NA,
+	   data_citation = "Global Wheat Program; IWIN Collaborators; Singh, Ravi; Payne, Thomas, 2019, '14th High Temperature Wheat Yield Trial', https://hdl.handle.net/11529/10548192, CIMMYT Research Data & Software Repository Network, V3, UNF:6:nUT7R+5rJbBpoezCfASFFg== [fileUNF]",
+	   data_institutions = "CIMMYT",
+	   carob_contributor="Andrew Sila",
+	   
+	   ## something like randomized control...
+	   experiment_type="On-station experiment",
+	   has_weather=FALSE,
+	   has_soil=FALSE,
+	   has_management=TRUE
+	)
+
+## download and read data 
+
+	ff  <- carobiner::get_data(uri, path, group)
+	js <- carobiner::get_metadata(dataset_id, path, group, major=3, minor=0)
+	dset$license <- carobiner::get_license(js)
+
+## process file(s)
+	proc_wheat <- carobiner::get_function("proc_wheat", path, group)
+	d <- proc_wheat(ff)
+	d$dataset_id <- dataset_id
+
+# all scripts must end like this
+	carobiner::write_files(dset, d, path, dataset_id, group)
+}
