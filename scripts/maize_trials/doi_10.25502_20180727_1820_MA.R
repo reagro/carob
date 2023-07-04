4074863ff45f8b4f29cf1527b366c7e96a14ed29
rhijmans <r.hijmans@gmail.com>
2023-07-03 19:47:42 -0700

    new maize

---
+++ scripts/maize_trials/doi_10.25502_20180727_1820_MA.R
@@ -0,0 +1,39 @@
+
+# striga file to be done
+
+carob_script <- function(path) {
+"Description:
+This is an international study that contains data on yield and other agronomic traits of maize including striga attacks on maize in Africa.
+
+The study was carried out by the International Institute of Tropical Agriculture in 2016 in eight African countries and one asian country.
+"
+
+	uri <- "doi:10.25502/20180727/1820/MA"
+	dataset_id <- carobiner::simple_uri(uri)
+	group <- "maize_trials"	
+		
+	## dataset level data 
+	dset <- data.frame(
+		dataset_id = dataset_id,
+		group=group,
+		uri = uri,
+		data_citation="Menkir, A. (2018). Grain Yield and Other Agronomic Traits of International Maize Trials – Gabon, 1990 [Data set]. International Institute of Tropical Agriculture (IITA). https://doi.org/10.25502/20180727/1820/MA",
+ 	    publication="doi:10.1016/j.jenvman.2017.06.058",
+		carob_contributor = "Robert Hijmans",
+		data_type = "experiment",
+		project="International Maize Trials",
+		data_institutions="IITA"
+	)
+
+	## download and read data 
+	ff  <- carobiner::get_data(uri, path, group)
+	js <- carobiner::get_metadata(dataset_id, path, major=2, minor=1, group)
+	dset$license <- carobiner::get_license(js)
+
+	mzfun <- carobiner::get_function("intmztrial_striga", path, group)
+
+	d <- mzfun(ff, "International_Maize_Trial_Gabon_Regular.csv", FALSE)
+	d$dataset_id <- dataset_id
+	
+	carobiner::write_files(dset, d, path=path)
+}
---
+++ scripts/maize_trials/doi_10.25502_20180830_1604_MA.R
@@ -0,0 +1,38 @@
+
+carob_script <- function(path) {
+"Description:
+This is an international study that contains data on yield and other agronomic traits of maize including striga attacks on maize in Africa.
+
+The study was carried out by the International Institute of Tropical Agriculture in 2016 in eight African countries and one asian country.
+"
+
+	uri <- "doi:10.25502/20180830/1604/MA"
+	dataset_id <- carobiner::simple_uri(uri)
+	group <- "maize_trials"	
+		
+	## dataset level data 
+	dset <- data.frame(
+		dataset_id = dataset_id,
+		group=group,
+		uri = uri,
+		data_citation="Menkir, A. (2018). Grain Yield and Other Agronomic Traits of International Maize Trials – Ghana, 2016 [Data set]. International Institute of Tropical Agriculture (IITA). https://doi.org/10.25502/20180830/1604/MA",
+ 	    publication="doi:10.1016/j.jenvman.2017.06.058",
+		carob_contributor = "Robert Hijmans",
+		data_type = "experiment",
+		project="International Maize Trials",
+		data_institutions="IITA"
+	)
+
+	## download and read data 
+	ff  <- carobiner::get_data(uri, path, group)
+	js <- carobiner::get_metadata(dataset_id, path, major=2, minor=1, group)
+	dset$license <- carobiner::get_license(js)
+
+	mzfun <- carobiner::get_function("intmztrial_striga", path, group)
+
+	d <- mzfun(ff, "Ghana_International_Maize_Trials_Regular2016.csv", FALSE)
+	d$dataset_id <- dataset_id
+	d$description <- as.character(d$description)
+	d$yield <- d$yield * 1000
+	carobiner::write_files(dset, d, path=path)
+}
