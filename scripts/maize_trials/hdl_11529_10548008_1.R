325a862618571f2de5333ec57bab8281f885418a
unknown <mitchynjukuya@gmail.com>
2023-09-07 12:01:32 +0200

    Group name changed

--- scripts/maize_trials/hdl_11529_10548008_1.R
+++ scripts/maize_trials/hdl_11529_10548008_1.R
@@ -14,7 +14,7 @@ Farmer participatory on-farm trials with CA technologies comparing with farmersâ
 
 	uri <- "hdl:11529/10548008"
 	dataset_id <- carobiner::simple_uri(uri)
-	group <- "maize_trial"
+	group <- "maize_trials"
 	## dataset level data 
 	dset <- data.frame(
 		dataset_id = dataset_id,
