8f908962eb1fc4813c406108c1d2d696595e8c98
unknown <mitchynjukuya@gmail.com>
2023-09-19 08:52:40 +0200

    Changes in group name

--- scripts/maize_trials/hdl_11529_10548008_2.R
+++ scripts/maize_trials/hdl_11529_10548008_2.R
@@ -16,7 +16,7 @@ carob_script <- function(path) {
   
   uri <- "hdl:11529/10548008"
   dataset_id <- carobiner::simple_uri(uri)
-  group <- "maize_trial"
+  group <- "maize_trials"
   ## dataset level data 
   dset <- data.frame(
     dataset_id = dataset_id,
