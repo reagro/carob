0b2413e835e30c7f9032d896f61956109c13cc12
rhijmans <r.hijmans@gmail.com>
2023-09-30 16:18:13 -0700

    dupliate

--- scripts/conservation_agriculture/hdl_11529_10825.R
+++ scripts/conservation_agriculture/hdl_11529_10825.R
@@ -1,4 +1,4 @@
-# R script for "carob"
+0# R script for "carob"
 
 ## ISSUES
 # ....
@@ -6,105 +6,128 @@
 
 carob_script <- function(path) {
   
-  "Description:
-  
-On-farm demonstration plots were set in Zambia to demonstrate the effects of conservation agriculture (CA) technologies as compared to the traditional farmers practice (ploughing with a mouldboard plough). The CA treatments included basins (BA), ripping (RI) and direct seeding with a direct seeder (DS) and direct seeding with a jab planter (JP). Also superimposed to the treatments are rotations and intercropping of maize with a grain legume (either soyabean or cowpea) and these are compared with continuous maize planting. The study is carried out in various communities of Zambia. Thus, the data set presents yields for maize and the legumes from these sites over 9 seasons (2006-2015). (2016-12-08)
+  "
+	Description:
+
+  On-farm demonstration plots were set in Zambia to demonstrate the 
+  effects of conservation agriculture (CA) technologies as compared 
+  to the traditional farmers practice (ploughing with a mouldboard plough). 
+  The CA treatments included basins (BA), ripping (RI) and direct seeding 
+  with a direct seeder (DS) and direct seeding with a jab planter (JP). 
+  Also superimposed to the treatments are rotations and intercropping of maize with 
+  a grain legume (either soyabean or cowpea) and these are compared 
+  with continuous maize planting. The study is carried out in various
+  communities of Zambia. Thus, the data set presents yields for maize 
+  and the legumes from these sites over 9 seasons (2006-2015). (2016-12-08)
 
 "
   
-  uri <- "hdl:11529/10825"
-  dataset_id <- carobiner::simple_uri(uri)
-  group <- "maize_trials"
-  ## dataset level data 
-  dset <- data.frame(
-    dataset_id = dataset_id,
-    group=group,
-    project=NA,
-    uri=uri,
-    data_citation="Thierfelder, Christian, 2016. Facilitating the widespread adoption of conservation agriculture in maize-based systems in Zambia. https://hdl.handle.net/11529/10825, CIMMYT Research Data & Software Repository Network, V3",
-    publication=NA,
-    data_institutions = "CIMMYT",
-    data_type="experiment",
-    carob_contributor="Fredy Chimire" 
-  )
+	uri <- "hdl:11529/10825"
+	dataset_id <- carobiner::simple_uri(uri)
+	group <- "wheat_trials"
+	## dataset level data 
+	dset <- data.frame(
+		dataset_id = dataset_id,
+		group=group,
+		project=NA,
+		uri=uri,
+		publication=NA,
+		data_citation="Thierfelder, Christian, 2016, Facilitating the widespread adoption of conservation agriculture 
+		in maize-based systems in Zambia, hdl:11529/10825, CIMMYT Research Data & Software Repository Network, V3",
+		data_institutions = "CIMMYT",
+		carob_contributor="Cedric Ngakou",
+		data_type="on-station experiment"
+    )
   
   ## download and read data 
   
   ff  <- carobiner::get_data(uri, path, group)
-  
   js <- carobiner::get_metadata(dataset_id, path, group, major=3, minor=1)
   dset$license <- carobiner::get_license(js)
   
-  
   f <- ff[basename(ff) == "Summary Zambia On-farm Demonstration 2006-2015.xls"]
   
-  #r <- read.csv(f)
-  r <- carobiner::read.excel(f, sheet = "Zambia all sites all maize")
-  r1 <- carobiner::read.excel(f, sheet = "Zambia all legume all years")
+  #read the data
   
-    
+  r <- carobiner::read.excel(f,sheet = 1) |> as.data.frame()
+  r1 <- carobiner::read.excel(f,sheet = 2) |> as.data.frame()
+  
+  
   ## process file(s)
   
+  d1 <- r[,c(2,3,4,18,19,21,22,20)]
+  # name columns with standard names 
+  colnames(d1) <- c("season","adm1","site","treatment","crop","residue_yield","yield","plant_density")
+  d1$crop <- "maize"
+  d2 <- r1[,c(2,3,4,13,14,16,17,15)]
+  colnames(d2) <- c("season","adm1","site","treatment","crop","residue_yield","yield","plant_density")
   
-  ## use a subset
-  #d <- carobiner::change_names(r, from, to)
+  # combine d1 and d2
+  d <- rbind(d1,d2)
   
-  d<-r
-  
-  #### about the data #####
-  ## (TRUE/FALSE)
-  
+  # add columns
+  d$country <- "Zambia"
   d$dataset_id <- dataset_id
-  d$harvest_date<- as.character(d$`1...2`)
-  d$on_farm <-TRUE
-  d$adm1<-d$`1...3`
+  d$trial_id <- paste0(d$dataset_id,"-",d$adm1)
+  d$planting_date  <- "2006"
+  d$harvest_date  <- "2015"
+  d$on_farm <- TRUE
   d$is_survey <- FALSE
-  #d$is_experiment <- TRUE
   d$irrigated <- FALSE
-  ## the treatment code	
-  d$treatment <- d$Tmnt.
-  d$yield<- d$`Grain yield (kg/ha)`
-  d$residue_yield<-d$`Stalk yield (kg/ha)`
-  d$crop<-"maize"
-  d$rep<- as.integer(d$`Plot No.`)
-  d$location<-d$`1...4`
-  d$country <-"Zambia"
-  d$yield_part <- "grain"
-  d$striga_trial <- FALSE
-  d$borer_trial <- FALSE
-  d$striga_infected <- FALSE
-  #d$trial_id <- 
-  d$trial_id = as.character(d$treatment)
+  #fix residue_yield and plant_density out of the bounds
+  d$residue_yield[d$residue_yield>60000] <-NA
+  d$plant_density[d$plant_density<1000] <-NA
+  # fix name treatment name
   
-  d <- d[,c("dataset_id","trial_id","harvest_date","on_farm","adm1","is_survey","irrigated","treatment","yield","residue_yield",
-            "crop","rep","location","country","yield_part","striga_trial","borer_trial","striga_infected")]
+  p <- carobiner::fix_name(d$treatment)
+  p <- gsub("Direct seeder","DS",p)
+  p <- gsub("direct seeder","DS",p)
+  p <- gsub("Direct","DS",p)
+  p <- gsub("Control plot","control",p)
+  #p <- gsub("DS, soybean-maize rotation","DS, maize-soybean rotation",p)
+  #p <- gsub("DS, cowpea-maize rotation","DS, maize-cowpea rotation",p)
   
+  d$treatment <-  p 
   
-  
-  
-  #d<-d[,(ncol(d)-13):ncol(d)]
-  
-  # Pick unique coordinates of the location
-  locs <- unique(d[,c("country","location","adm1")])
+  # fix crop name
+  e <- carobiner::fix_name(d$crop,"lower")
+  d$crop <- e 
+  d$crop[d$crop=="cowpeas"] <-  "cowpea"
+  #add inter crop crop rotation column 
  
-##RH do not run carobiner::geocode in the script!
-## use the most detailed info for geocoding and add the output to the script
- 
-  # Get the geo coordinates of the location
-  #geocodes <- carobiner::geocode(country=locs$country,location=locs$location,adm1=locs$adm1)
-#  geocodes <- carobiner::geocode(country=locs$country,location=locs$adm1)
-#  geocodes1 <- geocodes$df
+  d$intercrops  <- ifelse(d$treatment=="DS, maize/cowpea int" ,"cowpea",
+                ifelse(d$treatment=="DS, maize-cowpea rotation","soybean",
+                ifelse(d$treatment=="Ripper, maize-soybean rotation","cowpea",
+                ifelse(d$treatment=="DS, maize-soybean rotation","soybean",
+                ifelse(d$treatment=="DS, cowpea-maize rotation","soybean",
+                ifelse(d$treatment=="Ripper, soybean-maize rotation","cowpea",
+                ifelse(d$treatment=="DS, soybean-maize rotation","cowpea","no crop")))))))
+  d$crop_rotation <- ifelse(d$intercrops=="cowpea","soybean",
+                ifelse(d$intercrops=="no crop","no crop","cowpea"))
+                                   
   
+                                                             
+  # add longitude and  latitude
+  d$latitude[d$adm1=="Monze"]  <- -16.2759563
+  d$longitude[d$adm1=="Monze"]  <- 27.4763925
+  d$latitude[d$adm1=="Kabwe"]  <- -14.4571147
+  d$longitude[d$adm1=="Kabwe"]  <- 28.3992336
+  d$latitude[d$adm1=="Chipata"]  <- -13.7478246
+  d$longitude[d$adm1=="Chipata"]  <- 32.6324569
+  d$latitude[d$adm1=="Chibombo"]  <- -14.8569383
+  d$longitude[d$adm1=="Chibombo"]  <- 27.6530228
+  d$latitude[d$adm1=="Lundazi"]  <- -12.4137188
+  d$longitude[d$adm1=="Lundazi"]  <- 33.3487457
+  d$latitude[d$adm1=="Katete"]  <- -14.060241
+  d$longitude[d$adm1=="Katete"]  <- 32.04272
   
-#  mergeddf <- merge(d,geocodes2,by=c("country","adm1"))
-  
-    
+    # data type 
+    d$season <- as.character(d$season)
+    d$yield <- (as.double(d$yield))
+	d$yield_part <- "grain"
     # all scripts must end like this
-    #carobiner::write_files(dset, d, path=path)
-    carobiner::write_files(dset, mergeddf, path=path)
+    carobiner::write_files(dset, d, path=path)
 }
 
-## now test your function in a clean R environment 
-# path <- _____
-#carob_script(path)
+
 
