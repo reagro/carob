08b6cd4f3403b2df8327a7c45751d42bc1869d93
Fredy Chimire <Fredy.Chimire@econet.co.zw>
2023-09-30 15:32:28 +0200

    First standardised dataset sheet

---
+++ scripts/maize_trials/hdl_11529_10548052.R
@@ -0,0 +1,112 @@
+# R script for "carob"
+
+path<-getwd()
+# install.packages("remotes")
+# install.packages("httr")
+# 
+# 
+# remotes::install_github("reagro/carobiner")
+# ## ISSUES 
+
+# ....
+
+
+carob_script <- function(path) {
+  
+  "Description:
+• APSIM is an internationally well-recognized cropping systems models for scientists. An SRFSI team conducted many on-station trials in partners’ research station, which managed by SRFSI NARES partners’ scientists for validation of APSIM. The models simulated biophysical performances on the systems on a daily basis in terms of crop yields, water use, soil dynamics, climate, the fate of nutrients, and residues management. • APSIM was regionally validated using also results from on-farm experiments; validated models were used to explore the profitability, sustainability, and risk reduction potential of technological options under different contemporary and future climate scenarios. Using actual, historical, and predicted sets of climate data, the model was used to explore technology effects, sustainability, and riskiness over longer time periods than possible for field results in a 4-year project. Researchers from the region developed expertise in the use of the APSIM model, and the modeling effort was supported by CSIRO scientists
+
+"
+  
+  uri <- "hdl:11529/10548052"
+  dataset_id <- carobiner::simple_uri(uri)
+  group <- "maize_trials"
+  ## dataset level data 
+  dset <- data.frame(
+    dataset_id = dataset_id,
+    group=group,
+    project=NA,
+    uri=uri,
+    data_citation="Gaydon, Don (CSIRO-Australia) - ORCID: 0000-0002-0078-4154
+                  Laing, Alison (CSIRO-Australia) - ORCID: 0000-0001-8984-7029
+                 Poulton, Perry (CSIRO-Australia) - ORCID: 0000-0003-1341-5056
+                 SRFSI team",
+    ## if there is a paper, include the paper's doi here
+    ## also add a RIS file in references folder (with matching doi)
+    publication= NA,
+    data_institutions = "CIMMYT",
+    data_type="on_station",
+    carob_contributor="Fredy chimire" 
+  )
+  
+  ## download and read data 
+  
+  ff  <- carobiner::get_data(uri, path, group)
+  js <- carobiner::get_metadata(dataset_id, path, group, major=2, minor=1)
+  dset$license <- carobiner::get_license(js)
+  
+  
+  f <- ff[basename(ff) == "APSIM-IrrXN- rabi maize 2015-16-SRFSI-OFRD-Rangpur.xlsx"]
+  
+  # Select sheeet with revised data from the excel file 
+  d <- carobiner::read.excel(f, sheet = "Stat")
+  
+  #r <- readxl::read_excel(f,sheet = 5) |> as.data.frame() ''alternative to read excel''
+  
+  
+  #### about the data #####
+  ## (TRUE/FALSE)
+  
+  d$dataset_id <- dataset_id
+  d$on_farm <- FALSE
+  d$is_survey <- FALSE
+  d$is_experiment <- FALSE
+  d$irrigated <- ifelse(d$Irrigation=="I1",FALSE,TRUE)
+  ## the treatment code	
+
+d$treatment <- ifelse(d$Irrigation == "I5",
+                      "CT Maize (6 irrigations) (irrig at V4, V8, V12, tasseling, milking, soft-dough)",
+                      ifelse(d$Irrigation == "I4",
+                             "ST Maize (6 irrigations) (irrig at V4, V8, V12, tasseling, milking, soft-dough)",
+                             ifelse(d$Irrigation == "I3",
+                                    "ST Maize (4 irrigations) (irrig at V6, V10, tasseling, soft-dough)",
+                                    ifelse(d$Irrigation == "I2",
+                                           "ST Maize (2 irrigations) (irrig at V6, tasseling)",
+                                           "ST Maize (0 irrigations)"
+                                    )
+                             )
+                      )
+)
+
+
+
+
+  
+  ##### Location #####
+  ## make sure that the names are normalized (proper capitalization, spelling, no additional white space).
+  ## you can use carobiner::fix_name()
+  d$country <- "Bangladesh"
+  
+  #d$trial_id <- d$`HH-ID`
+  d$latitude <- 25.72093 # Gps are available from General and Phenology sheet
+  d$longitude <- 89.26442
+  d$planting_date <- as.Date("23/11/2015", format = "%d/%m/%Y")
+  d$yield_part <- "grain"
+  d$plant_height <- d$`Plant height`
+  d$variety <-  "Kavery25K60"
+  d$crop <-"maize"
+  d$yield <- d$`Grain Yield`*1000
+  
+  # selecting columns of interest which match the carob standard format
+  d <- d[,c("dataset_id","planting_date","on_farm","is_survey","is_experiment","irrigated","yield","latitude",
+            "longitude","crop","country","yield_part","plant_height","variety","plant_spacing","treatment")]
+
+  
+  # all scripts must end like this
+  #carobiner::write_files(dset, d, path=path)
+}
+
+## now test your function in a clean R environment 
+#path <- getwd()
+carob_script(path)
+
