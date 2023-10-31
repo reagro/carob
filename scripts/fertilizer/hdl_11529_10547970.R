5b07d48b396032baac3dd868f1fa3d5c310deac9
rhijmans <r.hijmans@gmail.com>
2023-10-31 18:41:59 +0100

    for #213

--- scripts/fertilizer/hdl_11529_10547970.R
+++ scripts/fertilizer/hdl_11529_10547970.R
@@ -34,111 +34,120 @@ carob_script <- function(path) {
 
 ## download and read data 
 	ff  <- carobiner::get_data(uri, path, group)
+	ff <- ff[-grep("^hdl", basename(ff))]
 	js <- carobiner::get_metadata(dataset_id, path, group, major=2, minor=1)
 	dset$license <- carobiner::get_license(js)
 
-	f <- ff[basename(ff) == "Rabi Maize 2016-17-LT-all nodes-Malda.xlsx"]
 
-# fix the bad spreadsheets
-	fix_ss <- function(x, hdr) {
-		nms <- x[hdr,]
-		if (is.na(nms[1,1])) nms[1,1] <- "" 
-		for (i in 2:ncol(nms)) {
-			if (is.na(nms[1,i])) nms[1,i] <- nms[1,i-1]
-		}		
-		nms <- apply(nms, 2, \(i) paste(i[!is.na(i)], collapse="_"))
-		x <- x[-c(1:max(hdr)), ]
-		names(x) <- nms
-		x
+	get_raw_data <- function(f) {
+		if (basename(f) == "Rabi Maize 2016-17-LT-all nodes-Malda.xlsx") {
+			skip1 = 3
+		} else {
+			skip1=4
+		}
+		r1 <- carobiner::read.excel.hdr(f, sheet ="4- Stand counts & Phenology", skip=skip1, hdr=2)
+		r2 <- carobiner::read.excel.hdr(f, sheet ="14 - Grain Harvest ", skip=4, hdr=2)
+		r3 <- carobiner::read.excel.hdr(f, sheet ="6 - Fertilizer amounts ", skip=4, hdr=2)
+
+		colnames(r2) <- gsub("Calculation_", "", colnames(r2))
+		colnames(r2) <- gsub("Straw.Stover.yield.t.ha" , "Straw.yield.t.ha" , colnames(r2))
+		
+		nms <- c("Site.No", "Tmnt", "Grain.yield.t.ha", "TGW.g", "Biomass.t.ha", "Straw.yield.t.ha")
+		r2 <- r2[, nms]
+		
+		colnames(r3) <- gsub("Kg.ha_N.kg.ha", "N.kg.ha", colnames(r3))
+		nms <- c("Site.No", "Tmnt", "N.kg.ha", "P2O5.kg.ha", "K2O.kg.ha", "Gypsum.kg.ha", "ZnSO4.kg.ha", "Boric.acid.kg.ha", grep("Product.used", names(r3), value=TRUE))
+		r3 <- r3[, nms]
+
+		r <- merge(r1, r2, by=c("Site.No", "Tmnt"))
+		merge(r, r3, by=c("Site.No", "Tmnt"))
 	}
 
 
-	pr <- carobiner::read.excel(f, sheet = "4- Stand counts & Phenology")
-	pr <- fix_ss(pr, 2:3)
-
-	fr <- carobiner::read.excel(f, sheet = "6 - Fertilizer amounts ")
-	fr <- fix_ss(fr, 3:4)
-
-	famnt <- c("N  (kg/ha)", "P2O5 (kg/ha)", "K2O (kg/ha)", "Gypsum (kg/ha)", "ZnSO4 (kg/ha)", "Boric acid (kg/ha)")
-	famnt <- paste0("Fertilizer 5 Application_", famnt)
-	prds <- grep("Product used", names(fr), value=TRUE)
-	fr <- fr[, c("Site No.", "Tmnt", famnt, prds)]
-
-
-	yr <- carobiner::read.excel(f, sheet = "14 - Grain Harvest ")
-	yr <- fix_ss(yr, 3:4)
-	nms <- c("Grain yield (t/ha)", "TGW (g)", "Biomass (t/ha)", "Straw yield (t/ha)", "HI")
-	yr <- yr[, c("Site No.", "Tmnt", paste0("Calculation_", nms))]
-	colnames(yr) <- gsub("Calculation_", "", colnames(yr))
-	
-	r <- merge(pr, fr, by=c("Site No.", "Tmnt"))
-	r <- merge(r, yr, by=c("Site No.", "Tmnt"))
-		
-
 #################################################### 4- Stand counts & Phenology #############################################	
-## do not do this and then subset
-#	d <- r
-# rather start a new data.frame
-   d <- data.frame(
-		trial_id = r$`Trial Code`, 
-		treatment = r$Tmnt,
-		crop=tolower(r$Crop), variety= r$Variety, 
-		season=r$Season, 
-		biomass_total = as.numeric(r$`Biomass (t/ha)`) * 1000,
-		residue_yield = as.numeric(r$`Straw yield (t/ha)`) * 1000,
-		yield = as.numeric(r$`Grain yield (t/ha)`) * 1000)
 
-	d$planting_date <- as.character(as.Date(as.numeric(r$`Date of seeding (dd-mm-yy)`), origin = "1899-12-30"))
-	d$harvest_date <- as.character(as.Date(as.numeric(r$`Datw of harvest (dd-mm-yy)`), origin = "1899-12-30"))
+	process_data <- function(r) {
 
-#	emergence= r$`100% emergence (DAS)`,
-#	flowering=r$`50% anthesis (DAS)`
-#	maturity=r$`80% physiological maturity (DAS)`,
-#	harvest=r$`Harvesting (DAS)`
+		## do not do this and then subset
+		#	d <- r
+		# rather start a new data.frame
+	   d <- data.frame(
+			trial_id = as.character(r$Trial.Code), 
+			treatment = r$Tmnt,
+			crop=tolower(r$Crop), variety= r$Variety, 
+			season=r$Season, 
+			biomass_total = r$Biomass.t.ha * 1000,
+			residue_yield = r$Straw.yield.t.ha * 1000,
+			yield = r$Grain.yield.t.ha * 1000,
+			emergence= r$X100.emergence.DAS,
+			flowering= r$X50.anthesis.DAS,
+			maturity = r$X80.physiological.maturity.DAS,
+			dataset_id = dataset_id,
+			N_fertilizer = r$N.kg.ha,
+			P_fertilizer = r$P2O5.kg.ha / 2.29,
+			K_fertilizer = r$K2O.kg.ha / 1.2051,
+			B_fertilizer = r$Boric.acid.kg.ha * 0.1748,
+			S_fertilizer = 0,
+			Zn_fertilizer = 0,
+			lime = 0,
+			gypsum = 0
+		)
 
-#### about the data #####
-## (TRUE/FALSE)
+		i <- grep("Date.of.seeding", names(r))
+		d$planting_date = as.character(as.Date(r[,i]))
+		i <- grep("Dat..of.harvest", names(r))
+		d$harvest_date = as.character(as.Date(r[,i]))			
 
-	d$dataset_id <- dataset_id
-	d$on_farm <- TRUE
-	d$is_survey <- FALSE 
-	d$irrigated <- TRUE
-    d$inoculated <- FALSE
-	d$yield_part <- "grain"
+		d$on_farm <- TRUE
+		d$is_survey <- FALSE 
+		d$irrigated <- TRUE
+		d$inoculated <- FALSE
+		d$yield_part <- "grain"
 
-##### Location #####
-	d$country <- "India"
-	d$adm1 <- "West Bengal"
-	d$adm2 <- "Malda"
-	d$location <- r$Node
-	d$longitude [d$location =="Mahadipur"] <- 88.1265
-	d$latitude [d$location =="Mahadipur"] <- 24.8501 
-	d$longitude [d$location =="Bidyanandapur"] <- 87.9903
-	d$latitude [d$location =="Bidyanandapur"] <- 25.9517
-	d$longitude [d$location =="Urgitola"] <- 88.1411
-	d$latitude [d$location =="Urgitola"] <- 25.0108
+		d$country <- "India"
+		d$adm1 <- "West Bengal"
+		d$adm2 <- "Malda"
+		d$location <- r$Node
+		
+	##### Fertilizer ########
+		frp <- apply(r[, grep("Product.used", names(r), value=TRUE)], 1, 
+			function(i) paste(unique(i[!is.na(i)]), collapse="; "))
+		
+		# the excel file has encoded 10:26:26 as "time"!
+		frp <- gsub("1899-12-31 10:26:26", "NPK", frp)
+		frp <- gsub("UREA", "urea", frp)
+		frp <- gsub("Urea", "urea", frp)
+		frp <- gsub("MoP", "KCl", frp)
+		frp <- gsub("Boron 20%", "Borax", frp) #?
+		frp <- gsub("Borax 21", "Borax", frp)
+		frp <- gsub("Boron", "Borax", frp) #?
+		frp[frp == ""] <- NA
+		d$fertilizer_type <- frp
+		d
+	}
+
+
+	fun <- function(f) {
+		#print(basename(f)); flush.console()
+		r <- get_raw_data(f)
+		d <- process_data(r)
+
+		d$longitude <- NA
+		d$latitude <- NA
+		d$longitude [d$location =="Mahadipur"] <- 88.1265
+		d$latitude [d$location =="Mahadipur"] <- 24.8501 
+		d$longitude [d$location =="Bidyanandapur"] <- 87.9903
+		d$latitude [d$location =="Bidyanandapur"] <- 25.9517
+		d$longitude [d$location =="Urgitola"] <- 88.1411
+		d$latitude [d$location =="Urgitola"] <- 25.0108
+
+		d
+	}
 	
-##### Fertilizer ########
-	pnms <- grep("Product used", names(fr), value=TRUE)
-	frp <- cbind(fr[, c("Site No.", "Tmnt")], value=apply(fr[, pnms], 1, 
-		function(i) paste(unique(i[!is.na(i)]), collapse="; "))
-	)
-	# the excel file has encoded 10:26:26 as "time"!
-	frp$value <- gsub("0.43502314814814813", "NPK", frp$value)
-	frp$value <- gsub("UREA", "urea", frp$value)
-	frp$value <- gsub("Borax 21", "Borax", frp$value)
+	dd <- lapply(ff, fun)
+	dd <- do.call(rbind, dd)
+	dd$crop <- gsub("kidneybean", "kidney bean", dd$crop)
 
-	d$fertilizer_type <- frp$value
-	d$N_fertilizer <- as.numeric(r$`Fertilizer 5 Application_N  (kg/ha)`)
-	d$P_fertilizer <- as.numeric(r$`Fertilizer 5 Application_P2O5 (kg/ha)`) / 2.29
-	d$K_fertilizer <- as.numeric(r$`Fertilizer 5 Application_K2O (kg/ha)`) / 1.2051
-	d$S_fertilizer <- 0
-	d$Zn_fertilizer <- 0
-	d$B_fertilizer <- as.numeric(r$`Fertilizer 5 Application_Boric acid (kg/ha)`) * 0.1748
-	d$lime <- 0
-	d$gypsum <- 0
-
-# all scripts must end like this
-	carobiner::write_files(dset, d, path=path)
+	carobiner::write_files(dset, dd, path=path)
 }
 
