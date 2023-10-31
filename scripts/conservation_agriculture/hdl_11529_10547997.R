a20f2f42bf39c880493eac976c6ce8e53eebb274
rhijmans <r.hijmans@gmail.com>
2023-10-31 16:30:40 +0100

    for #216

--- scripts/conservation_agriculture/hdl_11529_10547997.R
+++ scripts/conservation_agriculture/hdl_11529_10547997.R
@@ -2,7 +2,7 @@
 
 # ## ISSUES 
 
-#RH: this is 
+#RH: perhaps we should capture more managment variables 
 
 
 # ....
@@ -24,8 +24,6 @@ Farmer participatory on-farm trials with CA technologies comparing with farmersâ
     project="Rabi (winter) crops-all nodes-Alternative cropping systems trial-Sunsari-Nepal",
     uri=uri,
     data_citation= "Gathala, Mahesh K. (CIMMYT) - ORCID: 0000-0001-8282-2953, Tiwari, Thakur P. (CIMMYT), Islam, Saiful (CIMMYT) - ORCID: 0000-0002-6482-5031, Shrestha, Renuka (Nepal Agricultural Research Council), Shrestha, H.K. (Nepal Agricultural Research Council), Manandhar, S. (Nepal Agricultural Research Council) - ORCID: 0000-0002-6353-3539, Shrestha, Shukra Raj (Nepal Agricultural Research Council).",
-    ## if there is a paper, include the paper's doi here
-    ## also add a RIS file in references folder (with matching doi)
     publication= NA,
     data_institutions = "CIMMYT",
     data_type="on-farm experiment",
@@ -35,50 +33,67 @@ Farmer participatory on-farm trials with CA technologies comparing with farmersâ
   
   ## download and read data 
   
-  ff  <- carobiner::get_data(uri, path, group)
-  js <- carobiner::get_metadata(dataset_id, path, group, major=1, minor=1)
+	ff  <- carobiner::get_data(uri, path, group)
+	js <- carobiner::get_metadata(dataset_id, path, group, major=1, minor=3)
  # dset$license <- "not specified" #carobiner::get_license(js)
-  dset$license <- carobiner::get_license(js)
-  
-  
-  f <- ff[basename(ff) == "Maize-Rabi 2016-17-ACS-Bhokraha-Sunsari.xlsx"]
-  
-  
-  # Select sheet with yields from the excel file 
-  r <- carobiner::read.excel(f, sheet = "14 - Grain Harvest ",skip=4)
-  
+	dset$license <- carobiner::get_license(js)
+    
+	get_data <- function(f) {
+		r1 <- carobiner::read.excel.hdr(f, sheet ="4- Stand counts & Phenology", skip=4, hdr=2)
+		r2 <- carobiner::read.excel.hdr(f, sheet ="14 - Grain Harvest ", skip=4, hdr=2)
+		r3 <- carobiner::read.excel.hdr(f, sheet ="6 - Fertilizer amounts ", skip=4, hdr=2)
+
+		nms <- c("Site.No", "Tmnt", "Grain.yield.t.ha", "TGW.g", "Biomass.t.ha", "Straw.yield.t.ha")
+		r2 <- r2[, nms]
+		nms <- c("Site.No", "Tmnt", "N.kg.ha", "P2O5.kg.ha", "K2O.kg.ha", "Gypsum.kg.ha", "ZnSO4.kg.ha", "Boric.acid.kg.ha", grep("Product.used", names(r3), value=TRUE))
+		r3 <- r3[, nms]
+
+		r <- merge(r1, r2, by=c("Site.No", "Tmnt"))
+		merge(r, r3, by=c("Site.No", "Tmnt"))
+	}
+
+	f <- ff[basename(ff) == "Maize-Rabi 2016-17-ACS-Bhokraha-Sunsari.xlsx"]
+	r <- get_data(f)	
+	
   #### about the data #####
-  ## (TRUE/FALSE)
   
-  d <- data.frame(date=r$"...1",trial_id = r$"...3",season=r$"...2",treatment=r$"...9", yield = r$`Grain yield (t/ha)`*1000,
-                  biomass_total=r$`Biomass (t/ha)`*1000)
+	d <- data.frame(trial_id = as.character(r$Trial.Code), season = r$Season,
+			crop=tolower(r$Crop), variety= r$Variety, 
+			treatment = r$Tmnt, 
+			yield = r$Grain.yield.t.ha * 1000,  
+			biomass_total = r$Biomass.t.ha * 1000,
+			residue_yield = r$Straw.yield.t.ha * 1000,
+			N_fertilizer = r$N.kg.ha,
+			P_fertilizer = r$P2O5.kg.ha / 2.29,
+			K_fertilizer = r$K2O.kg.ha / 1.2051,
+			B_fertilizer = r$Boric.acid.kg.ha * 0.1748,
+			planting_date = as.character(as.Date(r$Date.of.seeding.dd.mm.yy)),
+			harvest_date = as.character(as.Date(r$Datw.of.harvest.dd.mm.yy)),			
+			flowering_date = as.character(as.Date(r$Date.of.50.anthesis.dd.mm.yy)),
+			site = paste("site ", r$Site.No),
+			row_spacing =r$Row.spacing.cm,
+			country= "Nepal",
+			dataset_id=dataset_id,
+			S_fertilizer= 0,
+			Zn_fertilizer= 0,
+			lime =0, gypsum =0
+		)
+			
   
   # for first dataset
-  d$dataset_id <- dataset_id
+	d$yield_part <- "grain"
+	d$irrigated <- TRUE
+	d$adm2 <- "Sunsari" # district provided in the excel
+	d$location <- "Bhokraha" # community provided in the excel
+	d$crop <- "maize"
+	d$latitude <- 26.5920003 # https://www.gps-coordinates.net/
+	d$longitude <- 87.1025479
+  	d$fertilizer_type <- apply(r[, grep("Product.used", names(r), value=TRUE)], 1, 
+		function(i) {
+			i <- gsub("Urea.*", "urea", i[!is.na(i)])
+			i <- gsub("MOP", "KCl", i[!is.na(i)])
+			paste(unique(i[!is.na(i)]), collapse="; ")
+		})
 
-  
-  ##### Location #####
-  d$country <- "Nepal"
-  d$adm2 <- "Sunsari" # district provided in the excel
-  d$site <- "Bhokraha" # community provided in the excel
-  d$crop <- "maize"
-  d$N_fertilizer <- 114
-  d$P_fertilizer <- 60/2.29
-  d$K_fertilizer <- 40/1.2051
-  d$harvest <- 151
-  d$emergence <- 9
-  d$harvest_date  <- as.Date("06/29/17", format = "%m/%d/%y")
-  d$variety <-  "TX369"
-  d$row_spacing <- 50
-  d$fertilizer_type <- "Diammonium phosphate"
-  
-  
-  d$latitude <- 26.5920003 # https://www.gps-coordinates.net/
-  d$longitude <- 87.1025479
-  d$yield_part <- "grain"
-  
-  # all scripts must end like this
-  carobiner::write_files(dset, d, path=path)
-  
-  
+	carobiner::write_files(dset, d, path=path)
 }
