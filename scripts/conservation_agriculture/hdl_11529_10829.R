de45a267793f28019bead9dc74920930e21763fc
rhijmans <r.hijmans@gmail.com>
2023-11-03 12:24:18 +0100

    for #220

--- scripts/conservation_agriculture/hdl_11529_10829.R
+++ scripts/conservation_agriculture/hdl_11529_10829.R
@@ -57,43 +57,151 @@ The data set presents yields for maize and the legumes from these sites over 10 
 	
 	# efyrouwa: .xlsx files can be read by carobiner::read.excel() 
 	f <- ff[basename(ff) == "Summary files Malawi 2005-15..xlsx"]
-  r <- carobiner::read.excel(f, sheet = 1)
-  r1 <- carobiner::read.excel(f, sheet = 2)
-  r2 <- carobiner::read.excel(f, sheet = 3)	
+	
+	# to avoid warnings
+	ctp <- c('numeric', 'numeric', 'text', 'text', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'text', 'numeric', 'text', 'text', 'text', 'numeric', 'numeric', 'numeric') 
+	r1 <- carobiner::read.excel(f, sheet = "Maize working data", col_types=ctp)
+	r2 <- carobiner::read.excel(f, sheet = "Legume yields")
+	r3 <- carobiner::read.excel(f, sheet = "Intercropped legume yields")	
   
   ## process file(s)
+	# it is confusing to use "d, d1, d2". Instead use "d0, d1, d2"
   
-  ## use a subset
-	d <- carobiner::change_names(r, c("Harvest Year","District","Village","Site/area","crop grown","Tmnt.","Stalk yield (kg/ha)","Grain yield (kg/ha)"),c("harvest_date","adm1","site","rep","crop","treatment","residue_yield","yield"))
-	d$crop <- tolower(d$crop)
+  ## use a subset (like this to avoid subsetting later)
+	d1 <- r1[, c("Harvest Year", "District", "Village", "Plot No.", "crop grown", "Tmnt.", "Stalk yield (kg/ha)", "Grain yield (kg/ha)", "Farmer")]
+	colnames(d1) <- c("harvest_date", "adm1", "location", "rep", "crop", "treatment", "residue_yield", "yield", "fname")
+
+	d2 <- r2[,  c("Site", "Tmnt.", "Crop grown", "Final stand (pl/ha)", "Grain yield (kg/ha)", "total Biomass yield (kg/ha)",  "Year", "Site name", "Plot No.")]
+	colnames(d2) <- c("adm1", "treatment", "crop", "plant_density", "yield", "biomass_total", "harvest_date", "fname", "rep")
+
+	# third data set
+	d3 <- r3[, c("Year", "Site name", "Tmnt.", "Crop grown", "Final stand (pl/ha)", "total Biomass yield (kg/ha)", "Grain yield (kg/ha)", "Farmer")]
+	colnames(d3) <- c("harvest_date", "location", "treatment", "crop", "plant_density", "biomass_total", "yield", "fname")
+
+
+	## ? please comment on why you do this
+	d1$location <- carobiner::replace_values(d1$location, 
+		c("Champhira", "Lemu", "Matandika"), c("Champhila", "Balaka", "Machinga"))
+	  
+	geo1 <- data.frame( 
+        location = c("Malula", "Enyezini", "Chisepo", "Chipeni", "Zidyana", "Mwansambo","Balaka",
+					"Machinga", "Linga", "Herbert", "Songani", "Chinguluwe", "Champhila", "Kaluluma"), 
+        latitude = c(-14.96656, -11.46457, -13.63281, -13.81782, -13.21417, -13.6966, -14.97928, 
+					-15.06665, -13.06345, -13.5396, -15.3, -13.81066, -12.40513, -12.58075), 
+        longitude = c(34.99189, 33.86478, 33.47223, 33.38493, 34.31717, 33.55553, 34.95575, 35.22543, 
+					33.43611, 33.02705, 35.48333, 33.49862, 33.64337, 33.51833))
+	
+	d1 <- merge(d1, geo1, by ="location", all.x = TRUE)
+	
+	# second data set
+	d2$adm1 <- carobiner::replace_values(d2$adm1, 
+			c("Champhira", "Lemu", "Matandika"), c("Champhila", "Balaka", "Machinga"))
+
+	geo2 <- data.frame( 
+			adm1 = c("Malula", "Zidyana", "Chipeni", "Mwansambo", "Balaka", "Herbert", 
+	         "Kaluluma", "Champhila", "Chinguluwe", "Machinga", "Songani", "Linga"), 
+	    latitude = c(-14.96656, -13.21417, -13.81782, -13.6966, -14.97928, -13.5396, 
+	         -12.58075, -12.40513, -13.81066, -15.06665, -15.3, -13.06345), 
+	    longitude = c(34.99189, 34.31717, 33.38493, 33.55553, 34.95575, 33.02705, 
+	         33.51833, 33.64337, 33.49862, 35.22543, 35.48333, 33.43611))
+	
+	d2 <- merge(d2, geo2,  by="adm1",  all.x = TRUE)
+
+
+## need to georeference d3 (perhaps using data from d2 or d1
+# geo3 = 
+#joining tables
+	d <- carobiner::bindr(d1,  d2,  d3)
+	d$dataset_id <- dataset_id
+	d$harvest_date <- as.character(d$harvest_date)
+	d$yield_part <- "grain"
 	d$country <- "Malawi"
-	d$intercrops <- NA
-	d$intercrops[d$`sole/intercrop`==1 & d$treatment %in% c("CA +Maize/Pp", "CA+maize/Pp", "Maizepp")] <- "pigeon pea"
-	d$intercrops[d$`sole/intercrop`==1 & d$treatment %in% c("CA +Maize/mucuna")] <- "velvet bean"
-	d$intercrops[d$`sole/intercrop`==1 & d$treatment %in% c("CA+Maize/Cp")] <- "cowpea"
-	t <- d$treatment
-	t <- carobiner::fix_name(t, case = "lower")
-	t <- gsub("/pp","_with_peagon_pea",t)
-	t <- gsub("pp","_with_peagon_pea",t)
-	t <- gsub("/mucuna","_with_velvet_bean",t)
-	t <- gsub("/cp","_with_cowpea",t)
-	t <- gsub("ca","conservation_agriculture_",t)
-	t <- gsub("\\+\\s?mz","with_maize",t)
-	t <- gsub("\\s?\\+\\s?maize","with_maize",t)
-	t <- gsub(" ","_",t)
-	t <- gsub("\\s?\\/leg","_with_legume",t)
-	t <- gsub("_leg_","_legume_",t)
-	t <- gsub("_rot","_rotation",t)
-	t <- gsub("conventional_controls","conventional_control",t)
-  #efyrouwa: what is treatment ds? and sb
-	d$treatment <- t
 	d$on_farm <- TRUE 
 	d$is_survey <- FALSE
-	d$site <- carobiner::replace_values(d$site,c("Champhira","Lemu","Matandika"),c("Champhila","Balaka","Machinga"))
-  l <- unique(d[c("country", "site")])
-  
+
+
+	cleanname <- function(x) {
+		f <- tolower(x)
+		f[f=="nasoweka h."] <- "nasoweka" 
+		f = gsub("\\.", " ", f)
+		f = gsub("  ", " ", f)
+		f = gsub("(mrs)", "", f)
+		f = gsub("kamfamveka", "kafamveka", f)
+		sapply(strsplit(f, " "), \(i) i[length(i)])
+	}
+	d$fname <- cleanname(d$fname)
+	d$trial_id <- as.character(as.integer(as.factor(paste0(d$harvest_date, d$fname))))
+	d$fname <- NULL
+	d$rep <- as.integer(d$rep)
+	
+	i <- which(is.na(d$location))
+
+	d$crop <- carobiner::fix_name(d$crop,  case = "lower")
+	d$crop <- carobiner::replace_values(d$crop, "soya", "soybean")
+	d$crop[d$crop %in% c("pigeonpea", "p/peas")] <- "pigeon pea"
+	d$crop[grep("nuts", d$crop)] <- "groundnut"
+	d$crop[grep("cowpe", d$crop)] <- "cowpea"
+
+	tr <- carobiner::fix_name(d$treatment, case = "lower")
+	tr <- gsub("\\+", "_", tr)
+	tr <- gsub(" ", "_", tr)
+	tr <- gsub("__", "_", tr)
+	tr <- gsub("/", "_", tr)
+	tr <- gsub("variety_", "variety-", tr)
+	tr <- gsub("ppea", "pigeonpea", tr)
+	tr <- gsub("pp", "pigeonpea", tr)
+	tr <- gsub("p_peas", "pigeonpea", tr)
+	tr <- gsub("groudnnuts", "groundnuts", tr)
+	tr <- gsub("gnuts", "groundnuts", tr)
+	tr <- gsub("g_nuts", "groundnuts", tr)
+	tr <- gsub("mucuna", "velvetbean", tr)
+	tr <- gsub("cp", "cowpea", tr)
+	tr <- gsub("sb", "soybean", tr)
+	tr <- gsub("mz", "maize", tr)
+	tr <- gsub("leg_", "legume_", tr)
+	tr <- gsub("leg$", "legume", tr)
+	tr <- gsub("ca", "CA", tr)
+	tr <- gsub("_rot$", "_rotation", tr)
+	tr <- gsub("maizepig", "maize_pig", tr)
+	tr <- gsub("maizesole", "maize_sole", tr)
+	tr <- gsub("conventional_controls", "conventional_control", tr)
+	tr <- gsub("__", "_", tr)
+	tr <- gsub("1", "-1", tr)
+	tr <- gsub("2", "-2", tr)
+	tr <- gsub("--1", "-1", tr)
+	tr <- gsub("--2", "-2", tr)
+	tr <- gsub("CA-", "CA", tr)
+	d$treatment <- tr
+	##unique(tr)
+	## what are these?
+	##"ds_maize" "ds_intercrop" "sc_719", "zm523", "mh_30", "pan53", "dkc5053")
+ 
+
+	d$intercrops <- NA
+	i <- d$crop != "maize"
+	d$intercrops[i & grepl("maize", d$treatment)] <- "maize"
+	i <- d$crop == "maize"
+	d$intercrops[i & grepl("pigeon", d$treatment)] <- "pigeon pea"
+	d$intercrops[i & grepl("velvet", d$treatment)] <- "velvet bean"
+	d$intercrops[i & grepl("cowpea", d$treatment)] <- "cowpea"
+	d$intercrops[i & grepl("soybean", d$treatment)] <- "soybean"
+	d$intercrops[i & grepl("legume", d$treatment)] <- "legume"
+
+	d$planting_date <- as.character(NA)
+	
+	d <- d[!is.na(d$yield), ]
+	# all scripts must end like this
+	carobiner::write_files(dset,  d,  path=path)
+}
+
+
+
+
+
   # efyrouwa: finding the coordinates, this part should not be in the script or be commented out
-  # for (i in 1:nrow(l)) {
+  ##RH  this loop should not be necessary; should be done by carobiner
+  # 	loc <- unique(d[c("country", "site")])
+  #  for (i in 1:nrow(l)) {
   #     ll <- carobiner::geocode(country = l$country[i],location = l$site[i], service = "geonames", username = "efyrouwa")
   #     ii <- unlist(jsonlite::fromJSON(ll))
   #     c <- as.integer(ii["totalResultsCount"][[1]])
@@ -102,90 +210,4 @@ The data set presents yields for maize and the legumes from these sites over 10 
   #   }
   #  code below will print the cordinates then copy paste to make make a data frame
   #   sss <- dput(l) 
-   
-	ll <- data.frame(country = c("Malawi", "Malawi", "Malawi", "Malawi","Malawi", "Malawi", "Malawi",
-	                             "Malawi", "Malawi", "Malawi", "Malawi","Malawi", "Malawi", "Malawi"), 
-	                 site = c("Malula", "Enyezini", "Chisepo", "Chipeni", "Zidyana", "Mwansambo","Balaka", 
-	                          "Machinga", "Linga", "Herbert", "Songani", "Chinguluwe", "Champhila", "Kaluluma"), 
-	                 latitude = c(-14.96656, -11.46457, -13.63281, -13.81782, -13.21417, -13.6966, -14.97928, 
-	                              -15.06665, -13.06345, -13.5396, -15.3, -13.81066, -12.40513, -12.58075), 
-	                 longitude = c(34.99189, 33.86478, 33.47223, 33.38493, 34.31717, 33.55553, 34.95575, 35.22543, 
-	                               33.43611, 33.02705, 35.48333, 33.49862, 33.64337, 33.51833))
-	
-	d <- merge(d,ll, by = c("country", "site"), all.x = TRUE)
-	d$yield_part <- "grain"
-	
-	d <- d[,c("country","adm1","site","latitude","longitude","treatment","harvest_date","crop","yield_part","intercrops","on_farm","is_survey","residue_yield","yield")]
-	
-	# second data set
-	d1 <- carobiner::change_names(r1,c("Site","Tmnt.","Crop grown","Final stand (pl/ha)","Grain yield (kg/ha)","total Biomass yield (kg/ha)", "Year" ),c("adm1","treatment","crop","plant_density","yield","biomass_total","harvest_date"))
-	d1$country <- "Malawi"
-	d1$crop <- carobiner::fix_name(d1$crop, case = "lower")
-	d1$crop <- carobiner::replace_values(d1$crop,c("soya","cowpeas"),c("soybean","cowpea"))
-	d1$crop[d1$crop %in% c("pigeonpea","p/peas")] <- "pigeon pea"
-	d1$crop[d1$crop %in% c("groundnuts","gnuts","g/nuts")] <- "groundnut"
-	t <- d1$treatment
-	t <- carobiner::fix_name(t, case = "lower")
-	t <- gsub("ca", "conservation_agriculture",t)
-	t <- gsub("\\s?\\+\\s?legume","_with_legume",t)
-	t <- gsub(" ","_",t)
-	t <- gsub("\\s?\\+\\s?g/nuts","with_groundnut",t)
-	t <- gsub("\\gnuts","with_groundnut",t)
-	t <- gsub("\\s?\\+groundnuts","with_groundnut",t)
-	t <- gsub("\\+groundnnuts","_with_groundnut",t)
-	t <- gsub("\\+|-mz", "_with_maize",t)
-	t <- gsub("\\+|?p/peas","_with_pigeonpea",t)
-	t <- gsub("\\+p/pea","_with_pigeonpea",t)
-	t <- gsub("\\+ppea","_with_pigeonpea",t)
-	t <- gsub("\\ppea","_with_pigeonpea",t)
-	t <- gsub("groudnnuts","_with_groundnut",t)
-	d1$treatment <- t
-	d1$on_farm <- TRUE 
-	d1$is_survey <- FALSE
-	d1$adm1 <- carobiner::replace_values(d1$adm1,c("Champhira","Lemu","Matandika"),c("Champhila","Balaka","Machinga"))
-	l <- unique(d1[,c("country","adm1")])
-	
-	# efyrouwa: lat and lon for the second dataset
-	# follow process above
-
-	ll <- data.frame(country = c("Malawi", "Malawi", "Malawi", "Malawi","Malawi", "Malawi", "Malawi",
-	                             "Malawi", "Malawi", "Malawi", "Malawi","Malawi"),
-	                  adm1 = c("Malula", "Zidyana", "Chipeni", "Mwansambo","Balaka", "Herbert", 
-	                           "Kaluluma", "Champhila", "Chinguluwe", "Machinga","Songani", "Linga"), 
-	                  latitude = c(-14.96656, -13.21417, -13.81782, -13.6966, -14.97928, -13.5396, 
-	                               -12.58075, -12.40513, -13.81066, -15.06665, -15.3, -13.06345), 
-	                  longitude = c(34.99189, 34.31717, 33.38493, 33.55553, 34.95575, 33.02705, 
-	                                33.51833, 33.64337, 33.49862, 35.22543, 35.48333, 33.43611))
-	
-	d1 <- merge(d1,ll, by=c("country", "adm1"), all.x = TRUE)
-	d1$yield_part <- "seed"
-  d1 <- d1[, c("country","adm1","harvest_date","treatment","crop","yield_part","plant_density","biomass_total",
-                "yield", "on_farm","is_survey","latitude","longitude")]
-
-	# third data set
-	d2 <- carobiner::change_names(r2, c("Year","Site name","Tmnt.","Crop grown","Final stand (pl/ha)","total Biomass yield (kg/ha)","Grain yield (kg/ha)"),
-	                                  c("harvest_date","site","treatment","crop","plant_density","biomass_total","yield"))
-	d2$crop[d2$crop %in% c("P/peas","p/peas","Pigeonpea")] <- "pigeon pea"
-	d2$crop[d2$crop %in% c("cowpeas","cowpes")] <- "cowpea"
-	d2$treatment <- carobiner::replace_values(d2$treatment,"Legume intercrop","legume_intercrop")
-	d2$country <- "Malawi"
-	d2$on_farm <- TRUE 
-	d2$is_survey <- FALSE
-	d2$yield_part <- "seed"
-	d2 <- d2[,c("country","site","harvest_date","crop","treatment","biomass_total","plant_density","yield","yield_part","on_farm","is_survey")]
-	
-	#joining tables
-	d5 <- carobiner::bindr(d, d1, d2)
-	d5$dataset_id <- dataset_id
-	d5$trial_id <- paste(1:nrow(d5),d5$treatment, sep = "_")
-	d5$harvest_date <- as.character(d5$harvest_date)
-	
-	d5 <- d5[, c("country","adm1","site","latitude","longitude","treatment","harvest_date","crop","yield_part","intercrops","on_farm","is_survey","residue_yield","yield","plant_density","biomass_total","dataset_id","trial_id")]
-
-	
-	# all scripts must end like this
-	carobiner::write_files(dset, d5, path=path)
-}
-
-
 
