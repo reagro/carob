fdf3b08e49c339996121762168fdebf37f505dd6
rhijmans <r.hijmans@gmail.com>
2024-02-06 10:52:32 +0100

    m

--- scripts/wheat_trials/hdl_11529_10548290.R
+++ scripts/wheat_trials/hdl_11529_10548290.R
@@ -20,7 +20,7 @@ carob_script <- function(path) {
 		project="Global_Wheat_Program",
 		uri=uri,
 		data_citation="Global Wheat Program; IWIN Collaborators; Ammar, Karim; Payne, Thomas, 2020. 39th International Durum Yield Nursery, https://hdl.handle.net/11529/10548291, CIMMYT Research Data & Software Repository Network, V1",
-		publication="",
+		publication=NA,
 		data_institutions = "CIMMYT",
    		data_type="experiment", 
 		carob_contributor="Blessing Dzuda",
@@ -40,6 +40,7 @@ carob_script <- function(path) {
 	
 	#removing out of bound value
 	d$soil_pH[d$soil_pH == 24] <- NA
+	d$soil_pH[d$soil_pH == 0] <- NA
 
 	carobiner::write_files(dset, d, path=path)
 }
