6100e56e396f23bc352fea0a20fffd351f2d76cb
rhijmans <r.hijmans@gmail.com>
2024-02-08 10:29:33 +0100

    f

--- scripts/fertilizer/hdl_20.500.11766.1_FK2_LYKEFM.R
+++ scripts/fertilizer/hdl_20.500.11766.1_FK2_LYKEFM.R
@@ -4,14 +4,11 @@
 ## ISSUES
 # ....
 
-## should also be written to fertilizer
-
 
 carob_script <- function(path) {
  " 
 Description:
-Final dataset from agronomic experiment in Gumara Maksegnit (2016), as elaborated by GARC researcher in charge for this trial (Baye Ayalew). 
-Please contact author and contact person at ICARDA to obtain more detailed metadata or to propose collaboration.
+Final dataset from agronomic experiment in Gumara Maksegnit (2016), as elaborated by GARC researcher in charge for this trial (Baye Ayalew). Please contact author and contact person at ICARDA to obtain more detailed metadata or to propose collaboration.
 
 "
    
@@ -35,66 +32,77 @@ Please contact author and contact person at ICARDA to obtain more detailed metad
    )
    
    ## download and read data 
-   ff  <- carobiner::get_data(uri, path, group)
-   js <- carobiner::get_metadata(dataset_id, path, group, major=2, minor=0)
-   dset$license <- carobiner::get_license(js)
-  dset$title <- carobiner::get_title(js)
+	ff  <- carobiner::get_data(uri, path, group)
+	js <- carobiner::get_metadata(dataset_id, path, group, major=2, minor=0)
+	dset$license <- carobiner::get_license(js)
+	dset$title <- carobiner::get_title(js)
 	dset$authors <- carobiner::get_authors(js)
 	dset$description <- carobiner::get_description(js)
    
-   bn <- basename(ff)
-   ## process file(s)
+  ## process file(s)
     
-    r <- read.csv(ff[bn=="Mandie_s_Farm.csv"],sep = ";")
-    r1 <- read.csv(ff[bn=="Worku_s_Farm.csv"],sep = ";") 
-    newname<- c("year","treatment","rep","plant_height","dmy_total","yield")
-    oldname<- c("Year","Treatment","Replicate","Plant_Height","Biomass","Yield")
-    d1<- carobiner::change_names(r[,c("Year","Treatment","Replicate","Plant_Height","Biomass","Yield")],oldname,newname)
-    d2<- carobiner::change_names(r1[,c("Year","Treatment","Replicate","Plant_Height","Biomass","Yield")],oldname,newname)
+    r1 <- read.csv(ff[basename(ff)=="Mandie_s_Farm.csv"],sep = ";")
+    r2 <- read.csv(ff[basename(ff)=="Worku_s_Farm.csv"],sep = ";") 
+
+    newname <- c("year", "treatment", "rep", "plant_height", "dmy_total", "yield")
+    oldname <- c("Year", "Treatment", "Replicate", "Plant_Height", "Biomass", "Yield")
+    d1 <- carobiner::change_names(r1[, oldname], oldname, newname)
+    d2 <- carobiner::change_names(r2[, oldname], oldname, newname)
     d1$site <- "Mandie"
     d2$site <- "Worku"
-    
+    d1$dmy_total <- d1$dmy_total / 1000
+	
     # append d1 and d2
-    d<- carobiner::bindr(d1,d2)
-    d$planting_date[d$year==1]<- "2013-06-03"
-    d$harvest_date[d$year==1]<- "2013-10-25"
+    d <- rbind(d1, d2)
     
     ## process the treatment file 
-    r2<- read.csv(ff[bn=="Treatment.csv"],sep = ";") 
-    r2$Treatment1<- paste("planting_tillering_Booting (",r2$N_Planting,r2$N_Tillering,r2$N_Booting,")",sep = "_")
-    d3<- r2[,c("Treatment","Nitrogen_Rate","Treatment1")]
-    colnames(d3)<- c("treatment","N_fertilizer","Treatment1")
-    d3<- d3[!is.na(d3$Treatment),]
+    r3 <- read.csv(ff[basename(ff)=="Treatment.csv"], sep = ";") 
+	r3 <- r3[!is.na(r3$Treatment), ]
+	
+	r3$N_splits <- rowSums(r3[, c("N_Planting", "N_Tillering", "N_Booting")] > 0) - 1
+	r3$N_splits <- as.integer(pmax(0, r3$N_splits))
+    r3$Treatment1 <- paste("plant_till_boot", r3$N_Planting, r3$N_Tillering, r3$N_Booting, sep = "_")
+
+    d3 <- r3[,c("Treatment", "Nitrogen_Rate", "Treatment1", "N_splits")]
+    colnames(d3) <- c("treatment", "N_fertilizer", "Treatment1", "N_splits")
     
     # merge d3 with d
-    d<- merge(d,d3,by="treatment",all.x = TRUE)
-    d$treatment<- d$Treatment1
-    d$Treatment1<- d$year <- NULL
+    d <- merge(d, d3, by="treatment", all.x = TRUE)
+    d$treatment <- d$Treatment1
+    d$Treatment1 <- d$year <- NULL
     #add columns
-   d$crop<- "wheat"
-   d$dataset_id <- dataset_id
-   d$country <- "Ethiopia"
-   d$location<- "Gumara Maksegnit"
-   d$on_farm <- TRUE
-   d$is_survey <- FALSE
-   d$irrigated <- FALSE
-   d$inoculated <- FALSE
-   d$yield_part <- "grain" 
-   d$trial_id <- paste(d$rep,d$location,sep = "_")
+	d$crop <- "wheat"
+	d$dataset_id <- dataset_id
+	d$country <- "Ethiopia"
+	d$location <- "Gumara Maksegnit"
+	d$on_farm <- TRUE
+	d$is_survey <- FALSE
+	d$irrigated <- FALSE
+	d$inoculated <- FALSE
+	d$yield_part <- "grain" 
+	d$trial_id <- paste(d$rep,d$location,sep = "_")
    ## add lon and lat
    # EGB:
    # EPSG:32637 (https://epsg.io/map#srs=32637&x=349106&y=1373852&z=5&layer=streets)
-   d$longitude<- 38.056
-   d$latitude <- 10.401
-   d$longitude[d$site == "Mandie"] <- 37.583
-   d$latitude[d$site == "Mandie"] <- 12.421
+	d$longitude <- 38.056
+	d$latitude <- 10.401
+	d$longitude[d$site == "Mandie"] <- 37.583
+	d$latitude[d$site == "Mandie"] <- 12.421
+
+    d$planting_date <- "2013-06-03"
+    d$harvest_date <- "2013-10-25"
+
+# assuming
+	d$P_fertilizer <- 0
+	d$K_fertilizer <- 0
    
    ##  fix dmy_total range
-   #d$dmy_total[d$dmy_total>20000]<- NA
+   #d$dmy_total[d$dmy_total>20000] <- NA
    ## CN 
    # the plant_height values are lower than the valmin in record.csv (10cm) which doesn't make sense. The unit is given in cm, but I suppose this is probably an error.Should we remove the variable ?
    
-   d$plant_height<-d$plant_height*10 # assuming it was in dm and convert into cm
+   # assuming it was in dm and convert into cm
+   d$plant_height <- d$plant_height * 10
    # all scripts must end like this
    carobiner::write_files(dset, d, path=path)	
 }
