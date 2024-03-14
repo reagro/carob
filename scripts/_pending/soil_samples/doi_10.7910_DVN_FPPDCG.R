2f581136be454dd4056ce6976b75315c16a14cb6
asila <musilisila@gmail.com>
2024-03-14 14:14:16 +0300

    added missing information

--- scripts/_pending/soil_samples/doi_10.7910_DVN_FPPDCG.R
+++ scripts/_pending/soil_samples/doi_10.7910_DVN_FPPDCG.R
@@ -24,15 +24,16 @@ carob_script <- function(path) {
   ## download data from the uri provided
   ff <- carobiner::get_data(uri, path, group)
   js <- carobiner::get_metadata(dataset_id, path, group, major=1, minor=0)
-  dset$license <- carobiner::get_license(js)$name
+  dset$license <- carobiner::get_license(js)
   dset$title <- carobiner::get_title(js)
 	dset$authors <- carobiner::get_authors(js)
 	dset$description <- carobiner::get_description(js)
  
   ##RH why not?
   # No need to read the table with MIR data
-  #f0 <- ff[basename(ff) == "Infrared MIR.csv"]
-  #d0 <- data.frame(read.csv2(f0, sep = ","))
+  f0 <- ff[basename(ff) == "Infrared MIR.csv"]
+  d0 <- data.frame(read.csv2(f0, sep = ","))
+  colnames(d0) <- c('SSN', paste0('m', round(as.numeric(substr(colnames(d0[,-1]),2,19)),1)))
   
   # read the table with wetchem data
   f1 <- ff[basename(ff) == "Wet Chemistry.csv"]
@@ -54,31 +55,8 @@ carob_script <- function(path) {
 	# Select from d2 , records from cluster 1 and plot 1 which were analyzed for wet chemistry
 	d2 <- subset(d2, Plot == '1')
 	# merge d1 and d2 tables
-	# d <- merge(d2,d1, by = 'SSN')
-	# 
-	# # Remove any -ve B values
-	# 
-	# bn <- which(d$soil_B < 0)
-	# 
-	# d <-  d[-bn,]
-	# 
-	# # Add other common soil variables normally tested
-	# d$soil_total_carbon <- NULL
-	# d$soil_clay <- NULL
-	# d$soil_sand <- NULL
-	# d$soil_silt <- NULL
-	# d$soil_pH_KCl <- NULL
-	# d$soil_pH_CaCl2 <- NULL
-	# d$soil_P_available <- NULL
-	# d$dataset_id <- dataset_id
-	# d$trial_id <- NA
-	# d$crop <- NA
-	# d$yield_part <- NA
-	# d$yield <- NA
-	# 
-	# # Drop id, original_id, year and district columns from d
-	# k <- which(colnames(d) %in% c("id", "original_id", "year", "district"))
-	# carobiner::write_files(dset, d[,-k], path=path)
+	
+	# This data cannot be merged to include sampling locations and merge with GPS locations. 
 	
 	carobiner::write_files(dset, d1, path=path)
 }
