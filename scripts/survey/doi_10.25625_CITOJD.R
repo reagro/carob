976f3b9ee15278b3ed817780ee63e8231e9f589d
rhijmans <r.hijmans@gmail.com>
2024-03-15 10:40:01 +0100

    r

--- scripts/survey/doi_10.25625_CITOJD.R
+++ scripts/survey/doi_10.25625_CITOJD.R
@@ -4,31 +4,24 @@ carob_script <- function(path) {
   
 #This study collected evidence on the use of early sown wheat varieties and complementary zero tillage technologies in Northwest India. Detailed information on farmers’ knowledge, adoption decisions, personal experience, and perceptions of early sown wheat and zero tillage technologies were collected at the household level using different survey tools. Additional information on agricultural practices during the Rabi Season 2021/22 were collected at the plot level and geocoded. Overall, the dataset comprises responses from 1206 wheat farmers in 70 villages across 7 districts in Punjab and Haryana that were collected between September and October 2022. The villages were selected using stratified random sampling based on a sampling frame of 1722 communities that had been identified as predominantly wheat growing areas based on remote-sensing data from satellite images. The dataset provides rich information that may be used for assessing the diffusion and impact of recently developed wheat varieties designed for early sowing, identifying barriers to the wider adoption of these technologies, and informing policy making aimed at improving adoption and usage decisions of agricultural innovations.
 
-  uri <- "doi:10.25625/CITOJD"
-  dataset_id <- carobiner::simple_uri(uri)
-  group <- "survey"
-  ## dataset level data 
-  dset <- data.frame(
-    dataset_id = dataset_id,
-    group=group,
-    project="",
-    uri=uri,
-    data_citation="Naeher, Dominik; Albanna, Basma; Kumar, Abhijeet; Vollmer, Sebastian, 2023. Household and plot-level survey data on adoption, outcomes, and perceptions of early sown wheat and zero tillage in Northwest India. https://doi.org/10.25625/CITOJD, GRO.data, V1, UNF:6:5l5PoTiAWRas7QFCI+GcBA== [fileUNF]",
-    publication= NA,
-    data_institutions = " Göttingen",
-    data_type="survey", 
-    carob_contributor="Robert Hijmans",
-    carob_date="2024-01-29"
-  )
-  
-  ## download and read data 
+	uri <- "doi:10.25625/CITOJD"
+	dataset_id <- carobiner::simple_uri(uri)
+	group <- "survey"
   
 	ff  <- carobiner::get_data(uri, path, group)
-	js <- carobiner::get_metadata(dataset_id, path, group, major=1, minor=2)
-	dset$license <- carobiner::get_license(js)
-	dset$title <- carobiner::get_title(js)
-	dset$authors <- carobiner::get_authors(js)
-	dset$description <- carobiner::get_description(js)
+	js <- carobiner::get_metadata(dataset_id, path, group, major=3, minor=0)
+
+	dset <- data.frame(
+		carobiner::extract_metadata(js, uri, group),
+		data_citation="Naeher, Dominik; Albanna, Basma; Kumar, Abhijeet; Vollmer, Sebastian, 2023. Household and plot-level survey data on adoption, outcomes, and perceptions of early sown wheat and zero tillage in Northwest India. https://doi.org/10.25625/CITOJD, GRO.data, V1, UNF:6:5l5PoTiAWRas7QFCI+GcBA== [fileUNF]",
+		publication= NA,
+		data_institutions = "Göttingen",
+		data_type="survey", 
+		carob_contributor="Robert Hijmans",
+		carob_date="2024-01-29"
+	)
+  
+  ## download and read data 
 
 	return(TRUE)	
 }
--- scripts/wheat_trials/hdl_11529_10998.R
+++ scripts/wheat_trials/hdl_11529_10998.R
@@ -2,25 +2,17 @@
 
 carob_script <- function(path) {
 
-"Description:
+"International Durum Yield Nurseries are replicated yield trials designed to measure the yiel d potential and adaptation of superior CIMMYT-bred spring durum wheat germplasm that have been developed from tests conducted under irrigation and induced stressed cropping conditions in northwest Mexico. These materials have been subjected to numerous diseases (leaf, stem and yellow rust; Septoria tritici blotch) and varied growing environments. It is distributed to 70 locations, and contains 50 entries. (2014)"
 
-    [International Durum Yield Nurseries are replicated yield trials designed to measure the yiel d potential and adaptation of superior CIMMYT-bred spring durum wheat germplasm that have been developed from tests conducted under irrigation and induced stressed cropping conditions in northwest Mexico. These materials have been subjected to numerous diseases (leaf, stem and yellow rust; Septoria tritici blotch) and varied growing environments. It is distributed to 70 locations, and contains 50 entries. (2014)]
-
-"
-#### Identifiers
 	uri <- "hdl:11529/10998"
 	group <- "wheat_trials"
-
-# the script filename should be paste0(dataset_id, ".R")
 	dataset_id <- carobiner::simple_uri(uri)
 
-#### Download data 
 	ff  <- carobiner::get_data(uri, path, group)
 	js <- carobiner::get_metadata(dataset_id, path, group, major=4, minor=0)
 
-##### dataset level metadata 
 	dset <- data.frame(
-	  carobiner::extract_metadata(js, uri, group=group,dataset=dataset_id),
+	  carobiner::extract_metadata(js, uri, group=group),
 	  data_citation="Global Wheat Program; IWIN Collaborators; Ammar, Karim; Payne, Thomas, 2017, 46th International Durum Yield Nursery, https://hdl.handle.net/11529/10998, CIMMYT Research Data & Software Repository Network, V4",
 	  data_institutions = "CIMMYT",
 	  publication=NA,
@@ -30,18 +22,9 @@ carob_script <- function(path) {
 	  carob_date="2024-02-22"
 	)
 	
-##### PROCESS data records
-
 	proc_wheat <- carobiner::get_function("proc_wheat", path, group)
 	d <- proc_wheat(ff)
 	d$dataset_id <- dataset_id
-	#d$previous_crop<-carobiner::replace_values(d$previous_crop,"sesbania aculeata","sesbania")
 	
-# all scripts must end like this
 	carobiner::write_files(dset, d, path=path)
 }
-
-## now test your function in a _clean_ R environment (no packages loaded, no other objects available)
-# path <- _____
-# carob_script(path)
-
