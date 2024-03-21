84083f9692e5f746ff884c88a3009d225e501d18
rhijmans <r.hijmans@gmail.com>
2024-03-21 10:25:49 -0700

    for #316

--- scripts/conservation_agriculture/hdl_11529_10548609.R
+++ scripts/conservation_agriculture/hdl_11529_10548609.R
@@ -11,18 +11,13 @@ carob_script <- function(path) {
     [We investigated the effect of bed width on grain yield under irrigated and rainfed conditions, for crops grown on permanent beds, where the top of the raised beds is not tilled. The study included nine sites in Central Mexico, where wide and narrow permanent beds were compared at the same site for at least three consecutive crop cycles. Six trials were selected under rainfed conditions in the states of Queretaro, Guanajuato, Michoacan, and State of Mexico, and three more with irrigation, which were located in Guanajuato and Queretaro. The data were collected in different periods, from 2007 to 2019. The database contains yield data for maize (Zea mays L.), wheat (Trititcum aestivum L.) and barley (Hordeum vulgare L.) planted on wide and narrow permanent beds.]
 
 "
-#### Identifiers
 	uri <- "hdl:11529/10548609"
-	group <- "maize_trials"
-
-# the script filename should be paste0(dataset_id, ".R")
+	group <- "conservation_agriculture"
 	dataset_id <- carobiner::simple_uri(uri)
 
-#### Download data 
 	ff  <- carobiner::get_data(uri, path, group)
 	js <- carobiner::get_metadata(dataset_id, path, group, major=1, minor=1)
 
-##### dataset level metadata 
 	dset <- data.frame(
 		carobiner::extract_metadata(js, uri, group),
 		data_citation="Verhulst, Nele; Saldivia Tejeda, Abel; Guan, Taiyu; Fonteyne, Simon, 2021, Yield of maize, wheat and barley planted on wide and narrow permanent beds, under irrigated and rainfed conditions in Mexico, https://hdl.handle.net/11529/10548609, CIMMYT Research Data & Software Repository Network, V1, UNF:6:bMwiJ4W0pL9LG8olmN3meA== [fileUNF]",
@@ -34,48 +29,30 @@ carob_script <- function(path) {
 		carob_date="2024-03-14"
 	)
 	
-##### PROCESS data records
-	
-	# read data 
-	
 	f <- ff[basename(ff) == "DAT-BedWidth-2021-07.xlsx"]
 	r <- carobiner::read.excel(f, sheet = "Data")
-	
-	## process file(s)
-	
-	## use a subset
+
 	d <- data.frame(
-	  crop = r$Crop,
-	  country = "Mexico",
-	  adm1 = r$State,
-	  adm2 = r$Municipality,
-	  site = r$Site_experiment,
-	  planting_date = r$Year,
-	  yield_part = "grain",
-	  yield = r$Yield_moist,
-	  rep = r$Num_Rep,
-	  irrigated = r$Water_Regime
+		crop = tolower(r$Crop),
+		country = "Mexico",
+		adm1 = r$State,
+		adm2 = r$Municipality,
+		site = r$Site_experiment,
+		planting_date = as.character(r$Year),
+		yield_part = "grain",
+		yield = r$Yield_moist * 1000,
+		rep = as.integer(r$Num_Rep),
+		irrigated = r$Water_Regime
 	)
-	
-	
-	#### about the data #####
-	## (TRUE/FALSE)
 	
 	d$dataset_id <- dataset_id
 	d$on_farm <- FALSE
-	  d$is_survey <- FALSE
-	  d$is_experiment <- TRUE
-	  d$irrigated <- TRUE
-	 d$striga_trial<- NA
-	 d$borer_trial<- NA
-	 d$striga_infected<- NA
+	d$is_survey <- FALSE
+	d$irrigated <- TRUE
 	  
-	  ##### Location #####
-	## each site must have corresponding longitude and latitude
-	## see carobiner::geocode
-	  g <-unique(d[,c("country","adm2")])
-	  g1<-carobiner::geocode(country = g$country,location = g$adm2,service = "nominatim")
-	  gg<-data.frame(
+#	  g <-unique(d[,c("country","adm2")])
+#	  g1<-carobiner::geocode(country = g$country,location = g$adm2,service = "nominatim")
+	gg <- data.frame(
 	    adm2 = c("Cadereyta", "Indaparapeo", "San Juan del Rio I", 
 	                 "Apaseo el Alto", "Irapuato", "San Juan del Rio III",
 	                 "Texcoco", "Penjamo"),
@@ -83,14 +60,10 @@ carob_script <- function(path) {
 	                -99.9643, -98.8554, -101.8405),
 	    latitude=c(20.7912, 19.7602, 20.3756, 20.4326, 20.6749, 
 	               20.428, 19.4756, 20.4055))
-	  d$trial_id<- paste0(dataset_id,"_",d$rep)
-	  d <- merge(d, gg, by="adm2", all.x=TRUE)
-	  d$crop<-tolower(d$crop)
-	  d$rep<-as.integer(d$rep)
-	  d$planting_date<-as.character(d$planting_date)
-	  d$irrigated<-as.logical(d$irrigated)
-	  d$yield<-d$yield*1000
+	
+	d <- merge(d, gg, by="adm2", all.x=TRUE)
+	d$trial_id <- as.character(as.factor(d$adm2))
 	  
-	  # all scripts must end like this
-	  carobiner::write_files(dset, d, path=path)
+	carobiner::write_files(dset, d, path=path)
 }
+
