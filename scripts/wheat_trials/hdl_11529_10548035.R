34c2dd44389e44e3241045322b3fd7f4d37cd27b
rhijmans <r.hijmans@gmail.com>
2024-03-22 10:28:07 -0700

    w

--- scripts/wheat_trials/hdl_11529_10548035.R
+++ scripts/wheat_trials/hdl_11529_10548035.R
@@ -3,24 +3,18 @@
 
 carob_script <- function(path) {
   
-  "Description:
-
-  [International Durum Yield Nurseries are replicated yield trials designed to measure the
-  yield potential and adaptation of superior CIMMYT-bred spring durum wheat germplasm 
-  that have been developed from tests conducted under irrigation and induced stressed 
-  cropping conditions in northwest Mexico. These materials have been subjected to
-  numerous diseases (leaf, stem and yellow rust; Septoria tritici blotch) and varied
-  growing environments. It is distributed to 70 locations, and contains 50 entries.(2016)]"
-  
-  
+"International Durum Yield Nurseries are replicated yield trials designed to measure the yield potential and adaptation of superior CIMMYT-bred spring durum wheat germplasm that have been developed from tests conducted under irrigation and induced stressed cropping conditions in northwest Mexico. These materials have been subjected to numerous diseases (leaf, stem and yellow rust; Septoria tritici blotch) and varied growing environments. It is distributed to 70 locations, and contains 50 entries.(2016)"
+   
 	uri <- "hdl:11529/10548035"
-	dataset_id <- carobiner::simple_uri(uri)
 	group <- "wheat_trials"
+	dataset_id <- carobiner::simple_uri(uri)
+
+	ff	<- carobiner::get_data(uri, path, group)
+	js <- carobiner::get_metadata(dataset_id, path, group, major=6, minor=1)
+
 	dset <- data.frame(
-		dataset_id = dataset_id,
-		group=group,
-		project="High Rainfall Wheat Yield Trial",
-		uri=uri,
+		carobiner::extract_metadata(js, uri, group=group),
+		project="International Durum Yield Nurseries",
 		data_citation="Global Wheat Program; IWIN Collaborators; Singh, Ravi; Payne, Thomas, 2019, 1st to 10th High Rainfall Wheat Yield Trial, https://hdl.handle.net/11529/10548195, CIMMYT Research Data & Software Repository Network, V4, UNF:6:1bUNVa1OpwFsogRFMSF4vQ== [fileUNF]",
 		publication=NA,
 		data_institutions = "CIMMYT",
@@ -28,13 +22,6 @@ carob_script <- function(path) {
 		carob_contributor="Blessing Dzuda",
 		carob_date="2024-01-11"
 	)
-	 
-	ff	<- carobiner::get_data(uri, path, group)
-	js <- carobiner::get_metadata(dataset_id, path, group, major=6, minor=1)
-	dset$license <- carobiner::get_license(js)
-	dset$title <- carobiner::get_title(js)
-	dset$authors <- carobiner::get_authors(js)
-	dset$description <- carobiner::get_description(js)
 	 
 	proc_wheat <- carobiner::get_function("proc_wheat", path, group)
 	d <- proc_wheat(ff)
