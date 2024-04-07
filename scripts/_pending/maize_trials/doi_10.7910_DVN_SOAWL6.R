de76492b431bd99792f2a9f24172fa9aabaf0b2b
rhijmans <r.hijmans@gmail.com>
2024-04-07 00:34:34 -0700

    major update

--- scripts/_pending/maize_trials/doi_10.7910_DVN_SOAWL6.R
+++ scripts/_pending/maize_trials/doi_10.7910_DVN_SOAWL6.R
@@ -5,7 +5,7 @@ carob_script <- function(path) {
 "Human Zn and Fe deficiencies can be reduced through agronomic biofortification, but information on factors influencing maize grain-Zn and -Fe levels remains scant. This analysis: (1) Establishes the global distribution of Zn and Fe concentrations in maize grain; (2) Assesses different agronomic practices’ contributions to increasing maize-grain Zn and Fe levels; and (3) Identifies key biophysical factors to guide agronomic biofortification. Using 1,332 data points in 102 published papers from 24 countries, we estimated a 24% probability of grain-Zn concentrations exceeding the benchmark target of 38 mg kg−1."
 
 	uri <- "doi:10.7910/DVN/SOAWL6"
-	group <- "maize_trials"
+	group <- "fertilizer"
 
 	ff <- carobiner::get_data(uri, path, group)
 
@@ -14,7 +14,7 @@ carob_script <- function(path) {
 		project=NA,
 		publication= NA,
 		data_institutions = "CIAT",
-		data_type="experiment",
+		data_type="compilation of experiments",
 		carob_contributor="Fredy Chimire",
 		carob_date="2024-2-24"
 	)
@@ -24,26 +24,28 @@ carob_script <- function(path) {
 
 # Avail_P, Avail_P_Mehlich.3, AveP.Znratio, AveP_method, Olsen.P.avail, OLsenP.class, 
 # Fe.application..Soil..Foliar., Fe.Zn.ratio, Fe_applied..Yes..No..,
-# Innoculation, Inoculation,
-# InorgP_Cnt, InorgP_Trt, InorgP_Trt.1, MGF_1_Description_Trt,
-# Old_SoilType
-# Organic_matter_added, Organic_matter_type,
+# Innoculation, Inoculation, MGF_1_Description_Trt,
 # Parent.material, pH_CaCl2, PH_Method, pHClass, Phenols.ug.g._Trt, Phyticphosphorus.mg.g._Trt, 
-# SOC.Clay.rati, SOCclass, Soil.type_New, Treat_code, Treatment.corrected,
+# SOCclass, Treat_code, Treatment.corrected,
 # X.If.yes..form.of.Zn.application..Soil..Foliar..,
-# Zn..kg.ton., Zn_applied..Yes..No.., Zn_applied_.Kg.ha., Zn_method, ZntoPratio
+# Zn_applied_.Kg.ha., Zn_method
 
 
 	d <- data.frame(
 		country=r$Country,
 		planting_date=r$Yr_experiment,
 		soil_pH_CaCl2=r$pH_CaCl2,
-		site= r$TrialSite,
+		site=r$TrialSite,
 		N_fertilizer=r$N_Applied_Trt,
-		K_fertilizer= r$K_Applied_Trt,
+		K_fertilizer=r$K_Applied_Trt,
 		P_fertilizer=r$P_Applied_Trt,
 		Zn_fertilizer=r$Zn_Applied_Trt,
 		Fe_fertilizer=r$Fe_Applied_Trt,
+
+		OM_type = tolower(r$Organic_matter_type),
+		OM_amount = r$FYM_Trt,
+		OM_used = !is.na(r$FYM_Trt) | (tolower(r$Organic_matter_added) == "yes"),
+
 		irrigated= tolower(r$Irrigation_Trt) == "yes",
 		yield = as.numeric(r$GrainYld_Trt) * 1000,
 		soil_clay=as.numeric(r$Clay),
@@ -56,11 +58,11 @@ carob_script <- function(path) {
 		soil_Fe = r$Soil_Fe,
 		soil_Zn = r$Soil_Zn,
 		soil_texture = gsub("_", " ", tolower(r$Texture)),
+		soil_type = r$Soil.type_New,
 		rain = r$SeasonalRainfall,
 		grain_Zn = r$GrainZn_Trt,
 		grain_Fe = r$GrainFe.mgkg.1._Trt,
 		grain_N = r$GrainN_Trt,
-		grain_P = r$InorgP_Trt,
 		grain_protein = r$Proteins_Trt,
 
 		yield_cnt = r$GrainYld_Cnt, 
@@ -70,15 +72,16 @@ carob_script <- function(path) {
 		Fe_fertilizer_cnt = r$Fe_Applied_Cnt, 
 		Zn_fertilizer_cnt = r$Zn_Applied_Cnt, 
 		grain_Zn_cnt = r$GrainZn_Cnt, 
-		grain_Fe_cnt = r$GrainFe.mgkg.1._Cnt
+		grain_Fe_cnt = r$GrainFe.mgkg.1._Cnt,
+		grain_P_cnt = r$InorgP_Cnt
 	)
 
-	d$striga_trial <- FALSE
-	d$borer_trial <- FALSE
-	d$striga_infected <- FALSE
+	d$grain_P = rowMeans(r[, c("InorgP_Trt", "InorgP_Trt.1")], na.rm=TRUE)
+
 	d$trial_id <- as.character(as.integer(as.factor(paste(d$country, d$site))))
 	d$land_prep_method <- c("conventional", "none", "reduced tillage", "unknown")[r$Tillage]
 	d$soil_texture[d$soil_texture == "silty"] <- "silt"
+	d$soil_type[d$soil_type=="20.5"] <- NA
 
 	ref <- iconv(r$Study, "UTF-8", "UTF-8")
 	d$reference <- trimws(gsub("\"", "", ref))
@@ -106,32 +109,17 @@ carob_script <- function(path) {
 	d$longitude = get_geo(r$Longitude)
 	d$latitude = get_geo(r$Latitude)
 
-
 	d$crop <- "maize"
 	d$yield_part <- "grain"
 	d$planting_date <- gsub("[^0-9]", "", d$planting_date)
 	d$planting_date[nchar(d$planting_date) != 4] <- NA
 	d$planting_date <- d$planting_date
 
-	d$N_fertilizer[tolower(d$N_fertilizer) == "yes"] <- NA
-	d$N_fertilizer <- as.numeric(d$N_fertilizer)
-
-	d$P_fertilizer[tolower(d$P_fertilizer) %in% c("yes", "ssp")] <- NA
-	d$P_fertilizer <- as.numeric(d$P_fertilizer)
-
-	d$K_fertilizer[tolower(d$K_fertilizer) == "yes"] <- NA
-	i <- d$K_fertilizer == "Yes (Foliar at 0.1%)"
-	d$K_fertilizer[i] <- NA ## for now
-	d$K_fertilizer <- as.numeric(d$K_fertilizer)
-
-	d$Zn_fertilizer[d$Zn_fertilizer == "Yes"] <- NA
-	d$Zn_fertilizer <- as.numeric(d$Zn_fertilizer)
-
-	d$OM_used = !is.na(r$FYM_Trt)
-	d$OM_amount <- r$FYM_Trt
 	d$OM_amount[d$OM_amount == "Yes"] <- NA
 	d$OM_amount <- as.numeric(d$OM_amount)
-	d$OM_type[d$OM_used] <- "farmyard manure"
+	d$OM_type <- carobiner::replace_values(d$OM_type, 
+				c("fym", "organic fertiliser"), 
+				c("farmyard manure", "unknown"))
 	
 	d <- d[!is.na(d$country), ]
 	d <- d[!is.na(d$yield), ]
@@ -153,6 +141,11 @@ carob_script <- function(path) {
 	i <- d$latitude == -17.723 & d$country == "Kenya"
 	d$latitude[i] <- d$longitude[i] <- NA
 
+	d$latitude[d$site == "Ismailia Governorate"] <- 30.695
+	d$latitude[d$site == "Nyankpala"] <- 9.421
+	d$latitude[d$site == "Palampur"] <- 32.1
+	d$country[d$site %in% c("Gabura", "Tulatuli")] <- "Bangladesh"
+
 	dd <- d[!is.na(d$yield_cnt), ] 
 	cnms <- grep("_cnt$", names(d), value=TRUE)
 	nms <- gsub("_cnt$", "", cnms)
@@ -161,6 +154,23 @@ carob_script <- function(path) {
 	d <- d[, !(names(d) %in% cnms)]
 
 	d <- rbind(d, dd)
+
+	d$N_fertilizer[tolower(d$N_fertilizer) == "yes"] <- NA
+	d$N_fertilizer <- as.numeric(d$N_fertilizer)
+
+	d$P_fertilizer[tolower(d$P_fertilizer) %in% c("yes", "ssp")] <- NA
+	d$P_fertilizer <- as.numeric(d$P_fertilizer)
+
+	d$K_fertilizer[tolower(d$K_fertilizer) == "yes"] <- NA
+	i <- d$K_fertilizer == "Yes (Foliar at 0.1%)"
+	d$K_fertilizer[i] <- NA ## for now
+	d$K_fertilizer <- as.numeric(d$K_fertilizer)
+
+	d$Zn_fertilizer[tolower(d$Zn_fertilizer) == "yes"] <- NA
+	d$Zn_fertilizer <- as.numeric(d$Zn_fertilizer)
+
+	d$Fe_fertilizer[tolower(d$Fe_fertilizer) == "yes"] <- NA
+	d$Fe_fertilizer <- as.numeric(d$Fe_fertilizer)
 
 	carobiner::write_files(dset, d, path=path)
 }
