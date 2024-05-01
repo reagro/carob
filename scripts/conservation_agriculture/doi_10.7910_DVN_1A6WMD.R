56c81cd4f633d432a4d900646fdaa12da880f0f4
rhijmans <r.hijmans@gmail.com>
2024-05-01 06:01:18 -0700

    m

--- scripts/conservation_agriculture/doi_10.7910_DVN_1A6WMD.R
+++ scripts/conservation_agriculture/doi_10.7910_DVN_1A6WMD.R
@@ -5,62 +5,56 @@ carob_script <- function(path) {
   
 "Many soils on smallholder farms in Malawi have poor soil organic matter content. This results in poor maize productivity when sufficient mineral fertilizers are not added. Building soil organic matter requires improving both cereal and legume crops primary productivity through mineral fertilizers, and retaining the associated crop residues on the cropped lands. These residues decompose to provide mineral N to crops grown in sequence, as well as being an important source for SOM capitalization. Residues of legumes crops have a narrow C/N ration and are hypothesized to improve N cycling and benefit the rotational crop, whereas residues of maize, which have a wide C/N ratio, promote immobilization. While this knowledge is widely known, what is not clear is the interaction between crop residue quality, quantity and soil water management on maize productivity and mineral N dynamics. The data will address the following: 1. Does incorporating soil water enhancing technologies increase/reduce the immobilization potential of maize residues? 2. What is the effect of varying the quantity of the crop residues incorporated (both maize and legumes) on mineral N dynamics, soil water content and maize productivity; 3. For farmers with limited fertilizer use (50% NP), how detrimental is use of maize residues (X0, X1, X2), with or without water conservation measures; 4. What is the fertilizer substitution value of different quantity residues generated from a groundnut/pigeonpea doubled up system? This data is for the residue generation phase"
   
-  uri <- "doi:10.7910/DVN/1A6WMD"
-  group <- "conservation_agriculture"
-  ff <- carobiner::get_data(uri, path, group)
+	uri <- "doi:10.7910/DVN/1A6WMD"
+	group <- "fertilizer"
+	ff <- carobiner::get_data(uri, path, group)
 
-  # dataset level data 
 	dset <- data.frame(
   	carobiner::read_metadata(uri, path, group, major=1, minor=1),
 		project=NA,
-		publication= "doi.org/10.1016/j.fcr.2021.108225",
+		publication= "doi:10.1016/j.fcr.2021.108225",
 		data_institutions = "IITA;IFPRI",
 		data_type="on-farm experiment", 
 		carob_contributor="Mitchelle Njukuya",
 		carob_date="2024-01-09"
 	)
   
-  
-  
-  f <- ff[basename(ff) == "NewDesign_gnut yields.csv"]
-  
-  r <- read.csv(f)
-  ## process file(s)
-  
-  d <- r
-  message("error: 'District' is adm2; EPA could be location with village being 'site'")
+	f <- ff[basename(ff) == "NewDesign_gnut yields.csv"]
+	r <- read.csv(f)
 
-  d <- carobiner::change_names(d,c("District","EPA","Village","Treatment","Year.of.establishment","Crop.harvested","Groundnut.variety","Rep","stover.dry.weight..kg.ha.","Grain.weight..kg.ha.","Total.biomass..kg.ha.")
-                  ,c("location","adm1","adm2","treatment","year","crop","variety","rep","dym_residue","yield","dym_total"))
+	d <- data.frame(
+		adm2=r$District,
+		location=r$Village,
+		site=r$EPA,
+		treatment=tolower(r$Treatment),
+		intercrops="pigeon pea",
+		crop=tolower(r$Crop.harvested),
+		variety=r$Groundnut.variety,
+		rep=r$Rep,
+		dmy_residue=r$stover.dry.weight..kg.ha.,
+		yield=r$Grain.weight..kg.ha.,
+		dmy_total=r$Total.biomass..kg.ha.
+	)
 				  
-					
-				  
-  d$treatment <- carobiner::replace_values(d$treatment,"Groundnut/pigeonpea","groundnut/pigeonpea")
-  d$crop <- carobiner::replace_values(d$crop,"Groundnut","groundnut")
+	d$on_farm <- TRUE
+	d$is_survey <- FALSE
+	d$trial_id <- d$Treatment.number
+	d$country <- "Malawi"
+
+	#planting dates sourced from publication
+	d$planting_date <- "2017"
+	d$N_fertilizer <- 34.5
+	d$yield_part <- "seed"
+	d$plant_density <- 164000
   
-  d$on_farm <- TRUE
-  d$is_experiment <- TRUE
-  d$trial_id <- d$Treatment.number
-  d$country <- "Malawi"
-  d1 <- unique(d[,c("country","location")])
-  d2 <- data.frame(
-		country = "Malawi", 
-		location = c("Dedza","Machinga"),
-		longitude = c(34.3197, 35.6026),
-		latitude = c(-14.2374, -14.9027))
-		
-  d <- merge(d,d2,by=c("country","location"),all.x=TRUE)
-  
-  #planting dates sourced from publication
-  d$planting_date <- as.character(2017)
-  d$N_fertilizer <- 34.5
-  d$yield_part <- "seed"
-  d$plant_density <- "164 000"
-  
-   # do not do this:
-  d <- d[,c("year", "on_farm","is_experiment","country","location","adm1","adm2","longitude","latitude","planting_date","treatment","rep","crop","variety","N_fertilizer","plant_density","dym_residue","dym_total","yield","yield_part")]
-   
-    # all scripts must end like this
+	d$trial_id <- as.character(as.integer(as.factor(d$location)))
+	geo <- data.frame(
+		location = c("Mbizi", "Mkambiri", "Tambula"), 
+		latitude = c(-14.176, -14.567, -14.623),
+		longitude= c(34.125, 35.538, 35.6456 )
+	)
+
+	d <- merge(d, geo, by="location", all.x=TRUE)   
     carobiner::write_files(dset, d, path=path)
 }
 
--- scripts/conservation_agriculture/doi_10.7910_DVN_1T4Q3F.R
+++ scripts/conservation_agriculture/doi_10.7910_DVN_1T4Q3F.R
@@ -2,8 +2,7 @@
 
 carob_script <- function(path) {
 
-"This dataset is generated from maize response trials (legume/maize rotation system) that were conducted in Linthipe EPA, Dedza district, and Ntubwi EPA in Machinga district in Malawi, 2019/2020 cropping season. In the previous season, certifies and recycled seed of different varieties of groundnut were grown. In the current season, maize was planted as a test crop.
-"
+"This dataset is generated from maize response trials (legume/maize rotation system) that were conducted in Linthipe EPA, Dedza district, and Ntubwi EPA in Machinga district in Malawi, 2019/2020 cropping season. In the previous season, certifies and recycled seed of different varieties of groundnut were grown. In the current season, maize was planted as a test crop."
 
 	uri <- "doi:10.7910/DVN/1T4Q3F"
 	group <- "conservation_agriculture"
@@ -11,65 +10,66 @@ carob_script <- function(path) {
 
 	dset <- data.frame(
 		carobiner::read_metadata(uri, path, group, major=1, minor=1),
-	  #data_citation="Lilongwe University of Agriculture and Natural Resources, 2021, Performance of Maize Following Groundnut Varieties at Different Densities, https://doi.org/10.7910/DVN/1T4Q3F, Harvard Dataverse, V1, UNF:6:oLZEjX4cmNU+x2ElSIOljA== [fileUNF]",
-	  data_institutions = "International Food Policy Research Institute (IFPRI)",
-	  publication= NA,
-	  project=NA,
-	  # data_type can be e.g. "on-farm experiment", "survey", "compilation"
-	  data_type= "experiment",
-	  carob_contributor= "Shumirai Manzvera",
-	  carob_date="2024-02-22"
+		data_institutions = "International Food Policy Research Institute (IFPRI)",
+		publication= NA,
+		project=NA,
+		data_type= "experiment",
+		carob_contributor= "Shumirai Manzvera",
+		carob_date="2024-02-22"
 	)
-	
-	
-##### PROCESS data records
-
-# read data 
-
 	f0 <- ff[basename(ff) == "Linthipe_Dedza_MaizeResopnsetoGroundnut_2019_2020.csv"]
 	f1 <- ff[basename(ff) == "Mtubwi_Machinga_MaizeResopnsetoGroundnut_2019_2020.csv"]
 	r0 <- read.csv(f0)
 	r1 <- read.csv(f1)
 
-## process file(s)
-
 ## use a subset
-	d0 <- data.frame(adm2=r0$District, adm3=r0$EPA, rep=r0$REPLICATION,
-			variety_code=r0$maize.variety.planted.in.2019.2020.season,
-	        previous_crop="groundnut", crop_rotation="maize; groundnut",
-	        dmy_total=r0$Total.biomass..kg.ha.,
-			yield=r0$Grain.yld.ha, treatment=r0$TREATMENT.No.,
-			longitude=34.1253751, latitude=-14.1832077, trial_id="1")
+	d0 <- data.frame(
+		adm2=r0$District, 
+		adm3=r0$EPA, 
+		rep=r0$REPLICATION,
+		variety_code=r0$maize.variety.planted.in.2019.2020.season,
+	    previous_crop="groundnut", 
+		crop_rotation="maize; groundnut",
+	    dmy_total=r0$Total.biomass..kg.ha.,
+		yield=r0$Grain.yld.ha, 
+		treatment=r0$TREATMENT.No.,
+		longitude=34.1253751, 
+		latitude=-14.1832077, 
+		trial_id="1"
+	)
 					
 
-	d1 <- data.frame(adm2=r1$District, adm3=r1$EPA, rep=r1$REPLICATION,
-			variety_code=r1$maize.variety.2019.2020.season,
-	        previous_crop="groundnut", crop_rotation="maize; groundnut",
-	        dmy_total=r1$Total.biomass..kg.ha.,
-			yield=r1$Grain.yld..kg.ha.,treatment=r1$TREATMENT.NO.,
-			longitude =35.5737, latitude = -14.9458, trial_id="2")
+	d1 <- data.frame(
+		adm2=r1$District, 
+		adm3=r1$EPA, 
+		rep=r1$REPLICATION,
+		variety_code=r1$maize.variety.2019.2020.season,
+	    previous_crop="groundnut", 
+		crop_rotation="maize; groundnut",
+	    dmy_total=r1$Total.biomass..kg.ha.,
+		yield=r1$Grain.yld..kg.ha.,treatment=r1$TREATMENT.NO.,
+		longitude =35.5737, 
+		latitude = -14.9458, 
+		trial_id="2"
+	)
+		
+	d <- carobiner::bindr(d0, d1)
 	
-	
-	d <- carobiner::bindr(d0,d1)
-	
-#### about the data #####
 	d$country<- "Malawi"
 	
 	d$on_farm <- FALSE
 	d$is_survey <- FALSE
 	d$irrigated <- FALSE
 
-##### Time #####
 	d$planting_date <- "2019"
 	d$harvest_date  <- "2020"
 
-	#what plant part does yield refer to?
 	d$crop <- "maize"
 	d$yield_part <- "grain"
+	d$dmy_total <- gsub(",", "", d$dmy_total)
 	d$dmy_total <- as.numeric(d$dmy_total)
 	d$treatment <- as.character(d$treatment)
 	
-# all scripts must end like this
 	carobiner::write_files(dset, d, path=path)
 }
 
