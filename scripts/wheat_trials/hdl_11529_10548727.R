9b1bf81399efa1c6cbf88666525bb892e5de7675
rhijmans <r.hijmans@gmail.com>
2024-05-02 12:11:10 -0700

    w

--- scripts/wheat_trials/hdl_11529_10548727.R
+++ scripts/wheat_trials/hdl_11529_10548727.R
@@ -3,17 +3,17 @@
 
 carob_script <- function(path) {
   
-  "
-The Karnal Bunt Screening Nursery is a single replicate nursery that contains diverse spring bread wheat
+"The Karnal Bunt Screening Nursery is a single replicate nursery that contains diverse spring bread wheat
 (Triticum aestivum) germplasm adapted to ME1 (Optimally irrigated, low rainfall environment) with total 50-100 entries 
-and white/red grain color. (2021)
-"
+and white/red grain color. (2021)"
+
   uri <- "hdl:11529/10548727"
   group <- "wheat_trials"
   ff <- carobiner::get_data(uri, path, group)
+
   dset <- data.frame(
     carobiner::read_metadata(uri, path, group, major=2, minor=0),
-    project="Global Wheat Program",
+    project="Karnal Bunt Screening Nursery",
     publication = NA,
     data_institutions = "CIMMYT",
     carob_contributor="Mitchelle Njukuya",
