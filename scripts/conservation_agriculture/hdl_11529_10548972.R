df9baf362d9568b4f875585a638be089964d5a42
rhijmans <r.hijmans@gmail.com>
2024-05-17 06:10:39 -0700

    for #373

--- scripts/conservation_agriculture/hdl_11529_10548972.R
+++ scripts/conservation_agriculture/hdl_11529_10548972.R
@@ -1,68 +1,34 @@
 # R script for "carob"
 
-## ISSUES
-# 1.some values are in Spanish and carob could not recognize them. Had to look for
-# the English equivalent then match to what carob requires in crop,
-# land_prep_method,crop_rotation.
-# 2. Crop initials C and M were in Spanish and been converted to English
-# 3. under land_prep_method, 2 initials were only defined by the dataset
-
 
 carob_script <- function(path) {
 
-"
-The data were collected for the publication:Does leaving crop residues in the 
-field lead to greater frost damage in wheat and barley under conservation 
-agriculture? The dataset contains data from three experiments conducted to 
-evaluate if leaving more residue in the field is associated with higher levels 
-of frost damage. Farmers in the Bajio had indicated that leaving more crop
-residue in the field when sowing barley in permanent raised beds is associated
-with higher frost damage, which is an occasional problem in the region. To 
-evaluate whether this is really the case frost damage was evaluated in a 1) a 2 
-year on farm experiment in San Juan del Rio, Queretaro, Mexico where barley was 
-sown with different levels of residue retention, 2) An experiment with different
-types of tillage and levels of residue on the Sanjaya Rajaram station in Metepec,
-Mexico state, Mexico, where due to the high elevation frost damage was guaranteed
-to occur and 3) An comparison of yield in side by side comparison of conservation
-agriculture and conventional agriculture grown during the winter season across 
-Mexico to assess whether conservation agriculture is associated with lower yields
-which could be indicative of a real problem.
-"
+"The data were collected for the publication:Does leaving crop residues in the field lead to greater frost damage in wheat and barley under conservation agriculture? The dataset contains data from three experiments conducted to evaluate if leaving more residue in the field is associated with higher levels of frost damage. Farmers in the Bajio had indicated that leaving more crop residue in the field when sowing barley in permanent raised beds is associated with higher frost damage, which is an occasional problem in the region. To evaluate whether this is really the case frost damage was evaluated in a 1) a 2 year on farm experiment in San Juan del Rio, Queretaro, Mexico where barley was sown with different levels of residue retention, 2) An experiment with different types of tillage and levels of residue on the Sanjaya Rajaram station in Metepec, Mexico state, Mexico, where due to the high elevation frost damage was guaranteed to occur and 3) An comparison of yield in side by side comparison of conservation agriculture and conventional agriculture grown during the winter season across Mexico to assess whether conservation agriculture is associated with lower yields which could be indicative of a real problem."
 
 
-#### Identifiers
 	uri <- "hdl:11529/10548972"
 	group <- "conservation_agriculture"
 
-#### Download data 
 	ff  <- carobiner::get_data(uri, path, group)
 
-##### dataset level metadata 
 	dset <- data.frame(
 		carobiner::read_metadata(uri, path, group, major=1, minor=0),
 		data_institutions = "CIMMYT",
-		publication= "hdl.handle.net/10883/22808",
+		publication="hdl:10883/22808",
 		project=NA,
 		data_type= "experiment",
 		carob_contributor= "Blessing Dzuda",
 		carob_date="2024-05-16"
 	)
-	
-##### PROCESS data records
-
-# read data 
 
 	f <- ff[basename(ff) == "DAT-FrostExperiments.xlsx"]
 	r <- carobiner::read.excel(f, sheet = "San Juan del Rio III")
 	r <- r[1:96,1:56]
 
-## process file(s)
-
-## select the variables of interest and assign them to the correct name
 	d <- data.frame(
 		  adm1=r$Estado,
 		  latitude=r$Latitud,
-		  longitude=r$Longitud,
+		  longitude= -r$Longitud,
 		  elevation=r$Altitud,
 		  soil_texture=r$Soil,
 		  rep=r$Num_Rep,
@@ -78,7 +44,7 @@ which could be indicative of a real problem.
 		  K_fertilizer=r$Fert_K,
 		  row_spacing=r$Row_dist*100,
 		  plant_spacing=r$Intrarow*100,
-		  plant_height=r$Height*100,
+		  plant_height=r$Height,
 		  planting_date=r$Sowing_date,
 		  emergence_date=r$Emergence_date,
 		  flowering_date=r$Flower_days,
@@ -88,25 +54,16 @@ which could be indicative of a real problem.
 		  dmy_storage=r$Yield_dry,
 		  yield=r$Yield_moist,
 		  grain_weight=r$Thousand,
-		  crop_price=r$Price
-		  
+		  crop_price=r$Price / 1000 # assuming it was per ton		  
 	)
 
-	
-#### about the data #####
-## (TRUE/FALSE)
 	d$on_farm <- TRUE
 	d$is_survey <- FALSE
 	d$irrigated <- TRUE
 	d$trial_id <- paste0(d$adm1,"_", d$rep)
-##### Location #####
-## make sure that the names are normalized (proper capitalization, spelling, no additional white space).
-## you can use carobiner::fix_name()
 	d$country <- "Mexico"
 
 ##### Time #####
-## time can be year (four characters), year-month (7 characters) or date (10 characters).
-## use 	as.character(as.Date()) for dates to assure the correct format.
 	d$planting_date <- as.character(as.Date(d$planting_date))
 	d$harvest_date  <- as.character(as.Date(d$harvest_date))
 	d$emergence_date  <- as.character(as.Date(d$emergence_date))
@@ -116,26 +73,20 @@ which could be indicative of a real problem.
 	d$irrigation_number <- as.integer(d$irrigation_number)
 	d$grain_weight <- as.numeric(d$grain_weight)
 	
-
-##### Yield #####
-	#what plant part does yield refer to?
 	d$yield_part <- "grain"
-	d$crop <- gsub("Cebada","barley",d$crop)
-	d$crop <- gsub("Maíz","maize",d$crop)
-	d$crop_rotation <- gsub("M","maize",d$crop_rotation)
-	d$crop_rotation <- gsub("C","barley",d$crop_rotation)
-	d$crop_rotation <- gsub("-",",",d$crop_rotation)
-	d$soil_texture <- gsub("Vertisol","clay",d$soil_texture)
-	d$land_prep_method <- gsub("LC","conventional tilled beds",d$land_prep_method)
-	d$land_prep_method <- gsub("CPA","wide permanent beds",d$land_prep_method)
+	d$crop <- gsub("Cebada", "barley", d$crop)
+	d$crop <- gsub("Maíz", "maize", d$crop)
+	d$crop_rotation <- gsub("M", "maize", d$crop_rotation)
+	d$crop_rotation <- gsub("C", "barley", d$crop_rotation)
+	d$crop_rotation <- gsub("-", ";", d$crop_rotation)
+	d$soil_texture <- gsub("Vertisol", "clay", d$soil_texture)
+	d$land_prep_method <- gsub("LC", "conventional tilled beds", d$land_prep_method)
+	d$land_prep_method <- gsub("CPA", "wide permanent beds", d$land_prep_method)
 	
-	
-	
-# all scripts must end like this
+	d$currency <- "MXN"
+	i <- which(d$plant_height < 1)
+	d$plant_height[i] <- d$plant_height[i] * 100
+
 	carobiner::write_files(path, dset, d)
 }
-
-## now test your function in a _clean_ R environment (no packages loaded, no other objects available)
-# path <- _____
-# carob_script(path)
 
