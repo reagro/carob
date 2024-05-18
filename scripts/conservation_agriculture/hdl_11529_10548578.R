67c3e59d3ed7748e5828ed654eae5aa23bb38904
rhijmans <r.hijmans@gmail.com>
2024-05-18 11:44:40 -0700

    for #374

--- scripts/conservation_agriculture/hdl_11529_10548578.R
+++ scripts/conservation_agriculture/hdl_11529_10548578.R
@@ -1,49 +1,29 @@
 # R script for "carob"
 
-## ISSUES
-# ....
-
 
 carob_script <- function(path) {
   
-  "
-The database contains data about water use in barley (Hordeum vulgare) and maize
-(Zea Mays) production in the Mexican Bajío. Water use and grain yield were 
-evaluated under conventional tillage and under conservation agriculture in 
-permanent raised beds, both under furrow irrigation and under drip irrigation.
-The data in this study were collected between 2016 and 2021 in CIMMYTs’ Bajío 
-innovation hub in a research platform (a dedicated field experiment) and in 
-innovation modules (side by side comparisons in farmers’ fields). For the platform,
-de data base contains data of three maize (summer) and three barley (winter) cycles,
-including water use, grain yield and data on key field operations. For the farmers’
-fields, the data base contains the yield and water use data from 23 modules, 
-as well as fuel use and soil health data from a subset of modules. (2021-06-29)
-"
+"The database contains data about water use in barley (Hordeum vulgare) and maize (Zea Mays) production in the Mexican Bajío. Water use and grain yield were evaluated under conventional tillage and under conservation agriculture in permanent raised beds, both under furrow irrigation and under drip irrigation. The data in this study were collected between 2016 and 2021 in CIMMYTs’ Bajío innovation hub in a research platform (a dedicated field experiment) and in innovation modules (side by side comparisons in farmers’ fields). For the platform, the data base contains data of three maize (summer) and three barley (winter) cycles, including water use, grain yield and data on key field operations. For the farmers’ fields, the data base contains the yield and water use data from 23 modules, as well as fuel use and soil health data from a subset of modules. (2021-06-29)"
   
-  #### Identifiers
   uri <- "hdl:11529/10548578"
   group <- "conservation_agriculture"
   
-  #### Download data 
   ff  <- carobiner::get_data(uri, path, group)
   
-  ##### dataset level metadata 
   dset <- data.frame(
     carobiner::read_metadata(uri, path, group, major=2, minor=0),
     data_institutions = "CIMMYT",
     publication=NA,
     project=NA,
     data_type= "on-farm experiment",
+	exp_treatment = "land_prep_method", #;irrigation_method
     carob_contributor= "Blessing Dzuda",
     carob_date="2024-05-10"
   )
   
-  # read data 
-  
   f <- ff[basename(ff) == "DAT-PUB-Irrigation_CA_CT-Guanajuato-v2.xlsx"]
   r <- carobiner::read.excel(f, sheet = "Data field experiment")
-  
-  ## process file(s)
+    
   d <- data.frame(
     adm1=r$State,
     latitude=r$Latitude,
@@ -69,9 +49,7 @@ as well as fuel use and soil health data from a subset of modules. (2021-06-29)
     dmy_storage=r$Yield_dry,
     yield=r$Yield_moist,
     crop_price=r$Price
-    
   )
-  
   
   d$on_farm <- TRUE
   d$is_survey <- FALSE
@@ -85,14 +63,15 @@ as well as fuel use and soil health data from a subset of modules. (2021-06-29)
   d$land_prep_method <- gsub("PBW","permanent beds",d$land_prep_method)
   d$yield[1:19] <- d$yield[1:19]*1000
   
-  
   d$planting_date <- as.character(as.Date(d$planting_date))
+  d$harvest_date <- as.character(as.Date(d$harvest_date))
+  d$emergence_date  <- as.character(as.Date(d$emergence_date))
+
+  d[d=="."] <- NA
   d$plant_height <- as.numeric(d$plant_height)
   d$flowering_days <- as.integer(d$flowering_days)
   d$maturity_days <- as.integer(d$maturity_days)
   d$dmy_storage <- as.numeric(d$dmy_storage)
-  d$emergence_date  <- as.character(as.Date(d$emergence_date))
-  d$harvest_date <- as.character(as.Date(d$harvest_date))
   d$crop_price <- as.numeric(d$crop_price)/1000
   d$irrigation_number<- as.integer(d$irrigation_number)
   
