86022408995c2ee291eb66cd3fe09fc984e50973
rhijmans <r.hijmans@gmail.com>
2024-05-19 00:32:09 -0700

    m

--- scripts/conservation_agriculture/hdl_11529_10548578.R
+++ scripts/conservation_agriculture/hdl_11529_10548578.R
@@ -16,7 +16,7 @@ carob_script <- function(path) {
     publication=NA,
     project=NA,
     data_type= "on-farm experiment",
-	exp_treatment = "land_prep_method", #;irrigation_method
+	exp_treatments = "land_prep_method", #;irrigation_method
     carob_contributor= "Blessing Dzuda",
     carob_date="2024-05-10"
   )
