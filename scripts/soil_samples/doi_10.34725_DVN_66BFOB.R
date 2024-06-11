6d99395f3c51cb58a3bbdb328c2ed1258fb6fbee
rhijmans <r.hijmans@gmail.com>
2024-06-11 09:13:46 -0700

    m

--- scripts/soil_samples/doi_10.34725_DVN_66BFOB.R
+++ scripts/soil_samples/doi_10.34725_DVN_66BFOB.R
@@ -5,6 +5,7 @@ carob_script <- function(path) {
   
 "This dataset contains a subset of the samples collected during the AfSIS Phase I project and was a collaborative effort between World Agroforestry (ICRAF) and Rothamsted Research. The soil samples were retrieved from ICRAF Soil Archive: https://worldagroforestry.org/output/icraf-soil-archive-physical-archive-systematically-collected-soil-samples and subject to wet chemical analysis at Rothamsted Research in the UK under a Global Challenges Research Fund project, 'BBS/OS/GC/000014B: Chemical and Biological Assessment of AfSIS soils funded through the UK Biotechnology and Biological Sciences Research Council. This dataset includes the Site, Cluster, Plot as well as the GPS coordinates and wet chemistry data from 2002 samples collected from 18 countries and 51 LDSF sites. The original data collection was part of the AfSIS Phase I project, funded by the Bill and Melinda Gates Foundation (BMGF) and took place between 2009-2013. ICRAF and CIAT contributed the Site, Cluster, Plot and GPS coordinates for the soil samples, ICRAF organized the sub-sampling of the soil samples from the ICRAF physical archive in Nairobi and Rothamsted analysed the soil samples in the UK in 2017 and 2018. Visit our websites here: https://worldagroforestry.org/landhealth and https://www.rothamsted.ac.uk/. The AfSIS Phase I project funded by the Bill and Melinda Gates Foundation (BMGF) from 2009-2013, aimed to provide a consistent baseline of soil information across sub-Saharan Africa (SSA). Led by CIAT-TSBF, partners included: ISRIC, CIESIN, The Earth Institute at Columbia University and World Agroforestry (ICRAF). ICRAF led the systematic assessments of soil health using the Land Degradation Surveillance Framework (LDSF), which was developed at ICRAF, http://landscapeportal.org/blog/2015/03/25/the-land-degradation-surveillance-framework-ldsf/. LDSF sites were randomized using spatial stratification based on Koeppen-Geiger Climate zones across 19 countries in SSA. In total 60 LDSF sites were sampled. Soil samples were collected using the LDSF at two depths, 0-20 cm (labelled Topsoil) and 20-50 cm (labelled Subsoil). In each LDSF site, approximately 320 standard soil samples were collected. All of these were also scanned using MIR Spectroscopy and are available on Dataverse here: Vågen, Tor-Gunnar;Winowiecki, Leigh Ann;Desta, Luseged;Tondoh, Ebagnerin Jérôme;Weullow, Elvis;Shepherd, Keith;Sila, Andrew, 2020, 'Mid-Infrared Spectra (MIRS) from ICRAF Soil and Plant Spectroscopy Laboratory: Africa Soil Information Service (AfSIS) Phase I 2009-2013', https://doi.org/10.34725/DVN/QXCWP1, World Agroforestry - Research Data Repository, V1."
 
+
 	uri <- "doi:10.34725/DVN/66BFOB"
 	group <- "soil_samples"
 	ff  <- carobiner::get_data(uri, path, group)
@@ -26,16 +27,6 @@ carob_script <- function(path) {
 	#r2 <- carobiner::read.excel(f2, TRUE)
 #	r2[1:15,1:4]
 
-	# Replace -ve reported values with a small value equivalent to lowest detection limit
-## or are they wrong if < 0?
-## either way, this should be done with "d", not with "r"
-	r$eCEC[r$eCEC <= 0] <- 0.0001
-	r$Caex[r$Caex <= 0] <- 0.0001
-	r$Kex[r$Kex <= 0] <-0.0001
-	r$Mgex[r$Mgex <= 0] <- 0.0001
-	r$Naex[r$Naex <= 0] <- 0.0001
-	r$P[r$P <= 0] <- 0.0001
-	r$Percent_Org_C[r$Percent_Org_C<=0] <- 0.001
 	
 	d <- data.frame(
 		country = r$Country,
@@ -84,22 +75,22 @@ carob_script <- function(path) {
 	d$soil_sample_top[r$Depth=="Subsoil"] <- 20
 	d$soil_sample_bottom[r$Depth=="Subsoil"] <- 50
 
-	# correct country names
 	d$country[d$country == "SAfrica"] <- "South Africa"
 	d$country[d$country == "Zimbambwe"] <- "Zimbabwe"
 
-	# r$Site[is.na(as.numeric(r$Latitude))]
-	#bt <- r[r$Country=="Botswana",]
-	# Get geocodes for the two sites Prieska and Analavory
-	#p1 <- carobiner::geocode (country="South Africa", adm1="Prieska", location="Prieska")
-	#p2 <- carobiner::geocode (country="Madagascar", adm1="Analavory", location="Analavory")
-		# RH: the below may be correct but it is not comprehensible. Please fix
-	##r$Latitude[is.na(as.numeric(r$Latitude))] <-  c(-29.6776, rep(-18.9736, 125))
-	##r$Longitude[is.na(as.numeric(r$Longitude))] <-  c(22.7538,rep(46.9736, 125))	
-	# Geocode for Shoshong with points falling on water
-	#p3 <- carobiner::geocode (country="Botswana", adm1="Shoshong", location="Shoshong")
-	d$latitude[r$location == "Shoshong"] <- -23.07762
-	d$longitude[r$location == "Shoshong"] <- 26.5137
+	# Replace -ve reported values with a small value equivalent to lowest detection limit
+## or are they wrong if < 0?
+	d$soil_ECEC[d$soil_ECEC <= 0] <- 0.0001
+	d$soil_ex_Ca[d$soil_ex_Ca <= 0] <- 0.0001
+	d$soil_ex_K[d$soil_ex_K <= 0] <- 0.0001
+	d$soil_ex_Mg[d$soil_ex_Mg <= 0] <- 0.0001
+	d$soil_ex_Na[d$soil_ex_Na <= 0] <- 0.0001
+	d$soil_P_total[d$soil_P_total <= 0] <- 0.0001
+	d$soil_SOC[d$soil_SOC <= 0] <- 0.001
+
+## better not georeference by looking up names as the other locations seem quite precise.
+
+	d$longitude[d$location == "Shoshong"] <- NA
 
 	
 	carobiner::write_files(path, dset, d)
