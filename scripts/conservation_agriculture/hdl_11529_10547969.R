531aa25ab66022df062c4768ae68c972e81572d1
rhijmans <r.hijmans@gmail.com>
2024-06-11 09:43:52 -0700

    for #407

--- scripts/conservation_agriculture/hdl_11529_10547969.R
+++ scripts/conservation_agriculture/hdl_11529_10547969.R
@@ -15,11 +15,10 @@ carob_script <- function(path) {
   dset <- data.frame(
     carobiner::read_metadata(uri, path, group, major=2, minor=2),
     project=NA, 
-    publication= "doi.org/10.1016/j.fcr.2019.04.005", 
+    publication= "doi:10.1016/j.fcr.2019.04.005", 
     data_institute = "CIMMYT", 
     data_type="on-farm experiment", 
-	# also need to include land prep and perhaps other variables
-	  treatment_vars = "crop; crop_rotation; location; variety",
+	  treatment_vars = "land_prep_method;crop_rotation;longitude;latitude",
     carob_contributor="Mitchelle Njukuya", 
     carob_date="2024-05-21"
   )
@@ -32,7 +31,7 @@ carob_script <- function(path) {
     colnames(r1) <- gsub("Date.of.harvest.dd.mm.yy", "Datw.of.harvest.dd.mm.yy", colnames(r1))
    
     d1 <- data.frame(
-      treatment=r1$Tmnt, 
+      code=r1$Tmnt, 
       trial_id=paste0(r1$Node, "_", r1$Site.No), 
       location=r1$Node,
       variety=r1$Variety,
@@ -45,7 +44,7 @@ carob_script <- function(path) {
     r2 <- carobiner::read.excel.hdr(f, sheet ="6 - Fertilizer amounts ", skip=4, hdr=3)
     
     d2 <- data.frame(
-      treatment=r2$Tmnt,
+      code=r2$Tmnt,
       location=r2$Node,
       trial_id=paste0(r2$Node, "_", r2$Site.No),
       N_fertilizer=r2$N.kg.ha, 
@@ -64,7 +63,7 @@ carob_script <- function(path) {
     colnames(r3) <- gsub("Calculation_Calculation_Grain.yield.t.ha|Calculation_Grain.yield.t.ha|Calculation_Grain.Yield.t.ha", "Grain.yield.t.ha", colnames(r3))
                         
     d3 <- data.frame(
-      treatment=r3$Tmnt,
+      code=r3$Tmnt,
       location=r3$Node,
       trial_id=paste0(r3$Node, "_", r3$Site.No), 
       yield=r3$Grain.yield.t.ha * 1000,
@@ -73,58 +72,77 @@ carob_script <- function(path) {
     )
     
     ## merge all 
-    dd <- merge(d1, d2, by=c("treatment", "trial_id", "location"), all.x=TRUE)
-    dd <- merge(dd, d3, by=c("treatment", "trial_id", "location"), all.x=TRUE) 
+    dd <- merge(d1, d2, by=c("code", "trial_id", "location"), all.x=TRUE)
+    dd <- merge(dd, d3, by=c("code", "trial_id", "location"), all.x=TRUE) 
   }
   
   f0 <- grep("xlsx$", ff, value=TRUE)
   d0 <- lapply(f0, proc_data)
+
   d <- do.call(rbind, d0)
   
   
-  d$country <- "India"
-  d$on_farm <- TRUE
-  d$is_survey <- FALSE
-  d$irrigated <- TRUE
-  d$yield_part <- "grain" 
-  d$yield_part[d$crop == "lentil"] <- "seed"
+	d$country <- "India"
+	d$on_farm <- TRUE
+	d$is_survey <- FALSE
+	d$irrigated <- TRUE
+	d$yield_part <- "grain" 
+	d$yield_part[d$crop == "lentil"] <- "seed"
+
+	d$fertilizer_type <- gsub("MoP|MOP", "KCl", d$fertilizer_type)
+	d$fertilizer_type <- gsub("UREA", "urea", d$fertilizer_type)
+	d$fertilizer_type <- gsub("N:P:K|10-26-26|1899-12-31 10:26:26|:26:26", "NPK", d$fertilizer_type)
+	d$fertilizer_type <- gsub("[0-9]|-|:|,", '', d$fertilizer_type)
   
   
-  treatcode = c("CTTPR-RL-JTJ", "CTTPR-RL-ZTJ", "UPTPR-RL-ZTJ", "CTTPR-CTL-ZTJ", "CTTPR-ZTL-ZTJ", "UPTPR-CTL-ZTJ", "UPTPR-ZTL-ZTJ", 
-                "CTTPR-CTM", "UPTPR-ZTM", "CTTPR-ZTM", "UPTPR-CTM", "CTTPR-CTW", "UPTPR-ZTW", "CTTPR-CTW-ZTJ", 
-                "CTTPR-ZTW-ZTJ", "UPTPR-CTW-ZTJ", "UPTPR-ZTW-ZTJ")
+	code = c("CTTPR-RL-JTJ", "CTTPR-RL-ZTJ", "UPTPR-RL-ZTJ", "CTTPR-CTL-ZTJ", "CTTPR-ZTL-ZTJ", 
+		"UPTPR-CTL-ZTJ", "UPTPR-ZTL-ZTJ", "CTTPR-CTM", "UPTPR-ZTM", "CTTPR-ZTM", "UPTPR-CTM", "CTTPR-CTW", 
+		"UPTPR-ZTW", "CTTPR-CTW-ZTJ", "CTTPR-ZTW-ZTJ", "UPTPR-CTW-ZTJ", "UPTPR-ZTW-ZTJ")
   
-  rotationname = c("rice;rice_lentil;jute", "rice;rice_lentil;jute","rice;rice_lentil;jute", "rice;lentil;jute","rice;lentil;jute",
-                "rice;lentil;jute", "rice;lentil;jute", "rice;maize","rice;maize", "rice;maize", "rice;maize","rice;wheat",
-                "rice;wheat", "rice;wheat;jute", "rice;wheat;jute", "rice;wheat;jute", "rice;wheat;jute" )
+	rotation = c("rice;rice_lentil;jute", "rice;rice_lentil;jute","rice;rice_lentil;jute", "rice;lentil;jute", 
+			"rice;lentil;jute", "rice;lentil;jute", "rice;lentil;jute", "rice;maize","rice;maize", "rice;maize", "rice;maize","rice;wheat", "rice;wheat", "rice;wheat;jute", "rice;wheat;jute", "rice;wheat;jute", "rice;wheat;jute" )
+
+	treatname = c("conventional tillage transplanted puddled rice;rice_lentil;jute", 
+				"conventional tillage transplanted puddled rice;rice_lentil;jute",
+                "unpuddled transplanted rice;Rice_lentil;jute", 
+				"conventional tillage transplanted puddled rice;lentil;jute", 
+				"conventional tillage transplanted puddled rice;lentil;jute",
+                "unpuddled transplanted rice;lentil;jute", 
+				"unpuddled transplanted rice;lentil;jute", 
+				"conventional tillage transplanted puddled rice;maize",
+                "unpuddled transplanted rice;maize", 
+				"conventional tillage transplanted puddled rice;maize", 
+				"unpuddled transplanted rice;maize",
+				"conventional tillage transplanted puddled rice;wheat",
+                "unpuddled transplanted rice;wheat", 
+				"conventional tillage transplanted puddled rice;wheat;jute", 
+				"conventional tillage transplanted puddled rice;wheat;jute", 
+                "unpuddled transplanted rice;wheat;jute", 
+				"unpuddled transplanted rice;wheat;jute" )
   
-  treatname = c("Conventional tillage transplanted puddle rice;rice_lentil;jute", "Conventional tillage transplanted puddle rice;rice_lentil;jute",
-                "Unpuddle transplanted rice;Rice_lentil;jute", "Conventional tillage transplanted puddle rice;lentil;jute","Conventional tillage transplanted puddle rice;lentil;jute",
-                "Unpuddle transplanted rice;lentil;jute", "Unpuddle transplanted rice;lentil;jute", "Conventional tillage transplanted puddle rice;maize",
-                "Unpuddle transplanted rice;maize", "Conventional tillage transplanted puddle rice;maize", "Unpuddle transplanted rice;maize","Conventional tillage transplanted puddle rice;wheat",
-                "Unpuddle transplanted rice;wheat", "Conventional tillage transplanted puddle rice;wheat;jute", "Conventional tillage transplanted puddle rice;wheat;jute", 
-                "Unpuddle transplanted rice;wheat;jute", "Unpuddle transplanted rice;wheat;jute" )
   
-  d$crop_rotation <-rotationname[match(d$treatment,treatcode)]
-  d$treatment <- treatname[match(d$treatment,treatcode)]
+	land_prep = c("puddled", "puddled", "not puddled", "puddled", "puddled", "not puddled", "not puddled", "puddled", "not puddled", "puddled", "not puddled",	"puddled", "not puddled", "puddled", "puddled", "not puddled", "not puddled" )
+  
+	land_prep = sapply(treatname, \(i) unlist(strsplit(i, " "))[1]) 
+  
+	trt <- cbind(code=code, treatment=treatname, crop_rotation=rotation, land_prep_method=land_prep)
+	rownames(trt) <- NULL
+
+	d <- merge(d, trt, by="code")
+	d$code <- NULL
+	d$planting_method = "transplanted"
+
+	d$location <- gsub("DURGANAGAR","Durganagar", d$location)
+	d$location <- gsub("MANSAI","Mansai", d$location)
+	d$location <- gsub("GHUGHUMARI","Ghughumari", d$location)
+	d$location <- gsub("PATCHARA","Patchara", d$location)
  
   
-  d$fertilizer_type <- gsub("MoP|MOP", "KCl", d$fertilizer_type)
-  d$fertilizer_type <- gsub("UREA", "urea", d$fertilizer_type)
-  d$fertilizer_type <- gsub("N:P:K|10-26-26|1899-12-31 10:26:26|:26:26", "NPK", d$fertilizer_type)
-  d$fertilizer_type <- gsub("[0-9]|-|:|,", '', d$fertilizer_type)
-  d$location <- gsub("DURGANAGAR","Durganagar", d$location)
-  d$location <- gsub("MANSAI","Mansai", d$location)
-  d$location <- gsub("GHUGHUMARI","Ghughumari", d$location)
-  d$location <- gsub("PATCHARA","Patchara", d$location)
-  
-
-  
-  geo <- data.frame(
-    location=c("Durganagar", "Mansai", "Falimari", "Ghughumari", "Patchara"), 
-    latitude=c(24.7696, 25.2347, 26.0617, 26.3339, 24.4878), 
-    longitude=c(79.0309, 87.6779, 89.4739, 89.3261, 82.0019)
-  )
+	geo <- data.frame(
+		location=c("Durganagar", "Mansai", "Falimari", "Ghughumari", "Patchara"), 
+		latitude=c(24.7696, 25.2347, 26.0617, 26.3339, 24.4878), 
+		longitude=c(79.0309, 87.6779, 89.4739, 89.3261, 82.0019)
+	)
   
   d <- merge(d, geo, by="location", all.x = TRUE)  
   
