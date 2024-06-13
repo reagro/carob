5f1e163d10c6eb95bb59879db0ba419491825ce8
rhijmans <r.hijmans@gmail.com>
2024-06-13 07:00:03 -0700

    m

--- scripts/wheat_trials/hdl_11529_10548394.R
+++ scripts/wheat_trials/hdl_11529_10548394.R
@@ -1,14 +1,9 @@
 # R script for "carob"
 
-## ISSUES
-# ....
-
 
 carob_script <- function(path) {
 
-"
-Within the framework of SATYN, two types of nurseries are produced: SATYN series with odd numbers are lines for drought-stressed areas, and SATYN series with even numbers are lines for heat stress conditions. These nurseries have been phenotyped in the major wheat-growing mega environments through the International Wheat Improvement Network (IWIN) and the Cereal System Initiative for South Asia (CSISA) network, which included a total of 136 environments (site-year combinations) in major spring wheat-growing countries such as Bangladesh, China, Egypt, India, Iran, Mexico, Nepal, and Pakistan. (2009)
-"
+"Within the framework of SATYN, two types of nurseries are produced: SATYN series with odd numbers are lines for drought-stressed areas, and SATYN series with even numbers are lines for heat stress conditions. These nurseries have been phenotyped in the major wheat-growing mega environments through the International Wheat Improvement Network (IWIN) and the Cereal System Initiative for South Asia (CSISA) network, which included a total of 136 environments (site-year combinations) in major spring wheat-growing countries such as Bangladesh, China, Egypt, India, Iran, Mexico, Nepal, and Pakistan. (2009)"
 
   uri <- "hdl:11529/10548394"
   group <- "wheat_trials"
@@ -27,8 +22,6 @@ Within the framework of SATYN, two types of nurseries are produced: SATYN series
   
   proc_wheat <- carobiner::get_function("proc_wheat", path, group)
   d <- proc_wheat(ff)	
-  
-  d$previous_crop <- gsub("monsoon rice","rice",d$previous_crop)
   
   carobiner::write_files(path, dset, d)
 }
