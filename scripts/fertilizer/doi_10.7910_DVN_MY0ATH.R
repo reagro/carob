25c9c7d737419cbca78b1d0100a1520ab1b6b94f
rhijmans <r.hijmans@gmail.com>
2024-06-18 22:08:20 -0700

    m

--- scripts/fertilizer/doi_10.7910_DVN_MY0ATH.R
+++ scripts/fertilizer/doi_10.7910_DVN_MY0ATH.R
@@ -1,22 +1,15 @@
 # R script for "carob"
 
-## ISSUES
-# ....
-
 
 carob_script <- function(path) {
 
-"The data set evaluates adaptability and suitability of groundnut varieties to different ecozones.
-"
+"The data set evaluates adaptability and suitability of groundnut varieties to different ecozones."
 
-#### Identifiers
 	uri <- "doi:10.7910/DVN/MY0ATH"
 	group <- "fertilizer"
 
-#### Download data 
 	ff  <- carobiner::get_data(uri, path, group)
 
-##### dataset level metadata 
 	dset <- data.frame(
 		carobiner::read_metadata(uri, path, group, major=1, minor=4),
 		data_institute = "IITA",
@@ -29,8 +22,6 @@ carob_script <- function(path) {
 	)
 	
 ##### PROCESS data records
-
-# read data 
 
 	f <- ff[basename(ff) == "003_phosphorusRateonGroundnutMother_Ghana.xls"]
 	f1 <- ff[basename(ff) == "002_phosphorusRateonGroundnut_Ghana.xls"]
@@ -53,10 +44,8 @@ carob_script <- function(path) {
 		variety=r$VARTY,
 		plant_height=r$`PLNTH (cm)`,
 		flowering_days=r$DFFLW,
-		yield=r$`GYD/ha`,
-		residue_yield=NA,
-		dym_total=NA
-			)
+		yield=r$`GYD/ha`
+	)
 	
 	d2 <- data.frame(
 	  date="2015",
@@ -65,10 +54,9 @@ carob_script <- function(path) {
 	  P_fertilizer=r1$PRATE,
 	  variety=r1$VARTY,
 	  plant_height=r1$PLNTH,
-	  flowering_days=NA,
 	  yield=r1$`GYD /ha`,
 	  residue_yield=r1$`FODDER /ha`,
-	  dym_total=r1$TBMSS
+	  dmy_total=r1$TBMSS
 	)
 	#fixing names as given in label sheet
 	d1$site <- carobiner::replace_values(d1$site,c(1,2),c("Kassena Nankana","Bongo"))
@@ -86,7 +74,7 @@ carob_script <- function(path) {
 	  flowering_days=r2$DFFLW,
 	  yield=r2$GYD...17,
 	  residue_yield=r2$FODDER...13,
-	  dym_total=r2$TBMSS
+	  dmy_total=r2$TBMSS
 	)
 	
 	d4 <- data.frame(
@@ -99,7 +87,7 @@ carob_script <- function(path) {
 	  flowering_days=NA,
 	  yield=r3$GYD...10,
 	  residue_yield=r3$STOVER...12,
-	  dym_total=r3$TBMSS
+	  dmy_total=r3$TBMSS
 	)
 
 	#fixing names as given in label sheet
@@ -119,7 +107,7 @@ carob_script <- function(path) {
 	  flowering_days=r4$DFFLW,
 	  yield=r4$GRAIN,
 	  residue_yield=r4$FODDER,
-	  dym_total=r4$TBMM
+	  dmy_total=r4$TBMM
 	)
 	
 	d6 <- data.frame(
@@ -132,7 +120,7 @@ carob_script <- function(path) {
 	  flowering_days=NA,
 	  yield=r5$GRAIN,
 	  residue_yield=r5$FODDER,
-	  dym_total=r5$TBMM
+	  dmy_total=r5$TBMM
 	)
 
 	#fixing names as given in label sheet
@@ -145,27 +133,26 @@ carob_script <- function(path) {
 
 	d <- carobiner::bindr(d1, d2, d3, d4, d5, d6)	
 	
-	d$P_fertilizer <- carobiner::replace_values(d$P_fertilizer,c(1,2),c(60,90))
+	d$P_fertilizer <- carobiner::replace_values(d$P_fertilizer, c(1,2), c(60,90))
+# is this correct: ?
+	d$N_fertilizer <- 0
+	d$K_fertilizer <- 0
 	
-#### about the data #####
-
+	
 	d$on_farm <- TRUE
 	d$is_survey <- FALSE
 	d$irrigated <- FALSE
 	d$inoculated <- FALSE
-  d$crop <- "groundnut"
-  d$yield_part <- "seed"
+	d$crop <- "groundnut"
+	d$yield_part <- "seed"
 
 ##### Location #####
 	d$country <- "Ghana"
-	d$elevation <- NA
-	
 	geo <- data.frame(
 	  location=c("Bonia", "Nyangai", "Samboligo", "Tingoli", "Gbanjong", "Tibogunayili","Duko","Gorogori","Gyilly","Zanko","Pase"), 
 	  latitude=c(10.9078, 10.8950, 10.9078, 9.3750, 9.4319, 9.6242, 9.5636, 10.0606, 10.0606, 10.0676, 10.0367), 
 	  longitude=c(-0.8081, -1.0939, -0.8081, -1.0095, -1.0658, -0.8253, -0.8357, -2.5017, -2.5017, -2.5959, -2.7109)
 	)
-	
 	d <- merge(d, geo, by="location", all.x = TRUE)
    
 	carobiner::write_files(path, dset, d)
