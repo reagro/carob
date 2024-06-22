f371c9db10ece8508c2457f1d382ea04dd88c4cb
rhijmans <r.hijmans@gmail.com>
2024-06-22 09:48:03 -0700

    for #423

--- scripts/lateblight/doi_10.21223_6TRC9T.R
+++ scripts/lateblight/doi_10.21223_6TRC9T.R
@@ -1,13 +1,9 @@
-
-
 # R script for "carob"
-
 
 carob_script <- function(path) {
    
-   "
-   Panel evaluation for resistance to late blight in advanced potato clones from populations: B: B1C5, B3C1, B3C2 - 369 clones with resistance to Late Blight and LTVR- 294 clones with resistance to virus PVX, PVY and/or PLRV and BW - 24 clones with Bacterial Wilt resistant, under natural infection in Oxapampa, planted in September 2014 and harvested in January 2015, using observation plots, three replications/ten plants. Clones from population B showed in average AUDPC value of 185, and clones from population LTVR showed AUDPC average of 1630. This inforamtion will be used in manuscript “Global multi-environment resistance QTL for foliar late blight resistance in tetraploid potato with tropical adaptation”
-   " 
+"Panel evaluation for resistance to late blight in advanced potato clones from populations: B: B1C5, B3C1, B3C2 - 369 clones with resistance to Late Blight and LTVR- 294 clones with resistance to virus PVX, PVY and/or PLRV and BW - 24 clones with Bacterial Wilt resistant, under natural infection in Oxapampa, planted in September 2014 and harvested in January 2015, using observation plots, three replications/ten plants. Clones from population B showed in average AUDPC value of 185, and clones from population LTVR showed AUDPC average of 1630. This inforamtion will be used in manuscript “Global multi-environment resistance QTL for foliar late blight resistance in tetraploid potato with tropical adaptation"
+ 
    uri <- "doi:10.21223/6TRC9T"
    group <- "lateblight"
    ff  <- carobiner::get_data(uri, path, group)
@@ -23,14 +19,14 @@ carob_script <- function(path) {
       carob_date="2024-06-22"
    )
    
-   r <- carobiner::read.excel(ff[basename(ff)=="PTLate blight092014_OXAPMP_exp8.xlsx"],sheet = "Fieldbook")
+   r1 <- carobiner::read.excel(ff[basename(ff)=="PTLate blight092014_OXAPMP_exp8.xlsx"],sheet = "Fieldbook")
    
    d <- data.frame(
       crop="potato",
-      variety= r$INSTN,
-      rep= as.integer(r$REP),
-      yield=(r$TTWP/2.7 )*10000, # to kg/ha
-      AUDPC= r$AUDPC/100,
+      variety= r1$INSTN,
+      rep= as.integer(r1$REP),
+      yield=(r1$TTWP/2.7 )*10000, # to kg/ha
+      AUDPC= r1$AUDPC/100,
       on_farm= TRUE,
       irrigated= NA,
       inoculated= FALSE,
@@ -39,37 +35,35 @@ carob_script <- function(path) {
    )
    ## Add more variables
    m <- carobiner::read.excel(ff[basename(ff)=="PTLate blight092014_OXAPMP_exp8.xlsx"], sheet = "Minimal")
-   r1 <- data.frame(rbind(m$Value))
-   names(r1) <- m$Factor
-   d$country <- r1$Country
-   d$adm1 <- r1$Admin1
-   d$adm2 <- r1$Admin2
-   d$adm2 <- r1$Admin2
-   d$adm3 <- r1$Admin3
-   d$location <- r1$Locality
-   d$latitude <- as.numeric(r1$Latitude)
-   d$longitude <- as.numeric(r1$Longitude)
-   d$elevation <- as.numeric(r1$Elevation)
-   d$planting_date <- as.character(as.Date(r1$Begin_date, "%d/%m/%Y"))
-   d$harvest_date <- as.character(as.Date(r1$End_date, "%d/%m/%Y"))
+   r2 <- data.frame(rbind(m$Value))
+   names(r2) <- m$Factor
+   d$country <- r2$Country
+   d$adm1 <- r2$Admin1
+   d$adm2 <- r2$Admin2
+   d$adm2 <- r2$Admin2
+   d$adm3 <- r2$Admin3
+   d$location <- r2$Locality
+   d$latitude <- as.numeric(r2$Latitude)
+   d$longitude <- as.numeric(r2$Longitude)
+   d$elevation <- as.numeric(r2$Elevation)
+   d$planting_date <- as.character(as.Date(r2$Begin_date, "%d/%m/%Y"))
+   d$harvest_date <- as.character(as.Date(r2$End_date, "%d/%m/%Y"))
    d$plant_spacing <- 30 
    d$row_spacing <- 90
    d$plant_density <- 37037.03037037
    d$plot_area <- 2.7 # m2
    
-   ### Add disease scores during the season
-   lbvars <- grep('^LB', colnames(r), value=TRUE)
-   dd <- r[,lbvars]
+   ### disease scores during the season
+   lbvars <- grep('^LB', colnames(r1), value=TRUE)
+   dd <- r1[,lbvars]
    dd$record_id <- as.integer(1:nrow(dd))
    dates <- c("2014-10-31","2014-10-27", "2014-11-03", "2014-11-11", "2014-11-19",  "2014-11-25", "2014-12-02")
    x <- reshape(dd, direction="long", varying =lbvars, v.names="severity", timevar="step")
    x$time <- dates[x$step]
    x$step <- x$id <- NULL  
    
-   ##### fertilizer  #####################
-   d$N_fertilizer <- as.numeric(NA)
-   d$P_fertilizer <- as.numeric(NA)
-   d$K_fertilizer <- as.numeric(NA)
+   ### fertilizer
+   d$N_fertilizer <- d$P_fertilizer <- d$K_fertilizer <- as.numeric(NA)
    
    carobiner::write_files(path, dset, d,timerecs=x) 
    
