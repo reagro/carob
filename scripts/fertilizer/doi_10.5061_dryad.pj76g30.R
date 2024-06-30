70bf900c2a1323b0c711dd67471c5652ed301b3a
rhijmans <r.hijmans@gmail.com>
2024-06-29 18:08:40 -0700

    for #429

--- scripts/fertilizer/doi_10.5061_dryad.pj76g30.R
+++ scripts/fertilizer/doi_10.5061_dryad.pj76g30.R
@@ -3,39 +3,45 @@
 
 carob_script <- function(path) {
    
-   "
-   Fertilizer application can affect nutrient concentrations of edible plant products. Data from 70 crop-nutrient response trials conducted in Mali, Niger, Nigeria, and Tanzania were used to evaluate nutrient application effects on nutrient concentrations for grain of five pulse and five cereal crops and for storage roots of cassava (Manihot esculenta L.).
-   Treatments per trial were ≥12 but this study was limited to: no fertilizer applied; macronutrients applied (NPK or PK); and the macronutrient treatment plus Mg, S, Zn, and B applied (MgSZnB). Dried grain or cassava flour samples were analyzed for concentrations of all essential soil nutrients except for Ni and Cl. Concentrations of N and K were positively correlated with concentrations of most other nutrients.
-   The concentrations were relatively low overall for cowpea (Vigna unguiculata L.) and pigeonpea (Cajanus cajan L.) compared with other pulse crops and for maize (Zea mays L.) compared with other cereal crops. Application of NPK or PK had little effect on nutrient concentrations except for increased mean cereal grain concentrations for N, Ca, Mg, S, Zn, Cu, and B.
-   Bean (Phaseolus vulgaris L.), maize and rice (Oryza sativa L.) grain concentrations were reduced by MgSZnB for N, K, S, Cu, Mn, and B.There were no or inconsistent effects of MgSZnB on other crop-nutrient concentrations. Nutrient concentrations are not reduced by NPK for non-legumes or PK for pulses but MgSZnB often reduced bean and cereal nutrient concentrations with greater reductions for immobile compared with mobile nutrients.
-   "
-   uri <- "doi:10.5061/dryad.pj76g30"
-   group <- "fertilizer" 
+"Fertilizer application can affect nutrient concentrations of edible plant products. Data from 70 crop-nutrient response trials conducted in Mali, Niger, Nigeria, and Tanzania were used to evaluate nutrient application effects on nutrient concentrations for grain of five pulse and five cereal crops and for storage roots of cassava (Manihot esculenta L.).
+
+Treatments per trial were ≥12 but this study was limited to: no fertilizer applied; macronutrients applied (NPK or PK); and the macronutrient treatment plus Mg, S, Zn, and B applied (MgSZnB). Dried grain or cassava flour samples were analyzed for concentrations of all essential soil nutrients except for Ni and Cl. Concentrations of N and K were positively correlated with concentrations of most other nutrients.
+
+The concentrations were relatively low overall for cowpea (Vigna unguiculata L.) and pigeonpea (Cajanus cajan L.) compared with other pulse crops and for maize (Zea mays L.) compared with other cereal crops. Application of NPK or PK had little effect on nutrient concentrations except for increased mean cereal grain concentrations for N, Ca, Mg, S, Zn, Cu, and B.
+
+Bean (Phaseolus vulgaris L.), maize and rice (Oryza sativa L.) grain concentrations were reduced by MgSZnB for N, K, S, Cu, Mn, and B.There were no or inconsistent effects of MgSZnB on other crop-nutrient concentrations. Nutrient concentrations are not reduced by NPK for non-legumes or PK for pulses but MgSZnB often reduced bean and cereal nutrient concentrations with greater reductions for immobile compared with mobile nutrients."
+ 
+	uri <- "doi:10.5061/dryad.pj76g30"
+	group <- "fertilizer" 
    
-   ff  <- carobiner::get_data(uri, path, group)
+	ff  <- carobiner::get_data(uri, path, group)
    
-   dset <- data.frame(
-      carobiner::read_metadata(uri, path, group, major=1, minor=4), 
-      data_institute = "UNL", 
-      publication ="doi:10.2134/agronj2018.04.0274", 
-      project = NA, 
-      data_type = "experiment", 
-      treatment_vars = "N_fertilizer;P_fertilizer;P_fertilizer;K_fertilizer;S_fertilizer;Mg_fertilizer;Zn_fertilizer;B_fertilizer", 
-      carob_contributor = "Cedric Ngakou", 
-      carob_date = "2024-06-29"
-   )
+	dset <- data.frame(
+		carobiner::read_metadata(uri, path, group, major=1, minor=4), 
+		data_institute = "UNL", 
+		publication ="doi:10.2134/agronj2018.04.0274", 
+		project = NA, 
+		data_type = "experiment", 
+		treatment_vars = "N_fertilizer;P_fertilizer;P_fertilizer;K_fertilizer;S_fertilizer;Mg_fertilizer;Zn_fertilizer;B_fertilizer", 
+		carob_contributor = "Cedric Ngakou", 
+		carob_date = "2024-06-29"
+	)
    
-   ##### PROCESS data records
    f <- ff[basename(ff)=="Grain Nutrient Conc Study for Tropical Africa 2018.xlsx"]
    
    r <- carobiner::read.excel(f,sheet = "Data")
    
-#### soil information    
    d <- data.frame(
       country= r$Country,
       trial_id= r$Plot_code,
-      date= r$Year,
+      planting_date= as.character(r$Year),
       crop= r$Crop,
+      treatment= r$Contr1Macro2MgZnSB3,
+      on_farm= TRUE,
+      irrigated= NA,
+      is_survey= FALSE,
+
+	  # soil 
       soil_pH= r$pH,
       soil_B= r$B...21,
       soil_Cu= r$CU,
@@ -52,76 +58,8 @@ carob_script <- function(path) {
       soil_clay= r$Clay,
       soil_silt= r$Silt,
       soil_sand= r$Sand,
-      treatment= r$Contr1Macro2MgZnSB3,
-      on_farm= TRUE,
-      irrigated= NA,
-      inoculated= FALSE,
-      is_survey= FALSE
-   )
-   d$planting_date <- as.character(d$date)
-   d$date <- NULL
-   d$treatment <- c("control","NPK","MgSZnB")[d$treatment]
-   ### adding yield data ( from doi:10.2134/agronj2018.04.0274)
-   ### It is a mean value of yield per country and per crop 
-   ### Tanzania #############################
-   yd1 <- data.frame(
-         country= "Tanzania",
-         crop=c("Bean", "Cassava", "Cowpea", "Maize", "Pigeonpea", "Soybean", "Sorghum", "Wheat"),
-         yield1=c(1.83, 16.40, 2.35, 3.76, 2.08, 1.09, 4.33, 1.85), ### control treatment
-         yield2=c(2.06, 25.9, 2.61, 4.41, 2.29, 1.36, 4.935, 2.32), ### NPK trial treatment
-         yield3=c(1.83,19.21, 2.35, 376, 2.08, 1.09, 4.33, 2.18))   ### MgSZnB treatment
-   
-   ### Niger #############################
-   
-   yd2 <- data.frame(
-      country= "Niger",
-      crop=c("Cowpea", "Groundnut", "Maize", "Pearl millet", "Rice", "Sorghum"),
-      yield1=c(0.56, 0.64, 1.47, 0.66, 2.30, 1), ### control treatment
-      yield2=c(0.74, 0.87, 1.93, 0.87, 2.84, 1.43), ### NPK trial treatment
-      yield3= c(0.71, 0.79, 1.47, 0.66, 2.30, 1)) ### MgSZnB treatment
-   
-   ### Nigeria #############################
-   
-   yd3 <- data.frame(
-      country="Nigeria",
-      crop=c("Groundnut", "Maize", "Soybean", "Sorghum"),
-      yield1=c(1.07, 4.87, 1.83, 1.21), ### control treatment
-      yield2=c(1.74, 5.75, 2.42, 1.85), ### for NPK trial treatment
-      yield3=c(1.07, 4.87, 2.06, 1.47)) ### for MgSZnB treatment
-   
-   ### Mali #############################
-   yd4 <- data.frame(
-      country= "Mali",
-      crop=c("Maize", "Pearl millet", "Rice"),
-      yield1=c(2.82, 1.05, 2.21), # control treatment
-      yield2=c(4.03,1.24, 2.66),  # NPK trial treatment
-      yield3=c(2.82, 1.05, 2.21)) ## MgSZnB treatment
-   
-   #### joint yield data ##################################
-   yd <- carobiner::bindr(yd1,yd2,yd3,yd4)
-   
-   lst <- list()
-   var=c("1","2","3")
-   for (i in var) {
-      names(yd) <- gsub(i,"",names(yd))
-      yd <- yd[,c("country","crop","yield")]
-      yd$treatment  <- as.numeric(i)
-      lst[[i]] <- yd
-   }	
-   
-   d1 <- do.call(rbind, lst)
-   d1$treatment <- c("control","NPK","MgSZnB")[d1$treatment]
-  
-   d <- merge(d,d1,by=c("country","crop","treatment"),all.x = TRUE)
-   
-   p <- carobiner::fix_name(d$crop,"lower")
-   p <- gsub("bean","common bean",p)
-   p <- gsub("soycommon bean","soybean",p)
-   p <- gsub("pigeonpea","pigeon pea",p)
-   d$crop <- p
-   ### Nutrient contain in grain 
-   
-   d2 <- data.frame(
+
+	  # Nutrient contain in grain 
       grain_B= r$B...7,
       grain_Ca= r$Ca,
       grain_Cu= r$Cu,
@@ -133,10 +71,59 @@ carob_script <- function(path) {
       grain_S= r$S...16,
       grain_N= r$N
    )
+
+   d$treatment <- c("control", "NPK", "MgSZnB")[d$treatment]
+   ## adding yield data ( from doi:10.2134/agronj2018.04.0274)
+   ## It is a mean value of yield per country and per crop 
+   ## Tanzania 
+	yd1 <- data.frame(
+		country= "Tanzania",
+        crop=rep(c("Bean", "Cassava", "Cowpea", "Maize", "Pigeonpea", "Soybean", "Sorghum", "Wheat"), 3),
+		treatment = rep(c("control", "NPK", "MgSZnB"), each=8),
+        yield = c(1.83, 16.40, 2.35, 3.76, 2.08, 1.09, 4.33, 1.85, # control
+                   2.06, 25.9, 2.61, 4.41, 2.29, 1.36, 4.935, 2.32, # NPK
+                   1.83, 19.21, 2.35, 3.76, 2.08, 1.09, 4.33, 2.18)   # MgSZnB
+   )
+   ## Niger 
+   yd2 <- data.frame(
+      country= "Niger",
+      crop=rep(c("Cowpea", "Groundnut", "Maize", "Pearl millet", "Rice", "Sorghum"),3),
+	  treatment = rep(c("control", "NPK", "MgSZnB"), each=6),
+      yield=c(0.56, 0.64, 1.47, 0.66, 2.30, 1, # control
+				0.74, 0.87, 1.93, 0.87, 2.84, 1.43, # NPK
+				0.71, 0.79, 1.47, 0.66, 2.30, 1) # MgSZnB
+   )
+   ## Nigeria 
+   yd3 <- data.frame(
+      country="Nigeria",
+      crop=rep(c("Groundnut", "Maize", "Soybean", "Sorghum"), 3),
+	  treatment = rep(c("control", "NPK", "MgSZnB"), each=4),
+      yield=c(1.07, 4.87, 1.83, 1.21, # control
+				1.74, 5.75, 2.42, 1.85, # NPK
+				1.07, 4.87, 2.06, 1.47) #MgSZnB
+   )
+   ## Mali 
+   yd4 <- data.frame(
+      country= "Mali",
+      crop=rep(c("Maize", "Pearl millet", "Rice"), 3),
+	  treatment = rep(c("control", "NPK", "MgSZnB"), each=3),
+      yield=c(2.82, 1.05, 2.21, # control treatment
+				4.03,1.24, 2.66,  # NPK trial treatment
+				2.82, 1.05, 2.21) ## MgSZnB treatment
+   )
+
+   yd <- rbind(yd1, yd2, yd3, yd4)
+   yd$yield <- yd$yield * 1000 ## in kg/ha
    
-   d <- cbind(d,d2)
+   d <- merge(d, yd, by=c("country", "crop", "treatment"), all.x = TRUE)
    
-   ################ Adding fertilizer ###################
+   p <- carobiner::fix_name(d$crop, "lower")
+   p <- gsub("bean", "common bean", p)
+   p <- gsub("soycommon bean", "soybean", p)
+   p <- gsub("pigeonpea", "pigeon pea", p)
+   d$crop <- p
+   
+   ### fertilizer 
    d$N_fertilizer <- d$P_fertilizer <- d$P_fertilizer  <- d$K_fertilizer <- d$S_fertilizer <- d$Mg_fertilizer <-  d$Zn_fertilizer <- d$B_fertilizer <- 0
    
    d$N_fertilizer[d$treatment=="NPK"] <- 90
@@ -147,18 +134,13 @@ carob_script <- function(path) {
    d$Zn_fertilizer[d$treatment=="MgSZnB"] <- 3.5
    d$B_fertilizer[d$treatment=="MgSZnB"] <- 0.5
    
-   ###### adding longitude and latitude #############
-   ## The data and the document (published article) do not specify the site where the experiment was carried out.
-   ## should we set long and lat into NA instead of using country coordinate?
-   geo <- data.frame(country=c("Mali", "Niger", "Nigeria", "Tanzania"),
-                     latitude=c(16.3700359, 17.7356214, 9.6000359, -6),
-                     longitude=c(-2.2900239, 9.3238432, 7.9999721, 35))
-   
-   d <- merge(d,geo,by="country",all.x = TRUE)
-   d$yield <- d$yield*1000 ## in kg/ha
+   ## The data and the article do not specify the site where the experiment was carried out.
+	d$longitude <- d$latitude <- as.numeric(NA)
 
    d$yield_part= "grain"
    d$yield_part[d$crop=="cassava"] <- "roots"
+
+	d <- d[!is.na(d$treatment), ]
    
    carobiner::write_files(path, dset, d)
 }
