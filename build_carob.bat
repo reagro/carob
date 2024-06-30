bdf40c532b67620d3a417c31d2c18046fde7e0a9
rhijmans <r.hijmans@gmail.com>
2024-06-29 18:28:27 -0700

    m

--- build_carob.bat
+++ build_carob.bat
@@ -3,11 +3,11 @@
 IF %1.==. (GOTO cache) else (GOTO nocache)
 
 :cache
-Rscript.exe --vanilla -e "x <- carobiner::make_carob(path='.', cache=TRUE)" 
+Rscript.exe --vanilla -e "x <- carobiner::make_carob(path='.', cache=TRUE, report=TRUE)" 
 GOTO done
 
 :nocache
-Rscript.exe --vanilla -e "x <- carobiner::make_carob(path='.', cache=FALSE)"
+Rscript.exe --vanilla -e "x <- carobiner::make_carob(path='.', cache=FALSE, report=TRUE)"
 GOTO done
 
 :done
