a1a125660c01454be6dc78a67ca2832b7c1c08da
rhijmans <r.hijmans@gmail.com>
2024-07-04 09:49:18 -0700

    for #433

--- scripts/crop_cuts/hdl_11529_10548216.R
+++ scripts/crop_cuts/hdl_11529_10548216.R
@@ -1,15 +1,12 @@
-
 # R script for "carob"
 
 # NOTE 
 # Central  Statistical Authority  (CSA)
+
 carob_script <- function(path) {
    
-   "
-   Crop cut data collected by CSA in 2015 from 382 fields in West Showa, South West Showa, East and West Wollega, Jimma, Iliababur, East Showa, West and East Gojjam. Name of enumerator, name of farmer, gender, field coordinates (longitude, latitude, altitude), type of maize variety, name of variety, fertilizer use, fertilizer types, quantity (organic/inorganic), GPS location of the quadrant, number of crop stand, number of cobs, field weight of sub-samples, grain weight of sub-sample, weight of remaining cobs, field area (2016)
-   
-   "
-   
+"Crop cut data collected by CSA in 2015 from 382 fields in West Showa, South West Showa, East and West Wollega, Jimma, Iliababur, East Showa, West and East Gojjam. Name of enumerator, name of farmer, gender, field coordinates (longitude, latitude, altitude), type of maize variety, name of variety, fertilizer use, fertilizer types, quantity (organic/inorganic), GPS location of the quadrant, number of crop stand, number of cobs, field weight of sub-samples, grain weight of sub-sample, weight of remaining cobs, field area (2016)"
+
    uri <- "hdl:11529/10548216"
    group <- "crop_cuts"
    
@@ -26,8 +23,7 @@ carob_script <- function(path) {
       carob_date = "2024-07-04"
    )
    f <- ff[basename(ff) == "ET_Baseline_CSA_2015.xlsx"] 	
-   r <- carobiner::read.excel(f, sheet = "Revised_data")
-   r[r == "."] <- NA
+   r <- carobiner::read.excel(f, sheet = "Revised_data", na=c("", "."))
    d <- data.frame(
       country = "Ethiopia",
       latitude=r$latitude,
@@ -40,9 +36,11 @@ carob_script <- function(path) {
       variety=r$`Name of variety`,
       yield= r$`Moisture adjusted grain  yield (kg /ha)`,
       plot_area= 16, #16 m2
-      OM_used=as.logical(r$`Fertilizer type/organic`),
-      fertilizer_amount = as.numeric(r$`amount of Inorganic fertilizer`),
-      
+      OM_used= r$`Fertilizer type/organic`,
+      fertilizer_amount = r$`amount of Inorganic fertilizer`,
+	  plant_density  = 10000 * r$`Number of crop stands /16m2` / 16,
+	  cob_density = 10000 * r$`Number of cobs/16m2` / 16,
+
       ### Soil data
       soil_SOC = r$`Carbon (%)`,
       soil_pH  =r$pH,
@@ -60,17 +58,23 @@ carob_script <- function(path) {
       soil_B=r$`Boron  (mg/kg)`,
       soil_N = (r$`Nitrogen (%)`) * 10000 # from % to [mg/kg]
    )
-   
+	#OM_amount= r$`amount of organic fertilizer`,
+	#adjust with r$`Unit for Organic fertilizer`
+  
    d$variety_type <- gsub("Improved_variety", "improved", d$variety_type)
    d$variety_type <- gsub("local_variety", "local", d$variety_type)
    
    d$planting_date <- as.character(NA)
-   d$trial_id <- "1"
    d$on_farm <- TRUE
    d$is_survey <- TRUE
    d$irrigated <- as.logical(NA)
    d$N_fertilizer <- d$P_fertilizer <- d$K_fertilizer <- as.numeric(NA)
    
+	# in a survey each record is a separate "trial"
+   d$trial_id <- as.character(1:nrow(d))
+   
+   ## taking out some of the crazy ones. Others still very high.
+   d$fertilizer_amount[d$fertilizer_amount > 2000] <- NA
    
    carobiner::write_files(path, dset, d)
    
