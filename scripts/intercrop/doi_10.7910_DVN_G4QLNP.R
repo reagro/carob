b3536927c5ae05123e116c5cc73e9833b389af0b
rhijmans <r.hijmans@gmail.com>
2024-07-04 22:16:28 -0700

    m

--- scripts/intercrop/doi_10.7910_DVN_G4QLNP.R
+++ scripts/intercrop/doi_10.7910_DVN_G4QLNP.R
@@ -24,8 +24,9 @@ cropping system (sole maize, sole pigeonpea, maize-pigeonpea, maize-gliricidia, 
 		carob_date="2024-04-09",
 		treatment_vars="intercrops"
 	)
-	
-	f1 <- ff[basename(ff) == "006_siDom_droughtResistance.csv"]
+
+	#use grep because the filename has a space on linux	
+	f1 <- ff[grep("006_siDom_droughtResistance.csv", basename(ff))]
 	r1 <- read.csv(f1)
 	f2 <- ff[basename(ff) == "007_siteCharacterization_droughtResistance.csv"]
 	r2 <- colMeans(read.csv(f2))
