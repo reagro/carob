c7ef1b6f5ffcf29847a4fa25e4be546d43df1936
rhijmans <r.hijmans@gmail.com>
2024-07-11 21:24:01 -0700

    for #445

--- scripts/variety_trials/doi_10.21421_D2_LC1VFT.R
+++ scripts/variety_trials/doi_10.21421_D2_LC1VFT.R
@@ -1,12 +1,9 @@
-
-
-### Note
+# R script for "carob"
 
 
 carob_script <- function(path) {
    
-   
-   " This dataset includes the research work carried out in evaluating marker assisted backcross (MABC) lines for rust and late leaf spot (LLS) resistance in five locations during 2015 rainy season. The evaluation were carried out at Aliyarnagar, Tamil Nadu; Directorate of Groundnut Research (DGR) Junagadh, Gujarat; Dharwad, Karnataka; ICRISAT, Patancheru and Kasbe Digraj, Maharashtra. A set of 41 ILs – 18 ILs in ICGV 91114 background, 14 ILs in TAG 24 background and 9 in JL 24 background along with recurrent parents – ICGV 91114, TAG 24, JL24; donor parent – GPBD 4 and three local checks were evaluated. Multi-location evaluation is useful to identify stable performing entries across locations to facilitate their release both at national and state level. Based on the data on disease score, yield and yield associated traits (Shelling percent and Hundred seed weight), 12 ILs were selected and used for conducting first ever near isogenic line (NIL) trial under All India Co-ordinated Research Project on Groundnut (AICRP-G). Subsequently three best performing ILs were identified and advanced to Advanced Varietal Trials (AVT-1 & AVT-2) under AICRP-G. The data presented here is the mean location wise performance of the entries for disease resistance, yield and associated traits that enabled selection and advancement of best performing ILs at Kasbe Digraj, Maharashtra, India location. "
+" This dataset includes the research work carried out in evaluating marker assisted backcross (MABC) lines for rust and late leaf spot (LLS) resistance in five locations during 2015 rainy season. The evaluation were carried out at Aliyarnagar, Tamil Nadu; Directorate of Groundnut Research (DGR) Junagadh, Gujarat; Dharwad, Karnataka; ICRISAT, Patancheru and Kasbe Digraj, Maharashtra. A set of 41 ILs – 18 ILs in ICGV 91114 background, 14 ILs in TAG 24 background and 9 in JL 24 background along with recurrent parents – ICGV 91114, TAG 24, JL24; donor parent – GPBD 4 and three local checks were evaluated. Multi-location evaluation is useful to identify stable performing entries across locations to facilitate their release both at national and state level. Based on the data on disease score, yield and yield associated traits (Shelling percent and Hundred seed weight), 12 ILs were selected and used for conducting first ever near isogenic line (NIL) trial under All India Co-ordinated Research Project on Groundnut (AICRP-G). Subsequently three best performing ILs were identified and advanced to Advanced Varietal Trials (AVT-1 & AVT-2) under AICRP-G. The data presented here is the mean location wise performance of the entries for disease resistance, yield and associated traits that enabled selection and advancement of best performing ILs at Kasbe Digraj, Maharashtra, India location. "
    
    uri <- "doi:10.21421/D2/LC1VFT"
    dataset_id <- carobiner::simple_uri(uri)
@@ -33,7 +30,7 @@ carob_script <- function(path) {
          yield= r$PYH,
          seed_weight = as.numeric(r$HSW)* 10, ## 1000 seed weight,
          diseases = "late leaf spot;rust",
-         trial_id= basename(f)
+         trial_id= gsub(".xlsx", "", basename(f))
       )
       d
       
@@ -43,19 +40,20 @@ carob_script <- function(path) {
    d <- do.call(rbind,d)
    
    d$country <- "India"
-   d$location <- "Maharashtra"
+   d$adm1 <- "Maharashtra"
+   d$location <- "Kasbe Digraj"
    d$crop <- "groundnut"
    
    d$irrigated <- TRUE
-   d$irrigated[grep("RF KDG.xlsx",d$trial_id)] <- FALSE 
+   d$irrigated[grep("RF KDG", d$trial_id)] <- FALSE 
    d$on_farm <- TRUE
    d$is_survey <- FALSE
    d$inoculated <- FALSE
    d$yield_part <- "pod"
    
    d$planting_date <- "2015"
-   d$longitude  <- 75.6741579
-   d$latitude  <-  18.9068356
+   d$longitude  <- 74.5125
+   d$latitude  <-  16.9095
    
    d$N_fertilizer <- d$P_fertilizer <- d$K_fertilizer <- as.numeric(NA)
    
