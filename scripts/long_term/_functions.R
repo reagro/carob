412ad3dbbee105bcf46517b3281c11f8bb2f0603
rhijmans <r.hijmans@gmail.com>
2024-07-14 18:16:25 -0700

    f

--- scripts/long_term/_functions.R
+++ scripts/long_term/_functions.R
@@ -20,16 +20,15 @@ IRRI_LTE <- function(f) {
 		season = r$Season,
 		crop = tolower(r$Crop),
 		yield_part = "grain",
-		LTE_ID = r$Crop_no,
 		rep = r$Rep,
 		yield = r$GYtha * 1000,
 		N_fert_level = as.integer(gsub("F", "", r$Afactor)),
 		variety_code = paste0(r$Year, "_", r$Bfactor),
-		trial_id = paste0(r$Year, "_", r$Season)
+		trial_id = as.character(r$Crop_no)
 	)
 	x$N_fert_level <- c("zero", "low", "moderate", "high")[x$N_fert_level]
 	i <- match(x$season, c("DS", "EWS", "LWS"))
-	x$season <- c("dry", "early wet", "late wet")[x$season]
+	x$season <- c("dry", "early wet", "late wet")[i]
 	x$N_fertilizer <- x$P_fertilizer <- x$K_fertilizer <- as.numeric(NA)
 	x$N_fertilizer[x$N_fert_level == "zero"] <- 0
 	x
