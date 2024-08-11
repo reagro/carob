bfe528aec9ef41c7121897dc2f89632e442d8eea
rhijmans <r.hijmans@gmail.com>
2024-08-11 04:17:12 -0700

    for #483

--- scripts/agronomy/doi_10.5061_dryad.8w9ghx3q6.R
+++ scripts/agronomy/doi_10.5061_dryad.8w9ghx3q6.R
@@ -18,62 +18,88 @@ carob_script <- function(path) {
       response_vars= "yield", 
       treatment_vars = "variety;intercrops", 
       carob_contributor= "Cedric Ngakou", 
-      carob_date="2024-08-10"  
+      carob_date="2024-08-10",
+      notes="30-60cm soil data not yet processed"	  
    )
    
-   ff <- ff[grepl("xlsx", basename(ff))]
-   
+  
    process <- function(f){ 
       
+	  fill_missing <- function(x, v) {
+		a <- !is.na(x[v[1]])
+		x[, v] <- x[, v][which(a)[c(1, 1:sum(a))][cumsum(a)+1], ]
+		x
+	  }
+	  
       r1 <- carobiner::read.excel.hdr(f, sheet = "Agronomy Data", skip=0)
+	  r1 <- fill_missing(r1, c("Farm.No", "Latitute.degree", "Longitude.degree", "altitude.m"))
+
+
    ## Fixing longitude and latitude  
-      for (i in 2:nrow(r1)) if (is.na(r1$Latitute.degree[i])) r1$Latitute.degree[i] <- r1$Latitute.degree[i-1]
-      for (i in 2:nrow(r1)) if (is.na(r1$Longitude.degree[i])) r1$Longitude.degree[i] <- r1$Longitude.degree[i-1]
-      for (i in 2:nrow(r1)) if (is.na(r1$altitude.m[i])) r1$altitude.m[i] <- r1$altitude.m[i-1]
-      for (i in 2:nrow(r1)) if (is.na(r1$Farm.No[i])) r1$Farm.No[i] <- r1$Farm.No[i-1]
-      
-   ### Agronmy data
+
+       #for (i in 2:nrow(r1)) {
+	#	if (is.na(r1$Latitute.degree[i])) {
+	#		r1$Latitute.degree[i] <- r1$Latitute.degree[i-1]
+     #       r1$Longitude.degree[i] <- r1$Longitude.degree[i-1]
+      #      r1$altitude.m[i] <- r1$altitude.m[i-1]
+       #     r1$Farm.No[i] <- r1$Farm.No[i-1]
+       # }
+       #}
+	   
+   ## Agron0my data
       d1 <- data.frame(
+		 trial_id = r1$Farm.No,
          latitude= r1$Latitute.degree,
          longitude= r1$Longitude.degree,
          elevation= r1$altitude.m,
          crop= r1$Crop.species,
          variety= r1$Cultivar.variety,
-         intercrops = ifelse(grepl("Maize/beans Intercrop",r1$Cropping.System)& r1$Crop.species=="maize", "common bean",
-                      ifelse(grepl("Maize/beans Intercrop",r1$Cropping.System)& r1$Crop.species=="Beans", "maize", "none")),
+         intercrops = ifelse(grepl("Maize/beans Intercrop", r1$Cropping.System)& r1$Crop.species=="maize", "common bean",
+                      ifelse(grepl("Maize/beans Intercrop", r1$Cropping.System)& r1$Crop.species=="Beans", "maize", "none")),
       
          planting_date= as.character(as.Date(r1$Plant.date, "%d/%m/%Y")),
          weeding_dates= paste0(as.Date(r1$First.weeding.date, "%d/%m/%Y"), ";", as.Date(r1$Second.weeding.date, "%d/%m/%Y")),
          harvest_date=  as.character(as.Date(r1$Harvest.date, "%d/%m/%Y")),
          yield= r1$Yield.production.Ton.ha*1000, # kg/ha
-         season= substr(gsub("_", "-", basename(f)),26,34),
+         season= substr(gsub("_", "-", basename(f)), 26, 34),
          country= "South Africa",
-         trial_id= r1$Farm.No
+		 f = f
          )
   
    ## soil data 
-       r2 <- carobiner::read.excel.hdr(f,sheet = "Soil data",skip=0)
+   #here you could do 
+   #    r2 <- carobiner::read.excel.hdr(f, sheet = "Soil data", hdr=1, skip=1)
+
+       r2 <- carobiner::read.excel(f, sheet = "Soil data", fix_names=TRUE)
        r2 <- r2[-1,] ## removing the first row
-### Fixing longitude and latitude
-       for (i in 2:nrow(r2)) if (is.na(r2$Latitute.degree[i])) r2$Latitute.degree[i] <- r2$Latitute.degree[i-1]
-       for (i in 2:nrow(r2)) if (is.na(r2$Longitude.degree[i])) r2$Longitude.degree[i] <- r2$Longitude.degree[i-1]
-       for (i in 2:nrow(r2)) if (is.na(r2$altitude.m[i])) r2$altitude.m[i] <- r2$altitude.m[i-1] 
-        names(r2) <- gsub("pH.H2O", "pH",names(r2))
-   
+        names(r2) <- gsub("Farmer.No", "Farm.No", names(r2))
+   ## Fixing longitude and latitude
+
+ 	  r2 <- fill_missing(r2, c("Farm.No", "Latitute.degree", "Longitude.degree", "altitude.m"))
+ 
+		#for (i in 2:nrow(r2)) if (is.na(r2$Latitute.degree[i])) r2$Latitute.degree[i] <- r2$Latitute.degree[i-1]
+       #for (i in 2:nrow(r2)) if (is.na(r2$Longitude.degree[i])) r2$Longitude.degree[i] <- r2$Longitude.degree[i-1]
+       #for (i in 2:nrow(r2)) if (is.na(r2$altitude.m[i])) r2$altitude.m[i] <- r2$altitude.m[i-1] 
+        names(r2) <- gsub("pH.H2O", "pH", names(r2))
+        names(r2) <- gsub("Particle.size.distribution", "Sand", names(r2))
+        names(r2) <- gsub("X.14", "Silt", names(r2))
+        names(r2) <- gsub("X.15", "Clay", names(r2))
+
+       if (!is.null(r2$Depth))  r2 <- r2[r2$Depth == "0-30", ]
+
        if(is.null(r2$S)) r2$S <- NA
        if(is.null(r2$EC)) r2$EC <- NA
        if(is.null(r2$CEC)) r2$CEC <- NA
-       if(is.null(r2$Particle.size.distribution)) r2$Particle.size.distribution <- NA
-       if(is.null(r2$X.14))  r2$X.14 <- NA
-       if(is.null(r2$X.15))  r2$X.15 <- NA
-       if(is.null(r2$Depth))  r2$Depth <- NA
+       if(is.null(r2$Clay))  r2$Clay <- NA
+       if(is.null(r2$Silt))  r2$Silt <- NA
+       if(is.null(r2$Sand))  r2$Sand <- NA
+       if(is.null(r2$EA)) r2$EA <- NA
+       if(is.null(r2$AS)) r2$AS <- NA
    
       d2 <- data.frame(
-         latitude= r2$Latitute.degree,
-         longitude= r2$Longitude.degree,
-         elevation=  r2$altitude.m,
-         soil_sample_bottom= substr(r2$Depth, 3, 5),
-         soil_sample_top = substr(r2$Depth, 1, 2),
+		 trial_id = r2$Farm.No,
+         soil_sample_bottom= 30,
+         soil_sample_top = 0,
          soil_pH= as.numeric(r2$pH),
          soil_P_available= as.numeric(r2$P),
          soil_Ca= as.numeric(r2$Ca),
@@ -81,23 +107,26 @@ carob_script <- function(path) {
          soil_K= as.numeric(r2$K),
          soil_Na= as.numeric(r2$N),
          soil_EC= as.numeric(r2$EC),
-         soil_sand= as.numeric(r2$Particle.size.distribution),
-         soil_silt= as.numeric(r2$X.14),
-         soil_clay= as.numeric(r2$X.15),
+         soil_sand= as.numeric(r2$Sand),
+         soil_silt= as.numeric(r2$Silt),
+         soil_clay= as.numeric(r2$Clay),
          soil_S= as.numeric(r2$S),
-         soil_CEC= as.numeric(r2$CEC)
+         soil_CEC= as.numeric(r2$CEC),
+		 soil_ex_Al = as.numeric(r2$EA),
+		 soil_Al_sat = as.numeric(r2$AS)
          )
       
-      merge(d1,d2,by=c("longitude", "latitude", "elevation"), all.x = TRUE)
-  
+        # merge(d1,d2,by=c("longitude", "latitude", "elevation"), all.x = TRUE)
+        merge(d1, d2, by="trial_id", all.x = TRUE)
+ 
    }
    
    d <- lapply(ff, process)
    d <- do.call(rbind,d)
+	d$trial_id = as.character(as.integer(as.factor(paste(d$trial_id, d$f))))
+	d$f <- NULL
    d$weeding_dates[grepl("2029-01-15",d$weeding_dates)] <- "2019-01-15"
    d$crop <- gsub("Beans", "common bean", d$crop)
-   d$soil_sample_bottom <- as.numeric(gsub("-60", 60, d$soil_sample_bottom))
-   d$soil_sample_top <- as.numeric(gsub("0-", 0, d$soil_sample_top))
    
    d$irrigated <- NA
    d$on_farm <- TRUE
@@ -109,7 +138,7 @@ carob_script <- function(path) {
    d$adm1 <- "Free State"
    d$geo_from_source <- TRUE
    
-   ### adding information from data description
+   ## adding information from data description
    d$plant_spacing <- 90
    d$row_spacing <- 25
    d$row_spacing[d$crop=="common bean"] <- 5
@@ -121,20 +150,20 @@ carob_script <- function(path) {
    d$herbicide_used  <- TRUE
  
      
-### record weather data 
+## weather data 
    
   weather <- function(f){
      Sheet <- "Climate data"
      if (grepl("2019_2020", basename(f)) ) Sheet <- "Sheet1"
      w <- carobiner::read.excel(f, sheet = Sheet, na=c("--",NA))
-     ## treat header 
-     names(w) <- w[grep("Year", w[,1])[1], ] ## 
-     w <- w[-1,1:11] ### keep rows with useful information 
-     ## removing separate line between trials to get continuous data 
-     ind=grep("Average|Total|Highest|Lowest|Day", w[,3]) 
-     w <- w[-ind,] 
-     w <- w[!is.na(w$Year),]
-     ## process
+     # set names
+     names(w) <- w[grep("Year", w[,1])[1], ]
+	 # keep rows with useful information 
+	 keeprows <- which(substr(w$Year, 1, 1) == "2")
+     w <- w[keeprows, ] 
+     remrows <- grep("Average|Total|Highest|Lowest|Day", w[,3]) 
+     w <- w[-remrows,]
+	 
      data.frame(
         country = "South Africa",
         date = as.character(as.Date(paste(w$Day, w$Month, w$Year, sep="-"), "%d-%m-%Y")),
@@ -157,7 +186,7 @@ carob_script <- function(path) {
     dw <- lapply(ff, weather)
     dw <- do.call(rbind,dw)  
     ## remove empty rows in date 
-    dw <- dw[!is.na(dw$date),] 
+    dw <- dw[!is.na(dw$date), ] 
 
 	d$soil_pH[d$soil_pH < 1] <- NA
    carobiner::write_files(path, meta, d,wth = dw)
