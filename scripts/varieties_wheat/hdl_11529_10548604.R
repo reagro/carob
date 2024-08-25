6c3167e3412af53ea82976c2d8b9f3190b951067
Fredy <fredy.chimire@econet.co.zw>
2024-08-25 21:19:47 +0200

    Standardised dataset hdl_11529_10548604.R

---
+++ scripts/varieties_wheat/hdl_11529_10548604.R
@@ -0,0 +1,27 @@
+# R script for "carob"
+
+
+carob_script <- function(path) {
+  
+  "The WYCYT international nurseries are the result of research conducted to raise the yield potential of spring wheat through the strategic crossing of physiological traits related to source and sink potential in wheat. These trials have been phenotyped in the major wheat-growing mega environments through the International Wheat Improvement Network (IWIN) and the Cereal System Initiative for South Asia (CSISA) network, which included a total of 136 environments (site-year combinations) in major spring wheat-growing countries such as Bangladesh, China, Egypt, India, Iran, Mexico, Nepal, and Pakistan. (2020)"
+  uri <- "hdl:11529/10548604"
+  group <- "varieties_wheat"
+  ff  <- carobiner::get_data(uri, path, group)
+  
+  meta <- data.frame(
+    carobiner::read_metadata(uri, path, group, major=2, minor=0),
+    data_institute = "CIMMYT",
+    publication= NA,
+    project="Wheat Yield Collaboration Yield Trial",
+    data_type= "experiment",
+    carob_contributor= "Fredy Chimire",
+    carob_date="2024-08-25",
+    response_vars = "yield",
+    treatment_vars = "variety_code"
+  )
+  
+  proc_wheat <- carobiner::get_function("proc_wheat", path, group)
+  d <- proc_wheat(ff)	
+  
+  carobiner::write_files(path, meta, d)
+}
