b9741f7795e4d2384584e2f4fd7e8724a8772a32
egbendito <e.gbendito@gmail.com>
2024-08-28 14:27:08 +0000

    update _pending/agronomy/doi_10.7910_DVN_LJPW4O.R

--- scripts/_pending/agronomy/doi_10.7910_DVN_LJPW4O.R
+++ scripts/_pending/agronomy/doi_10.7910_DVN_LJPW4O.R
@@ -2,6 +2,10 @@
 
 #RH: needs a lot more work
 
+# Notes EGB (2024-08-28):
+# # 1. It has been worked and a lot of the missing data is now captured.
+# # 2. The data includes 2 seasons but the protocols only indicate 1.
+# # 3. Biomass information (2a Dry matter measurements.xlsx) contains in-season measurements of biomass that carob cannot capture.
 
 carob_script <- function(path) {
 
@@ -18,100 +22,168 @@ carob_script <- function(path) {
 		carob_date="2021-06-18",
 		data_type = "experiment",
 		data_institute="CIAT",
-		project="AgMIP"
-		treatment_vars = "N_fertilizer;P_fertilizer",
-		response_vars = "yield"
+		project="AgMIP",
+		treatment_vars = "N_fertilizer;P_fertilizer;K_fertilizer;Zn_fertilizer;Ca_fertilizer;Mg_fertilizer;OM_used",
+		response_vars = "yield;fwy_residue;leaf_N;leaf_P;leaf_K;leaf_Ca;leaf_Mg;grain_N;grain_P;grain_K;grain_Ca;grain_Mg;residue_N;residue_P;residue_K;residue_Ca;residue_Mg",
+		last_modified = "2024-08-28",
+		notes = "Assuming that the biomass, stover and soil records correspond to season 1. Season 2 is nowhere indicated in the protocols"
 	)
 
 
 	f <- ff[basename(ff) == "9a Yield data.xlsx"]
 	r <- carobiner::read.excel(f)
+	
+	height <- carobiner::read.excel(ff[basename(ff) == "5a Plant growth data.xlsx"])
+	height <- aggregate(height$`Plant Height (CM)`, by = list(height$Site, height$Season, height$Block, height$Treatment), FUN = mean, na.action = na.omit)
+	colnames(height) <- c("Site", "Season", "Block", "Treatment", "plant_height")
+	
+	r1 <- merge(r, height, c("Site", "Season", "Block", "Treatment"))
 
-	# no season variable hence cannot be merged with r
-	# biomass <- carobiner::read.excel(ff[basename(ff) == "2a Dry matter measurements.xlsx"])
-
-	# Manure applied ("1a Cattle manure lab analysis.xlsx")
-	# Missing information on amount of manure applied.
-	# Have sent an email to Job Kihara to find out about the manure applied (2021/09/17)
 	###########
-
-	# Soil data ("8a Soil lab data.xlsx")
-	soil <- carobiner::read.excel(ff[basename(ff) == "8a Soil lab data.xlsx"], skip = 12)
 
 	d <- data.frame(
 		country = "Uganda",
 		adm1 = "Wakiso",
 		adm2 = "Jinja",
 		adm3 = ifelse(r$Site == "Kawanda", "Nabweru", "Busukuma"),
+		season = r$Season,
 		trial_id = r$Site,
+		block = r$Block,
+		treatment = r$Treatment,
 		latitude = ifelse(r$Site == "Kawanda", 0.4172778, 0.5256090),
 		longitude = ifelse(r$Site == "Kawanda", 32.5355326, 32.6136960),
-		planting_date = "2013-08-10",
+		geo_from_source = FALSE,
+		planting_date = ifelse(r$Season == 1, "2013-08-10", "2014-03"),
 		harvest_date = "2014",
 		on_farm = FALSE,
-		is_survey = TRUE,	
+		is_survey = FALSE,	
 		crop = "maize",
-		yield_part = "grain",
 		variety = "Longe 10H",
-		variety_code = "SC627"
+		variety_code = "SC627",
+		yield = r$`Grain yield (kg/plot -5.625m2)` * (10000/5.625),
+		fwy_residue = r$`Stover yield (kg/plot - 5.625m2)` * (10000/5.625),
+		yield_part = "grain",
+		insecticide_used = TRUE,
+		insecticide_product = "Dursban",
+		insecticide_timing = 60,
+		herbicide_used = TRUE,
+		herbicide_product = "glyphosate",
+		land_prep_method = "ploughing",
+		plant_density = 53333
 	)
-						  
-						  
-	# Adding Treatment information
-	d1$fertilizer_type <- "urea"
-	d1$N_fertilizer <- ifelse(r$Treatment %in% c(8,7,6,5,4), 200,
-	                   ifelse(r$Treatment == 3, 60, 0))
-	d1$N_splits <- NA
-	d1$N_splits[d1$N_fertilizer > 0] <- 3L
-
-	d1$P_fertilizer <- ifelse(r$Treatment %in% c(8,7,3,2), 90,
-						 ifelse(r$Treatment %in% c(6), 50,
-						   ifelse(r$Treatment %in% c(5), 20, 0)))
-	d1$K_fertilizer <- ifelse(r$Treatment == 1, 0, 75)
-	d1$Zn_fertilizer <- ifelse(r$Treatment == 1, 0, 75)
 	
+	# Adding Treatment information
+	# EGB:
+	# # From Protocol:
+	# # "Nitrogen will be applied (30%) in planting holes as CAN (27.5%N) in order to supply some calcium (CaO – 3.5%)
+	# # and magnesium (MgO – 4%);  30 DAP (30%) and 60 DAP (40%) as Urea (46%) in bands and incorporated or covered with soil.
+	# # Potassium will be applied as muriate of potash (52% K), and P as triple super phosphate (20% P) at planting.
+	# # Zinc, Boron and Molybdenum - 5 Zn, 0.5 Mo, 1B (kg ha−1) will be applied at planting using Zinc sulphate,
+	# # Sodium molybdate and Borax to eliminate deficiencies, except for treatment (8) which will instead receive manure (5 Mg ha−1) at planting."
+	d$N_fertilizer <- ifelse(r$Treatment %in% c(8,7,6,5,4), 200,
+	                         ifelse(r$Treatment == 3, 60, 0))
+	d$N_splits <- NA
+	d$N_splits[d$N_fertilizer > 0] <- 3L
+	
+	d$P_fertilizer <- ifelse(r$Treatment %in% c(8,7,3,2), 90,
+	                         ifelse(r$Treatment %in% c(6), 50,
+	                                ifelse(r$Treatment %in% c(5), 20, 0)))
+	d$K_fertilizer <- ifelse(r$Treatment == 1, 0, 75)
+	d$Zn_fertilizer <- ifelse(r$Treatment %in% c(1,8), 0, 5)
+	d$B_fertilizer <- ifelse(r$Treatment %in% c(1,8), 0, 1)
+	d$Ca_fertilizer <- ifelse(r$Treatment %in% c(1,2), 0,
+	                          ifelse(r$Treatment %in% c(4:8), ((0.577*10000)/5.625)*0.035, ((0.173*10000)/5.625)*0.035))
+	d$Mg_fertilizer <- ifelse(r$Treatment %in% c(1,2), 0,
+	                          ifelse(r$Treatment %in% c(4:8), ((0.577*10000)/5.625)*0.04, ((0.173*10000)/5.625)*0.04))
+	d$OM_used <- r$Treatment == 8
+	d$OM_type <- ifelse(r$Treatment == 8, "farmyard manure", NA)
+	d$OM_amount <- ifelse(r$Treatment == 8, 5000, 0) # From protocols shared by author.
+	d$OM_N <- d$OM_amount*(0.1796/100)*(0.755/100) # OM$K (%)
+	d$OM_P <- d$OM_amount*(0.1796/100)*(200.67532467532467/1e+06) # OM$P (ppm)
+	d$OM_K <- d$OM_amount*(0.1796/100)*(66.9072/1e+06) # OM$K (ppm)
 
-	d1$OM_used <- d1$Treatment == 8
-	d1$OM_type <- ifelse(d1$Treatment == 8, "farmyard manure", NA)
-	d1$OM_amount <- ifelse(d1$Treatment == 8, 5000, 0)
-	d1$OM_N <- d1$OM_amount*(0.1796/100)*(0.755/100) # OM$K (%)
-	d1$OM_P <- d1$OM_amount*(0.1796/100)*(200.67532467532467/1e+06) # OM$P (ppm)
-	d1$OM_K <- d1$OM_amount*(0.1796/100)*(66.9072/1e+06) # OM$K (ppm)
-
+	# EGB:
+	# # Combine grains, leaves, residues and soil
+	grains <- carobiner::read.excel(ff[basename(ff) == "4a Plant_Grains_sample  data.xlsx"])
+	colnames(grains) <- c("Site", "Grain", "Field label", "Block", "Treatment", "Plot", "grain_N", "grain_P", "grain_K", "grain_Ca", "grain_Mg")
+	leaves <- carobiner::read.excel(ff[basename(ff) == "6a Plant_Leaves at flowering_sample  data.xlsx"])
+	colnames(leaves) <- c("Site", "Leaves at flowering", "Field label", "Block", "Treatment", "Plot", "leaf_N", "leaf_P", "leaf_K", "leaf_Ca", "leaf_Mg")
+	stover <- carobiner::read.excel(ff[basename(ff) == "7a Plant_Stovers_sample data.xlsx"])
+	colnames(stover) <- c("Site", "Stovers", "Field label", "Block", "Treatment", "Plot", "residue_N", "residue_P", "residue_K", "residue_Ca", "residue_Mg")
+	soil <- carobiner::read.excel(ff[basename(ff) == "8a Soil lab data.xlsx"], skip = 12)
 	soil$Block <- sub("^\\D*(\\d+).*$", "\\1",  soil$`Client's ref`)
 	soil$Plot <- sub('.*(?=.{2}$)', '', soil$`Client's ref`, perl=T)
 	soil1 <- soil[,c(1,4:17)]
 	# Consider only the first 30 cm
-	soil1 <- soil1[soil1$`Depth (CM)` == "0-15" | soil1$`Depth (CM)` == "15-30", ]
+	# # EGB: Should we?
+	# soil1 <- soil1[soil1$`Depth (CM)` == "0-15" | soil1$`Depth (CM)` == "15-30", ]
 	soil1$`P (ppm)`[soil1$`P (ppm)` == "trace"] = 0.01
 	soil1$`P (ppm)` <- as.numeric(soil1$`P (ppm)`)
 	soil1$Block <- as.numeric(soil1$Block)
 	soil1$Plot <- as.numeric(soil1$Plot)
-	soil1$`N (%)` <- soil1$`N (%)`*10
-	soil1$`P (ppm)` <- soil1$`P (ppm)`/1000
-	soil1$`K (ppm)` <- soil1$`K (ppm)`/1000
-	soil2 <- aggregate(soil1[, c(3,5,6,9,10,11)], list(Site = soil1$Site, Block = soil1$Block, Plot = soil1$Plot), mean, na.rm = TRUE)
+	soil2 <- aggregate(soil1[, c(3,4,5,6,7,8,9,10,11,12)], list(Site = soil1$Site, Block = soil1$Block, Plot = soil1$Plot), mean, na.rm = TRUE)
 	soil2 <- soil2[order(soil2[,"Site"], soil2[,"Block"], soil2[,"Plot"]), ]
-	d2 <- merge(d1, soil2, by = intersect(names(d1), names(soil2)), all.x = TRUE)
-	d2$yield <- d2$`Grain yield (kg/plot -5.625m2)` * (10000/5.625)
-	d2$fwy_residue <- d2$`Stover yield (kg/plot - 5.625m2)` * (10000/5.625)
-	d2$irrigated <- TRUE
-	d2$row_spacing <- 75
-	d2$plant_spacing <- 25
+	soil2 <- carobiner::change_names(soil2,
+	                                 from = colnames(soil2)[4:length(soil2)],
+	                                 to = c("soil_pH", "soil_SOM", "soil_N", "soil_P_available",
+	                                        "soil_Ca", "soil_Mg", "soil_K",
+	                                        "soil_sand", "soil_clay", "soil_silt"))
+	d1 <- merge(merge(merge(leaves,
+	                        grains, by =  c("Site","Block","Plot")),
+	                  stover, by =  c("Site","Block","Plot")),
+	            soil2, by = c("Site","Block","Plot"), all.x = TRUE)
+	d1 <- d1[,c("Site","Block","Plot","Treatment",
+	            "leaf_N","leaf_P","leaf_K","leaf_Ca","leaf_Mg",
+	            "grain_N","grain_P","grain_K","grain_Ca","grain_Mg",
+	            "residue_N","residue_P","residue_K","residue_Ca","residue_Mg",
+	            "soil_pH","soil_SOM","soil_N","soil_P_available","soil_Ca","soil_Mg","soil_K","soil_sand","soil_clay","soil_silt")]
+	# EGB:
+	# # Assuming the protocol was applied only the first season of the experiment
+	d1$season <- 1
+	# Join all data
+	d <- merge(d, d1, by.x = c("trial_id", "block", "treatment", "season"), by.y = c("Site","Block","Treatment", "season"), all.x = TRUE)
 	
-	# process file(s)
-	d <- carobiner::change_names(d2,
-	     c("Site", "pH", "N (%)", "K (ppm)", "P (ppm)", "Sand (%)", "Clay (%)"),
-	     c("site", "soil_pH", "soil_N", "soil_K", "soil_P_total", "soil_sand", "soil_clay"))
-		 	 
-	d <- d[,c("country", "adm1", "adm2", "adm3", "latitude", "longitude", "site", "planting_date", "harvest_date", "season", "on_farm", "is_survey", "crop", "variety", "variety_code", "dmy_total", "yield", "fwy_residue", "fertilizer_type", "N_fertilizer", "N_splits", "P_fertilizer", "K_fertilizer", "Zn_fertilizer", "OM_used", "OM_type", "OM_amount", "OM_N", "OM_P", "OM_K", "soil_pH", "soil_N", "soil_K", "soil_P_total", "soil_sand", "soil_clay", "irrigated", "row_spacing","plant_spacing")]
-
-	id <- ifelse(r$site == "Kawanda", seq(1,sum(r$site == "Kawanda")), 
-										seq(1,sum(r$site == "Namulonge")))
+	d$fertilizer_type[d$treatment == 1] <- NA
+	d$fertilizer_type[d$treatment == 2] <- "TSP; KCl; ZnSO4; borax"
+	d$fertilizer_type[d$treatment == 4] <- "CAN; KCl; urea; ZnSO4; borax"
+	d$fertilizer_type[d$treatment %in% c(3,5,6,7,8)] <- "CAN; TSP; KCl; urea; ZnSO4; borax"
+	d$irrigated <- TRUE
+	d$row_spacing <- 75
+	d$plant_spacing <- 25
 	
-	r$trial_id <- paste0(r$trial_id, "-", id)
+	# % to kg/ha
+	d$residue_N <- d$residue_N * d$yield
+	d$residue_P <- d$residue_P * d$yield
+	d$residue_K <- d$residue_K * d$yield
+	d$residue_Ca <- d$residue_Ca * d$yield
+	d$residue_Mg <- d$residue_Mg * d$yield
 	
-	r$yield_part <- "grain"
+	# % or(‰) to mg/kg
+	d$soil_N <- d$soil_N * 10000
+	d$soil_K <- d$soil_K * 10000
+	d$grain_N <- d$grain_N * 10000
+	d$grain_P <- d$grain_P * 10000
+	d$grain_K <- d$grain_K * 10000
+	d$grain_Ca[d$grain_Ca == "trace"] = 0.01
+	d$grain_Ca <- as.numeric(d$grain_Ca) * 10000
+	d$grain_Mg <- d$grain_Mg * 10000
+	d$leaf_N <- d$leaf_N * 10000
+	d$leaf_P <- d$leaf_P * 10000
+	d$leaf_K <- d$leaf_K * 10000
+	d$leaf_Ca <- d$leaf_Ca * 10000
+	d$leaf_Mg <- d$leaf_Mg * 10000
+	
+	d <- d[,c("country", "adm1", "adm2", "adm3", "latitude", "longitude", "geo_from_source", "trial_id", "planting_date", "harvest_date", "on_farm", "is_survey", "irrigated",
+	          "crop", "variety", "variety_code", "yield", "yield_part", "fwy_residue",
+	          "fertilizer_type", "N_fertilizer", "N_splits", "P_fertilizer", "K_fertilizer", "Zn_fertilizer", "B_fertilizer", "Ca_fertilizer", "Mg_fertilizer",
+	          "OM_used", "OM_type", "OM_amount", "OM_N", "OM_P", "OM_K",
+	          "leaf_N", "leaf_P", "leaf_K", "leaf_Ca", "leaf_Mg",
+	          "grain_N", "grain_P", "grain_K", "grain_Ca", "grain_Mg",
+	          "residue_N", "residue_P", "residue_K", "residue_Ca", "residue_Mg",
+	          "soil_pH", "soil_SOM", "soil_N", "soil_P_available", "soil_Ca", "soil_Mg", "soil_K", "soil_sand", "soil_clay", "soil_silt",
+	          "insecticide_used", "insecticide_product", "insecticide_timing", "herbicide_used", "herbicide_product", "land_prep_method",
+	          "plant_density", "row_spacing","plant_spacing")]
+	
 	carobiner::write_files(meta, d, path=path)
 
 }
