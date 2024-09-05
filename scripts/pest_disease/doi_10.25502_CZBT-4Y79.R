9cb4a4a2fa07dee0c0ffce5f0f28c0612802c3f4
cedric Ngakou <cedric.ngakou@aims-cameroon.org>
2024-09-05 11:10:23 +0100

    New dataset (doi:10.25502/CZBT-4Y79)

---
+++ scripts/pest_disease/doi_10.25502_CZBT-4Y79.R
@@ -0,0 +1,80 @@
+# R script for "carob"
+
+
+carob_script <- function(path) {
+   
+" Biofortification of Cassava using Uniform Yield Trial (21 clones) in Ibadan 2008/2009 Breeding Season "
+   
+   uri <- "doi:10.25502/CZBT-4Y79"
+   group <- "pest_disease" 
+   ff  <- carobiner::get_data(uri, path, group)
+   
+   meta <- data.frame(
+      carobiner::read_metadata(uri, path, group, major=1, minor=0), 
+      data_institute ="IITA", 
+      publication = NA, 
+      project = NA, 
+      data_type = "experiment",
+      response_vars = "yield;dmy_roots;disease_severity",
+      treatment_vars = "variety_code", 
+      carob_contributor = "Cedric Ngakou", 
+      carob_date = "2024-09-05"
+   )
+   
+   f <- ff[basename(ff)=="2020-04-03T101459phenotype_download.csv"]
+   
+   r <- read.csv(f)  
+   
+   d <- data.frame(
+      country= "Nigeria",
+      #description= r$programDescription,
+      planting_date= r$plantingDate,
+      location= r$locationName,
+      trial_id= r$observationUnitName,
+      rep= r$replicate,
+      variety_code= r$germplasmName,
+      dmy_roots= r$dry.yield.CO_334.0000014*1000,
+      yield= r$fresh.root.yield.CO_334.0000013*1000,
+      harvest_index= r$harvest.index.variable.CO_334.0000015,
+      mc1= r$cassava.mosaic.disease.severity.1.month.evaluation.CO_334.0000191,
+      mc3= r$cassava.mosaic.disease.severity.3.month.evaluation.CO_334.0000192,
+      mc6= r$cassava.mosaic.disease.severity.6.month.evaluation.CO_334.0000194,
+      Bb1= NA,
+      Bb3= r$cassava.bacterial.blight.incidence.3.month.evaluation.CO_334.0000178,
+      Bb6= r$cassava.bacterial.blight.severity.6.month.evaluation.CO_334.0000176
+      
+   )
+   d$record_id <- 1:nrow(d)
+   d$planting_date <- gsub("July","07", d$planting_date)
+   d$crop <- "cassava"
+   d$irrigated <- NA
+   d$is_survey <- FALSE
+   d$inoculated <- FALSE
+   d$on_farm <- TRUE
+   d$yield_part <- "roots"
+   d$geo_from_source <- FALSE
+   d$longitude <- 3.8969928 
+   d$latitude <- 7.3786064 
+   
+   d$N_fertilizer <- d$P_fertilizer <- d$K_fertilizer <- as.numeric(NA)
+   
+   ### capturing disease score
+   dmb <- names(d)[grepl("mc|Bb", names(d))]
+   
+   dd <- d[,c("record_id", dmb)]
+   dates <- rep(c("2008-08-09","2008-09-09","2008-12-09"),2)
+   x <- reshape(dd, direction="long", varying =dmb , v.names="disease_severity", timevar="step")
+   x$time <- dates[x$step]
+   x$diseases <- ifelse(x$step < 4, "mosaic", "bacterial blight")
+   #x$severity_scale <- "1-4"	# not sure  
+   x$disease_severity <- as.character(x$disease_severity)
+   x$id <- x$step <- NULL
+   
+   d[dmb] <-  NULL
+   
+   carobiner::write_files (path, meta, d, timerecs = x)
+   
+}
+   
+   
+   
