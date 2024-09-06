091540b9c21050f498dcb113021a7af24fe53751
rhijmans <r.hijmans@gmail.com>
2024-09-06 08:09:09 +0200

    for #513

--- scripts/hdl_11529_10548722.R
+++ scripts/hdl_11529_10548722.R
@@ -1,23 +1,22 @@
 # R script for "carob"
 
-## ISSUES
-# if there are remaining issues, please put these in "meta$notes"
+# phenology from metadata:
+#V4-V5	6/22/2016
+#V7-V8	6/28/2016
+#V9-V10	7/1/2016
+#V11	7/6/2016
+#V-12	7/14/2016
 
 
 carob_script <- function(path) {
 
-"
-This experiments were established with different rates of nitrogen in order to generate a wide range of values for NDVI and grain yield in order to develop a calibration model for the GreenSeeker in Chihuahua. (2022-07-06)
-"
+"This experiments were established with different rates of nitrogen in order to generate a wide range of values for NDVI and grain yield in order to develop a calibration model for the GreenSeeker in Chihuahua. (2022-07-06)"
 
-## Identifiers
 	uri <- "hdl:11529/10548722" 
 	group <- "agronomy"
 
-## Download data 
 	ff  <- carobiner::get_data(uri, path, group)
 
-## metadata 
 	meta <- data.frame(
 		carobiner::read_metadata(uri, path, group, major=1, minor=0),
 		data_institute = "CIMMYT",
@@ -25,28 +24,34 @@ This experiments were established with different rates of nitrogen in order to g
 		project = NA,
 		data_type = "experiment",
 		treatment_vars = "N_fertilizer",
-		response_vars = "yield", 
+		response_vars = "yield;NDVI", 
 		carob_contributor = "Shumirai Manzvera",
 		carob_date = "2024-09-03",
 		notes = NA
 	)
 	
-## read data 
-
 	f <- ff[basename(ff) == "GreenSeeker Chihuahua 2016.xlsx"]
-   r <- carobiner::read.excel(f, sheet = "Data-Buenaventura" )
-   r1 <- r[-c(1, 2, 4), ]
-   
+   r <- carobiner::read.excel.hdr(f, sheet = "Data-Buenaventura", hdr=1, skip=3)
+
 	d <- data.frame(
 		country = "Mexico",
 		crop= "maize", 
-		rep=as.integer(r1$Rep),
-		treatment =r1$TRT,
-		yield=as.numeric(r1$`Yield at 14% hum`))
-		
+		rep=as.integer(r$Rep_),
+		treatment = as.character(r$TRT_),
+		plot_area = r$Plot.size_m.2,
+		yield=as.numeric(r$Yield.at.14pct.hum_kg.ha),
+		N_fertilizer = as.numeric(gsub("KgN", "", r$N.at.Planting_kg.ha))
+	)
+	d$record_id <- 1:nrow(d)
 
-	d$trial_id <- "1"
-	
+	nv <- grep("NDVI", names(r), value=TRUE)
+	ndvi <- r[, nv]
+	ndvi$record_id <- 1:nrow(ndvi)
+	dates <- as.numeric(gsub("NDVI_", "", nv)) + as.Date("1900-01-01") - 2
+	ndvi <- reshape(ndvi, direction="long", varying=nv, v.names="NDVI", times = as.character(dates))
+  ndvi$id <- NULL
+  
+  d$trial_id <- "1"
 	d$on_farm <- TRUE
 	d$is_survey <- FALSE
 	d$irrigated <- TRUE
@@ -60,17 +65,10 @@ This experiments were established with different rates of nitrogen in order to g
 	d$land_prep_method <- "conventional"
 	d$yield_part <- "grain"
 
-	r1$`N at Planting` <- gsub("KgN","" , r1$`N at Planting`)
-	r1$`N at Planting` <- as.numeric(r1$`N at Planting`)
+	# from metadata: 104 kg/ha MAP
+	d$N_fertilizer <- (104*0.11) + d$N_fertilizer
+	d$P_fertilizer <- (104*0.52) / 2.29
+	d$K_fertilizer <- 0
 	
-	d$N_fertilizer <- (104*0.12)+ r1$`N at Planting`
-	d$P_fertilizer <- (104*0.61)/2.29
-
-# all scripts must end like this
-	carobiner::write_files(path, meta, d)
+	carobiner::write_files(path, meta, d, timerecs=ndvi)
 }
-
-## now test your function in a _clean_ R environment (no packages loaded, no other objects available)
-# path <- _____
-# carob_script(path)
-
