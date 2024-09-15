7397a1d38429b0983822cd9370591846664e9344
rhijmans <r.hijmans@gmail.com>
2024-09-14 22:40:45 -0700

    for #531

--- scripts/varieties/doi_10.21223_P3_H50YAO.R
+++ scripts/varieties/doi_10.21223_P3_H50YAO.R
@@ -1,12 +1,9 @@
 # R script for "carob"
 
-## ISSUES
-# ....
-
 
 carob_script <- function(path) {
   
-    "In anticipation of the effects of global warming on potato cultivation in both tropical and subtropical environments. Since 2004, efforts have turned to the development of a new group in Population B with improved adaptation to warm environments, resistance to late blight and virus, mid-season maturity (90 day growing period under short day length conditions), adaptation to mid elevations, low glycoalkaloids content, along with economically important traits such as high tuber yield, quality for table and industry. And so group LBHT of population B was developed, denominated LBHT because of its late blight and heat tolerance.  \r\nAll trials were conducted in randomized complete block design (RCBD) with 3-4 replicates or in simple lattice design, using Désirée as a heat tolerant control and Amarilis as a non-tolerant to heat control at La Molina-Peru, in spring-summer season, located at 300 masl in Lima, an arid environment in lowland tropics."
+"In anticipation of the effects of global warming on potato cultivation in both tropical and subtropical environments. Since 2004, efforts have turned to the development of a new group in Population B with improved adaptation to warm environments, resistance to late blight and virus, mid-season maturity (90 day growing period under short day length conditions), adaptation to mid elevations, low glycoalkaloids content, along with economically important traits such as high tuber yield, quality for table and industry. And so group LBHT of population B was developed, denominated LBHT because of its late blight and heat tolerance.  \r\nAll trials were conducted in randomized complete block design (RCBD) with 3-4 replicates or in simple lattice design, using Désirée as a heat tolerant control and Amarilis as a non-tolerant to heat control at La Molina-Peru, in spring-summer season, located at 300 masl in Lima, an arid environment in lowland tropics."
   
   uri <- "doi:10.21223/P3/H50YAO"
   group <- "varieties"
@@ -25,7 +22,7 @@ carob_script <- function(path) {
       notes = NA
   )
   
-  process <- function(filename){
+  process <- function(filename) {
       
       r <- carobiner::read.excel(filename, sheet="Fieldbook")               
       minimal <- carobiner::read.excel(filename, sheet="Minimal")
@@ -36,40 +33,14 @@ carob_script <- function(path) {
       
       n <- as.list(installation$Value)
       names(n) <- installation$Factor
-      
-      if((!"yield"%in%colnames(r)) && ("TTWP" %in% colnames(r))){
-          
-          TTYNA = (as.numeric(r$TTWP) / as.numeric(n$`Plot size (m2)`)) * 10
-          
-          r$yield = TTYNA * 1000
-          
-      } 
-      
-      if((!"yield_marketable"%in%colnames(r)) && ("MTWP" %in% colnames(r))){
-          
-          MTYNA = (as.numeric(r$MTWP) / as.numeric(n$`Plot size (m2)`)) * 10
-          r$yield_marketable = MTYNA * 1000
-          
-      }
-      
-      df <- data.frame(
+
+	  plot_adj <- 10000 / as.numeric(n$`Plot size (m2)`)
+
+      data.frame(
           rep = as.integer(r$REP),
           variety = r$INSTN,
-          yield = if('yield' %in% colnames(r)) {
-              r$yield
-          } else {
-              as.numeric(NA)
-          },
-          yield_marketable = if('yield_marketable' %in% colnames(r)) {
-              r$yield_marketable
-          } else {
-              as.numeric(NA)
-          },
-          AUDPC = if('AUDPC' %in% colnames(r)) {
-              as.numeric(r$AUDPC) / 100
-          } else {
-              as.numeric(NA)
-          },
+          yield = as.numeric(r$TTWP) * plot_adj,
+          yield_marketable = as.numeric(r$MTWP) * plot_adj,
           country = m$Country,
           adm1 = m$Admin1,
           adm2 = m$Admin2,
@@ -82,8 +53,6 @@ carob_script <- function(path) {
           harvest_date = m$`End date`,
           trial_id = gsub(".xls", "", basename(filename))
       )
-      
-      
   }
   
   f <- ff[grep("CIPHQ_LBHT", basename(ff))]
@@ -99,11 +68,6 @@ carob_script <- function(path) {
   d$N_fertilizer <- d$P_fertilizer <- d$K_fertilizer <- as.numeric(NA)
   
   
-  carobiner::write_files(path = path,
-                         metadata = meta,
-                         records = d)
+  carobiner::write_files(path = path, metadata = meta, records = d)
 }
 
-## now test your function in a _clean_ R environment (no packages loaded, no other objects available)
-# path <- _____
-# carob_script(path)
