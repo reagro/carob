d87f0fe1a82c1a08548ddd9cbbbf2e635bd576ce
Fredy Chimire <Fredy.Chimire@econet.co.zw>
2024-09-22 13:28:47 +0200

    Standardised dataset hdl_11529_10548699.R

---
+++ scripts/varieties_wheat/hdl_11529_10548699.R
@@ -0,0 +1,27 @@
+# R script for "carob"
+
+carob_script <- function(path) {
+  
+  "The International Septoria Observation Nursery (earlier Septoria Monitoring Nursery – SMN) is a single replicate nursery that contains diverse spring bread wheat (Triticum aestivum) germplasm adapted to ME2 (High rainfall environment) and ME4 (Low rainfall, semi-arid environment) with total 50-100 entries and white/red grain color. (2020)"
+  uri <- "hdl:11529/10548699"
+  group <- "varieties_wheat"
+  ff  <- carobiner::get_data(uri, path, group)
+  
+  meta <- data.frame(
+    carobiner::read_metadata(uri, path, group, major=2, minor=0),
+    data_institute = "CIMMYT",
+    publication= NA,
+    project="International Septoria Observation Nursery",
+    data_type= "experiment",
+    carob_contributor= "Fredy Chimire",
+    carob_date="2024-09-22",
+    response_vars = "yield",
+    treatment_vars = "variety_code"
+  )
+  
+  proc_wheat <- carobiner::get_function("proc_wheat", path, group)
+  d <- proc_wheat(ff)	
+
+  carobiner::write_files(path, meta, d)
+}
+carob_script(path)
