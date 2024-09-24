89a999e50eb0612f03f6fb8fc13feca105700c60
Henry Juarez <36743773+henryjuarez@users.noreply.github.com>
2024-09-24 14:49:24 -0500

    Update doi_10.21223_P3_M40FYT.R
    Update pending

--- scripts/_pending/varieties/doi_10.21223_P3_M40FYT.R
+++ scripts/_pending/varieties/doi_10.21223_P3_M40FYT.R
@@ -6,191 +6,38 @@
 
 carob_script <- function(path) {
   
-  "Description:
-
-    [copy the abstract from the repo]"
+    "Group B1, cycle B1C5 of Population B (fifth cycle of recombination of the pure native Andigena group B1), is the result of a new population improvement strategy in the absence of R- genes started at CIP in 1990. The group B1 derives of primitive cultivars of Solanum tuberosum ssp. andigena, known to be free of R- genes. \r\nThese clones were planted in a randomized complete block design (RCBD) with 2-4 replicates at La Victoria, Huancayo, Junin-Peru, located at 3290 masl."
   
   uri <- "doi:10.21223/P3/M40FYT"
-  dataset_id <- carobiner::simple_uri(uri)
   group <- "varieties"
-  
-  
-  ## download and read data 
-  
   ff  <- carobiner::get_data(uri, path, group)
-  js <- carobiner::read_metadata(uri, path, group, major=1, minor=3)
   
-  # f <- ff[basename(ff) == "16470_PTYield082019_Parental_Value_B3C3_CIPSRM19_02.xlsx"]
-  
-  # r <- read.csv(f, sep = ";")
-  
-  ## process file(s)
-  
-  files <- list.files(file.path(paste0(path,"/data/raw/varieties/",dataset_id,"/")), pattern = "CIPHYO", full.names = T)
-  cat("These are the available files for the doi link:\n\n")
-  print(files)
-  
-  ###
-  
-  filter_dfs <- function(file, variables=c("REP","INSTN","AUDPC","rAUDPC","TTYNA","MTYNA")){
-      
-      df <- carobiner::read.excel(f = file, sheet="Fieldbook")
-      
-      available_vars = which(colnames(df) %in% variables)
-      
-      missing_vars = which(!variables %in% colnames(df))
-      
-      
-      df <- df[,available_vars]
-      
-      for(missing_var in variables[missing_vars]){
-          
-          df[[missing_var]] = NA
-          
-      }
-      
-      
-      idx <- which(colnames(df) %in% c("REP","INSTN","TTYNA","MTYNA"))
-      
-      colnames(df)[idx] <- c("rep","variety","yield","yield_marketable") 
-      
-      
-      tryCatch({
-          df$rep <- df$rep |> as.integer()
-          df$yield <- df$yield * 1000
-          df$yield_marketable <- df$yield_marketable * 1000
-          df$AUDPC <- df$AUDPC / 100
-      },error = function(e){
-          print(paste0("Error: ", e$message))
-          Sys.sleep(1.5)
-      })
-      
-      
-      return(df)
-      
-  }
-  
-  ###
-  
-  extract_metadata <- function(file, data){
-      
-      metadata <- carobiner::read.excel(f = file, sheet="Minimal")
-      
-      data$country <- metadata[which(metadata$Factor=="Country"),]$Value
-      data$adm1 <- metadata[which(metadata$Factor=="Admin1"),]$Value
-      data$adm2 <- metadata[which(metadata$Factor=="Admin2"),]$Value
-      data$adm3 <- metadata[which(metadata$Factor=="Admin3"),]$Value
-      data$longitude <- metadata[which(metadata$Factor=="Longitude"),]$Value |> 
-          as.numeric()
-      data$latitude <- metadata[which(metadata$Factor=="Latitude"),]$Value |> 
-          as.numeric()
-      data$elevation <- metadata[which(metadata$Factor=="Elevation"),]$Value |> 
-          as.numeric()
-      data$planting_date <- metadata[which(metadata$Factor=="Begin date"),]$Value
-      data$harvest_date <- metadata[which(metadata$Factor=="End date"),]$Value
-      
-      return(data)
-      
-  }
-  
-  ###
-  
-  df_list <- list()
-  
-  trial_id_counter = 1
-  
-  for(dataframe in files){
-      
-      d <- filter_dfs(dataframe)
-      
-      df_name <- sub(".*\\/(.*)\\.xls$", "\\1", dataframe)
-      
-      d <- extract_metadata(dataframe, d)
-      
-      d$on_farm <- TRUE
-      d$is_survey <- FALSE
-      d$irrigated <- FALSE
-      d$treatment = "varieties"
-      d$crop <- "potato"
-      d$pathogen <- "Phytophthora infestans"
-      d$yield_part <- "tubers"
-      # d$is_experiment <- 'yes'
-      # d$sciname <- 'Solanum tuberosum'
-      d$trial_id = (trial_id_counter) |> 
-          as.character()
-      
-      trial_id_counter = trial_id_counter + 1
-      
-      
-      df_list[[df_name]] <- d
-      
-  }
-  
-  d <- do.call(rbind, df_list)
-  
-  d$record_id <- as.integer(1:nrow(d))
-  
-  ## required variabeles for metadata
-  
-  metadata_requirements <- read.csv("https://raw.githubusercontent.com/reagro/terminag/main/variables/variables_metadata.csv", sep = ",")
-  metadata_requirements <- metadata_requirements[which(metadata_requirements$required=="yes"),]
-  
-  
-  ## required variables for records
-  
-  crop_requirements <- read.csv("https://raw.githubusercontent.com/reagro/terminag/main/variables/variables_crop.csv", sep = ",")
-  crop_requirements <- crop_requirements[which(crop_requirements$required=="yes"),]
-  
-  ## filling metadata 
-  
-  missing_metadata_idx <- which(!metadata_requirements$name %in% colnames(js))
-  
-  missing_metadata_vars <- metadata_requirements[missing_metadata_idx,]$name
-  
-  print(missing_metadata_vars)
-  
-  fill_metadata <- c(
-      "Henry Juarez",
-      "experiment",
-      "treatment",
-      "AUDPC, rAUDPC, yield, yield_marketable",
-      NA,
-      "CIP",
-      NA,
-      Sys.Date() |> as.character()
+  meta <- data.frame(
+      carobiner::read_metadata(uri, path, group, major=1, minor=3),
+      data_institute = "CIP",
+      publication = NA,
+      project = NA,
+      data_type = "experiment",
+      treatment_vars = "variety",
+      response_vars = "yield;yield_marketable", 
+      carob_contributor = "Henry Juarez",
+      carob_date = "2024-09-13",
+      notes = NA
   )
   
-  data_vector <- setNames(fill_metadata, missing_metadata_vars)
+  process <- carobiner::get_function("process_cip_lbvars", path, group)
   
-  js <- cbind(js, data_vector |> 
-                  as.data.frame() |> 
-                  t())
-  
-  ###
-  
-  missing_data_idx <-  which(!crop_requirements$name %in% colnames(d))
-  
-  missing_data_vars <- crop_requirements[missing_data_idx,]$name
-  
-  print(missing_data_vars)
-  
-  
-  # Data
-  
-  # d$trial_id = js$dataset_id
-  d$geo_from_source = TRUE
-  d$N_fertilizer = NA
-  d$P_fertilizer = NA
-  d$K_fertilizer = NA
+  f <- ff[grep("CIPHYO", basename(ff))]
+  d <- lapply(f, process, addvars=c("AUDPC","rAUDPC","TTWP","MTWP"))
+  d <- do.call(rbind, d)
   
   
   carobiner::write_files(path = path,
-                         metadata = js,
+                         metadata = meta,
                          records = d)
   
   
-  
-}
+ }
 
 ## now test your function in a _clean_ R environment (no packages loaded, no other objects available)
 # path <- _____
