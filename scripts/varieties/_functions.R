92e30301e355b6d76da98479701583a1909a20d0
rhijmans <r.hijmans@gmail.com>
2024-09-24 21:12:14 -0700

    m

--- scripts/varieties/_functions.R
+++ scripts/varieties/_functions.R
@@ -22,9 +22,22 @@ process_cip_lbvars <- function(filename, addvars=NULL) {
 	plot_size <- as.numeric(n$`Plot_size_(m2)`)
 	plot_adj <- 10000 / plot_size
      
-  	if (grepl("/", m$Begin_date)) m$Begin_date <- as.character(as.Date(m$Begin_date,format = "%d/%m/%Y"))
-  	if (grepl("/", m$End_date)) m$End_date <- as.character(as.Date(m$End_date,format = "%d/%m/%Y"))
-	 
+  	if (grepl("/", m$Begin_date)) {
+		i <- is.na(m$Begin_date)
+		m$Begin_date <- as.character(as.Date(m$Begin_date,format = "%d/%m/%Y"))
+		j <- is.na(m$Begin_date)
+		if (sum(j) > sum(i)) warning("NAs introduced in Begin_date")
+  	}
+	if (grepl("/", m$End_date)) {
+		if (grepl("-", m$End_date)) {
+			# taking the last one, should perhaps take the average (doi_10.21223_KNC22E.R)
+			m$End_date <- strsplit(m$End_date, "-")[[1]][2]
+		}
+		i <- is.na(m$End_date)
+		m$End_date <- as.character(as.Date(m$End_date,format = "%d/%m/%Y"))
+		j <- is.na(m$End_date)
+		if (sum(j) > sum(i)) warning("NAs introduced in End_date")
+	} 
 	d <- data.frame(
 		rep = as.integer(r$REP),
 		variety = r$INSTN,
