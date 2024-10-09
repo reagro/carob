30a5ac4dd9cb33aa25198d5bc648d03c566d8515
rhijmans <r.hijmans@gmail.com>
2024-10-09 09:49:40 -0700

    for #559

--- scripts/varieties_maize/hdl_11529_10542.R
+++ scripts/varieties_maize/hdl_11529_10542.R
@@ -1,9 +1,6 @@
 # R script for "carob"
 # license: GPL (>=3)
 
-## ISSUES
-# I double checked Mexico coordinates I cannot see where the error is originating from
-
 
 carob_script <- function(path) {
 
@@ -19,58 +16,53 @@ carob_script <- function(path) {
 		publication = NA,
 		project = "International intermediate white hybrid trials",
 		data_type = "experiment",
-		treatment_vars = "variety;longitude;latitude",
+		treatment_vars = "variety_code",
 		response_vars = "yield", 
 		carob_contributor = "Shumirai Manzvera",
 		carob_date = "2024-10-09",
-		notes = "I double checked Mexico coordinates I cannot see where the error is originating from",
 		design = NA
 	)
-	
 
-	f0 <- ff[basename(ff) == "07CHTSW11-1.xls"]	
-	r0 <- r <- carobiner::read.excel(f0)
-	r0 <-r0[22:34, 2:46]
+	f <- ff[basename(ff) == "07CHTSW-Locations.xls"]
+	rlocs <- carobiner::read.excel.hdr(f, skip=9, hdr=2) 
+	rlocs <- rlocs[-1, ]
+	names(rlocs) <- c("ID", names(rlocs)[-ncol(rlocs)])
 	
-	d3 <- data.frame( 
-	  trial_id = as.character(r0$Name),
-	  variety=r0$BreedersPedigree1,
-	  yield=as.numeric(r0$GrainYieldTons_GrainWt)*1000,
-	  asi=as.numeric(r0$ASI),
-	  plant_height=as.numeric(r0$PlantHeightCm),
-	  ear_height = as.numeric(r0$EarHeightCm),
-	  rlper = as.numeric(r0$RootLodging1_5),
-	  slper = as.numeric(r0$StemLodgingPer),
-	  husk = as.numeric(r0$BadHuskCover1_5),
-	  e_rot = as.numeric(r0$EarRotTotalPer),
-	  moist = as.numeric (r0$GrainMoisturePer),
-	  plant_density = as.numeric(r0$PlantStand_NumPerPlot),
-	  e_asp = as.numeric(r0$EarAspect1_5),
-	  p_asp = as.numeric(r0$PlantAspect1_5),
-	  gls = r0$GrayLeafSpot1_5,
-	  rust = r0$CommonRust1_5,
-	  blight = r0$LeafBlightTurcicum1_5)
+	locs <- data.frame(
+		trial_id = as.character(rlocs$ID),
+		latitude = as.numeric(gsub("o", "", rlocs$Latitude_Latitud)) + 
+						as.numeric(gsub("'", "", rlocs$X)) / 60, 
+		longitude = (as.numeric(gsub("o", "", rlocs$Longitude_Longitud)) + 
+						as.numeric(gsub("'", "", rlocs$X.2)) / 60 ) * 
+						ifelse(rlocs$`X.3` == "W", -1, 1),
+		country = rlocs$Country_País,
+		location = rlocs$Location_Localidad,
+		elevation = rlocs$Masl_Altitud_msnm,
+		planting_date = as.character(rlocs$Date_Fecha.de_Siembra),
+		harvest_date = as.character(rlocs$Date_Fecha.de_Cosecha)
+	)
+	locs$country <- gsub("México", "Mexico", locs$country)
+	s <- sapply(strsplit(locs$location, ", "), \(i) i[1:2]) |> t()
+	locs$location <- s[,1]
+	locs$adm1 <- s[,2]
+	locs$adm1 <- gsub("Jal.", "Jalisco", s[,2])
+	locs$geo_from_source <- TRUE
+	i <- locs$location == "Usmajac"
+	locs$latitude[i] <- 19.875
+	locs$longitude[i] <- -103.5987
+	locs$geo_from_source[i] <- FALSE
+		
+		
 	
-	  d3$country <-"Mexico"
-	  d3$longitude <- 20.4736865
-	  d3$latitude <-  -103.4479314
-	  d3$elevation <- 1550
-	  d3$location <- "Tlajomulco, Jal"
-	  d3$planting_date <- "2007-06-08"
-	  d3$harvest_date  <- "2007-11-24"
-	
-	
-	
-	
-	get_data <- function(fname, id, country, longitude, latitude, elevation) {
+	get_data <- function(fname, id, cols=2:35) {
 		f <- ff[basename(ff) == fname]
 		r <- carobiner::read.excel(f) 
-		r <-r[22:34, 2:35]
-	
+		r <-r[22:34, cols]
 		
-		d <- data.frame( 
-		  trial_id = as.character(r$Name),
-		  variety=r$BreedersPedigree1,
+		data.frame( 
+		  trial_id = as.character(id),
+		  variety_code = as.character(r$Name),
+		  variety_pedigree=r$BreedersPedigree1,
 		  yield=as.numeric(r$GrainYieldTons_FieldWt)*1000,
 		  asi=as.numeric(r$ASI),
 		  plant_height=as.numeric(r$PlantHeightCm),
@@ -85,44 +77,22 @@ carob_script <- function(path) {
 		  p_asp = as.numeric(r$PlantAspect1_5),
 		  gls = r$GrayLeafSpot1_5,
 		  rust = r$CommonRust1_5,
-		  blight = r$LeafBlightTurcicum1_5,
-		  country=country,
-		  longitude=longitude,
-		  latitude=latitude,
-		  elevation = elevation)
+		  blight = r$LeafBlightTurcicum1_5
+		)
 	}
 	
-	d0 <- get_data("07CHTSW1-1.xls",1,"Mexico", -103.5986529, 19.875 , 1340)  
-	d0$location <- "Usmajac Mpio. de Sayula, Jal"
-	d0$planting_date <- "2007-06-01"
-	d0$harvest_date  <- "2007-12-12"
-	
-	d1 <- get_data("07CHTSW9-1.xls",2,"Egypt",  31.518, 30.733 , 10)  
-	d1$location <- "Sids"
-	d1$planting_date <- "2007-05-14"
-	d1$harvest_date  <- "2007-09-15"
-	
-	d2 <- get_data("07CHTSW10-1.xls",4,"Egypt",   30.947,  31.087 , 20)  
-	d2$location <- "Sakha"
-	d2$planting_date <- "2007-05-14"
-	d2$harvest_date  <- "2007-09-12"
-	
-	d4 <- get_data("07CHTSW24-1.xls",6,"Mexico",  -99.2222437 ,20.0919675, 2071)  
-	d4$location <- "Tlaxcoapan, Hidalgo"
-	d4$planting_date <- "2007-05-01"
-	d4$harvest_date  <- "2007-10-04"
-	
-	d5 <- get_data("07CHTSW26-1.xls",7,"Mexico",    -98.843,  18.596 , 1350)  
-	d5$location <- "Tepalcingo, Mor"
-	d5$planting_date <- "2007-06-06"
-	d5$harvest_date  <- "2007-11-20"
+	d1 <- get_data("07CHTSW1-1.xls", 1)
+	d2 <- get_data("07CHTSW9-1.xls", 2) 
+	d3 <- get_data("07CHTSW10-1.xls",3)
+	d4 <- get_data("07CHTSW11-1.xls", 4, 2:46)  
+	d5 <- get_data("07CHTSW24-1.xls", 5)  
+	d6 <- get_data("07CHTSW26-1.xls", 6)
 
-	
-	
-	d <- carobiner::bindr(d0, d1, d2, d3, d4, d5)
-	
-	d$geo_from_source <- FALSE
-	  
+	dd <- rbind(d1, d2, d3, d4, d5, d6)
+
+	d <- merge(dd, locs, by="trial_id")
+
+	d$geo_from_source <- TRUE
 	d$crop = "maize"
 	d$on_farm = TRUE
 	d$striga_trial = FALSE 
