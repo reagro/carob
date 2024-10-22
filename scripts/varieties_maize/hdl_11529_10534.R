8e607155913adde4f126f8484b2341b69ed6ce3d
rhijmans <r.hijmans@gmail.com>
2024-10-21 22:55:54 -0700

    for #570

--- scripts/varieties_maize/hdl_11529_10534.R
+++ scripts/varieties_maize/hdl_11529_10534.R
@@ -1,22 +1,15 @@
 # R script for "carob"
 # license: GPL (>=3)
 
-## ISSUES
-# list processing issues here so that an editor can look at them
-
 carob_script <- function(path) {
 
 "International Early White Hybrid trial - IEWH0852+ Summary results and individual trial results from the International Early White Hybrid - IEWH, (Highland Early White Hybrids - CHTHEW) conducted in 2008."
 
-
-## Identifiers
 	uri <- "hdl:11529/10534"
 	group <- "varieties_maize"
 
-## Download data 
 	ff  <- carobiner::get_data(uri, path, group)
 
-## metadata 
 	meta <- data.frame(
 		carobiner::read_metadata(uri, path, group, major=3, minor=0),
 		data_institute = "CIMMYT",
@@ -38,12 +31,8 @@ carob_script <- function(path) {
 	names(rlocs) <- c("ID", names(rlocs)[-ncol(rlocs)])
 	
 	locs <- data.frame(
-	  trial_id = as.character(rlocs$ID),
-	  latitude = as.numeric(gsub("o", "", rlocs$Latitude_Latitud)) + 
-	    as.numeric(gsub("'", "", rlocs$X)) / 60,
-	  longitude = (as.numeric(gsub("o", "", rlocs$Longitude_Longitud)) + 
-	                 as.numeric(gsub("'", "", rlocs$X.3)) / 60 ) * 
-	    ifelse(rlocs$`X.4` == "W", -1, 1),
+	  latitude = as.numeric(gsub("o", "", rlocs$Latitude_Latitud)) +  as.numeric(gsub("'", "", rlocs$X)) / 60,
+	  longitude = (as.numeric(gsub("o", "", rlocs$Longitude_Longitud)) + as.numeric(gsub("'", "", rlocs$X.3)) / 60 ) * ifelse(rlocs$`X.4` == "W", -1, 1),
 	  country = rlocs$Country_País,
 	  location = rlocs$Location_Localidad,
 	  elevation = rlocs$Masl_Altitud_msnm,
@@ -60,15 +49,15 @@ carob_script <- function(path) {
 	locs$adm1 <- s[,2]
 	locs$geo_from_source <- TRUE
 	
-	get_data <- function(fname, id, cols=2:35) {
+	get_data <- function(fname, location, cols=2:35) {
 	  f <- ff[basename(ff) == fname]
-	  r <- carobiner::read.excel(f) 
-	  r <-r[19:33, cols]
-	  r<- r[-(1:2), ]
-	  
-	  
-	  x <- data.frame( 
-	    trial_id = as.character(id),
+	  r <- carobiner::read.excel(f,  col_types="text")
+	  r <- r[19:33, cols]
+	  r <- r[-(1:2), ]
+	  	  
+	  data.frame( 
+	    trial_id = gsub("-1.xls", "", fname),
+		location = location,
 	    variety_code = as.character(r$Name),
 	    variety_pedigree=r$BreedersPedigree1,
 	    asi=as.numeric(r$ASI),
@@ -86,17 +75,18 @@ carob_script <- function(path) {
 	    rust = r$CommonRust1_5,
 	    blight = r$LeafBlightTurcicum1_5,
 	    yield = as.numeric(r$GrainYieldTons_FieldWt) * 1000
-	  ) }
+	  ) 
+	}
 	
-	d1 <- get_data("08CHTHEW7-1.xls", 1)
-	d2 <- get_data("08CHTHEW18-1.xls", 2)
-	d3 <- get_data("08CHTHEW23-1.xls", 3,)
-	d4 <- get_data("08CHTHEW27-1.xls", 4,)
-	d5 <- get_data("08CHTHEW29-1.xls", 5)
+	d1 <- get_data("08CHTHEW7-1.xls", "Huamantla")
+	d2 <- get_data("08CHTHEW18-1.xls", "San Juan del Rio")
+	d3 <- get_data("08CHTHEW23-1.xls", "Batán")
+	d4 <- get_data("08CHTHEW27-1.xls", "Jamay")
+	d5 <- get_data("08CHTHEW29-1.xls", "Durango")
 	
 	dd <- rbind(d1, d2, d3, d4, d5)
 	
-	d <- merge(dd, locs, by="trial_id")
+	d <- merge(dd, locs, by="location")
 	
 	d$crop = "maize"
 	d$on_farm = TRUE
@@ -106,19 +96,9 @@ carob_script <- function(path) {
 	d$yield_part = "grain"
 	d$is_survey <- FALSE
 	d$irrigated <- NA
-	
-	
+		
 	d$N_fertilizer <- d$P_fertilizer  <- d$K_fertilizer <- as.numeric(NA)
-	
 	d <- d[!is.na(d$yield), ]
-	
-	
-	
 	carobiner::write_files(path, meta, d)
 }
 	
-	
-## now test your function in a _clean_ R environment (no packages loaded, no other objects available)
-# path <- _____
-# carob_script(path)
-
