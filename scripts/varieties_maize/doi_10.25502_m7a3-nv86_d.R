772b27978ebe55a2e79ff52bfb98b6fb9b6dd1e0
rhijmans <r.hijmans@gmail.com>
2024-10-29 02:21:54 -0700

    for #582

--- scripts/varieties_maize/doi_10.25502_m7a3-nv86_d.R
+++ scripts/varieties_maize/doi_10.25502_m7a3-nv86_d.R
@@ -1,5 +1,8 @@
 # R script for "carob"
 
+
+# need to capture the variation in irrigation and in striga treatment
+
 carob_script <- function(path) {
 
 "The GGE biplot tool has potential for determining combining ability effects, identifying distinct heterotic groups and efficient testers in a line × tester study. However, its use for such analysis has not been adequately explored. The objectives of this study were to (i) assess combining ability of extra-early maturing lines (80–85 days to physiological maturity) and testers for grain yield (ii) classify lines into heterotic groups and (iii) identify most efficient testers using GGE biplot. Sixty-three lines crossed to four testers were evaluated under Strga-infested, drought and nonstress environments for 2 years in Nigeria. Results of GGE biplot analyses of combining ability and heterotic patterns of yield of lines, grouping and identification of testers were close to those of the conventional line × tester method. Testers TZEEI 13, TZEEI 21 and TZEEI 29 were highly efficient in grouping lines under stress environments while testers TZEEI 21 and TZEEI 29 were best under nonstress environments. The GGE biplot identified tester TZEEI 13, TZEEI 21 and TZEEI 29 as most efficient across stress environments and TZEEI 21 and TZEEI 29 across nonstress environments."
@@ -7,15 +10,12 @@ carob_script <- function(path) {
 
 	uri <- "doi:10.25502/m7a3-nv86/d"
 	group <- "varieties_maize"
-
-
 	ff  <- carobiner::get_data(uri, path, group)
-
  
 	meta <- data.frame(
 		carobiner::read_metadata(uri, path, group, major=1, minor=0),
 		data_institute = "IITA",
-		publication = "doi.org/10.1556/0806.47.2019.25",
+		publication = "doi:10.1556/0806.47.2019.25",
 		project = NA,
 		data_type = "experiment",
 		treatment_vars = "N_fertilizer",
@@ -23,35 +23,21 @@ carob_script <- function(path) {
 		carob_contributor = "Mitchelle Njukuya",
 		carob_date = "2024-10-29",
 		notes = NA, 
-		design = "16×9 lattice design"
+		design = "16×9 lattice"
 	)
 	
-
-	sort_data <- function(fname, id, treatment) {
+	sort_data <- function(fname, id, treatment, nrows=-1, skip=0) {
 	  
 	  f <- ff[basename(ff) == fname]
-	  r <- read.csv(f)
+      r <- read.csv(f, nrows=nrows, skip=skip)
+	  colnames(r) <- gsub("RLLNPERCT|RLHNPERCT","RLPERCT",colnames(r))
+	  colnames(r) <- gsub("SLLNPERCT|SLHNPERCT","SLPERCT",colnames(r))
+	  colnames(r) <- gsub("HUSKLN|HUSKHN","HC",colnames(r))
+	  colnames(r) <- gsub("LN$|HN$","", colnames(r))
 	  
-	  colnames(r) <- gsub("YIELDLN","YIELD",colnames(r))
-	  colnames(r) <- gsub("PLSTLN","PLST",colnames(r))
-	  colnames(r) <- gsub("DYSKLN","DYSK",colnames(r))
-	  colnames(r) <- gsub("POLLENLN","POLLEN",colnames(r))
-	  colnames(r) <- gsub("ASILN","ASI",colnames(r))
-	  colnames(r) <- gsub("PLHTLN","PLHT",colnames(r))
-	  colnames(r) <- gsub("EHTLN","EHT",colnames(r))
-	  colnames(r) <- gsub("RLLNPERCT","RLPERCT",colnames(r))
-	  colnames(r) <- gsub("SLLNPERCT","SLPERCT",colnames(r))
-	  colnames(r) <- gsub("HUSKLN","HC",colnames(r))
-	  colnames(r) <- gsub("PASPLN","PASP",colnames(r))
-	  colnames(r) <- gsub("PHARVLN","PHARV",colnames(r))
-	  colnames(r) <- gsub("EHARVLN","EHARV",colnames(r))
-	  colnames(r) <- gsub("EASPLN","EASP",colnames(r))
-	  colnames(r) <- gsub("EROTLN","EROT",colnames(r))
-	  colnames(r) <- gsub("MOISTLN","MOIST",colnames(r))
-	  
-	d <- data.frame(
-	  treatment = as.character(treatment),
-	  date = r$YEAR,
+	  data.frame(
+		treatment = as.character(treatment),
+		planting_date = r$YEAR,
 		location = r$LOC,
 		rep = as.integer(r$Rep),
 		variety = r$Pedigree,
@@ -69,15 +55,16 @@ carob_script <- function(path) {
 		p_harv = r$PHARV,
 		e_harv = r$EHARV,
 		moist = r$MOIST,
-		yield = as.numeric(r$YIELD),
-		trial_id = as.character(id)
-	)}
+		yield = as.numeric(r$YIELD)
+	  )
+	}
 
-	d1 <- sort_data("Drought.csv", 1, "drought_stress")
-	d2 <- sort_data("Low-N.csv", 2, "low_nitrogen")
-	d3 <- sort_data("Opt.csv", 3, "optimum")
+	d1 <- sort_data("Drought.csv", "drought_stress")
+	d2 <- sort_data("Low-N.csv", "low_N")
+	d3 <- sort_data("Opt.csv", "high_N", nrows=1536)
+	d4 <- sort_data("Opt.csv", "high_N", skip=1538)  
 
-	d <- rbind(d1, d2, d3)
+	d <- rbind(d1, d2, d3, d4)
 	
 	d <- d[d$location != "LOC", ]
 	d <- d[d$location != "", ]
@@ -114,19 +101,17 @@ carob_script <- function(path) {
 	
 	#fertilizer regimes (see publication)
 	d$fertilizer_type <- "NPK;urea"
-	d$N_fertilizer <-ifelse(d$treatment=="optimum",120,NA)
-	d$P_fertilizer <-ifelse(d$treatment=="optimum",60,NA)
-	d$K_fertilizer <-ifelse(d$treatment=="optimum",60,NA)
+	d$N_fertilizer <-ifelse(d$treatment=="high_N", 120, 30)
+	d$P_fertilizer <-ifelse(d$treatment=="high_N", 60, 30)
+	d$K_fertilizer <-ifelse(d$treatment=="high_N", 60, 30)
 	
 	#weed management (see publication)
-	d$herbicide_used <- ifelse(d$treatment=="optimum", TRUE, FALSE)
-	d$herbicide_product <- ifelse(d$treatment=="optimum", "primextra;paraquat dichloride", NA)
-	d$herbicide_amount <- ifelse(d$treatment=="optimum", 5, 0)
+	d$herbicide_used <- ifelse(d$treatment=="high_N", TRUE, FALSE)
+	d$herbicide_product <- ifelse(d$treatment=="high_N", "primextra;paraquat dichloride", NA)
+	d$herbicide_amount <- ifelse(d$treatment=="high_N", 5, 0)
 	
-	d$planting_date[d$date==2013] <- as.character("2013")
-	d$planting_date[d$date==2014] <- as.character("2014")
-	d$harvest_date[d$date==2013] <- as.character("2014")
-	d$harvest_date[d$date==2014] <- as.character("2015")
+	d$harvest_date <- as.character(d$planting_date + 1)
+	d$planting_date <- as.character(d$planting_date)
 	 
 	d$crop <- "maize"
 	d$yield_part <- "grain"
