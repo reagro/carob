def0966a6dcd93e5ea21cba6bd13c181e3e9be8e
rhijmans <r.hijmans@gmail.com>
2024-10-31 10:51:20 -0700

    for #586

--- scripts/survey/hdl_11529_11154.R
+++ scripts/survey/hdl_11529_11154.R
@@ -1,15 +1,13 @@
 # R script for "carob"
 # license: GPL (>=3)
 
-## ISSUES
-# list processing issues here so that an editor can look at them
 
 carob_script <- function(path) {
 
 "This data covers the crop aspect of the Agronomy Panel Survey (APS) implemented in 99 selected communities across 17 LGAs in 22 randomly selected 10km x 10 km sampling grids from Kano, Kaduna and Katsina States. The XLS file has tabs for Metadata, Variables, Data and Data history. (2017-12-07)"
 
 	uri <- "hdl:11529/11154"
-	group <- "agronomy"
+	group <- "survey"
 
 	ff  <- carobiner::get_data(uri, path, group)
 
@@ -19,8 +17,8 @@ carob_script <- function(path) {
 		publication = NA,
 		project = "TAMASA",
 		data_type = "survey",
-		treatment_vars = "longitude;latitude",
-		response_vars = "yield", 
+		treatment_vars = "none",
+		response_vars = "none",
 		carob_contributor = "Shumirai Manzvera",
 		carob_date = "2024-10-31",
 		notes = NA, 
@@ -30,7 +28,7 @@ carob_script <- function(path) {
 
 	f <- ff[basename(ff) == "NG_APS_CC_Yield_2016.xlsx"]
 
- r <- carobiner::read.excel(f, sheet = "Data")
+	r <- carobiner::read.excel(f, sheet = "Data")
 
 	d <- data.frame(
 		location = r$ward,
@@ -44,11 +42,13 @@ carob_script <- function(path) {
 		row_spacing=r$`rowdist(cm)`,
 		plant_spacing=r$`plantdis(cm)`,
 		trial_id=r$barcode_ccut,
-		yield_moisture=r$avgmc)
+		yield_moisture=r$avgmc,
+		dm_yield = r$`dry_yield(kg/ha)`
+	)
 	
 	d$country <- "Nigeria"	
 	d$crop <- "maize"
-  d$yield_part <- "grain"
+	d$yield_part <- "grain"
 	d$on_farm <- TRUE
 	d$is_survey <- TRUE
 	d$irrigated <-FALSE
@@ -62,7 +62,7 @@ carob_script <- function(path) {
 	d$adm1<- gsub("KN", "Kaduna", d$adm1)
 	
 	d$N_fertilizer <- d$P_fertilizer  <- d$K_fertilizer <- as.numeric(NA)
-	d$plant_spacing[d$plant_spacing %in% c(116, 230)] <- NA
+	#d$plant_spacing[d$plant_spacing %in% c(116, 230)] <- NA
 
 	carobiner::write_files(path, meta, d)
 }
