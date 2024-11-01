09e2c0901cb246f67c2520ffb5a8e099c2aa217b
Fredy Chimire <fredaschimex@gmail.com>
2024-11-01 15:24:40 +0200

    Standardised dataset hdl_11529_10548433.R

---
+++ scripts/varieties_wheat/hdl_11529_10548433.R
@@ -0,0 +1,27 @@
+# R script for "carob"
+
+
+carob_script <- function(path) {
+  
+  "The Harvest Plus Advanced Nursery (HPAN) contains spring bread wheat (Triticum aestivum) germplasm adapted to ME1 (Optimally Irrigated, low rainfall environment) and ME5 (Warmer area environment) environments. It has 100-200 entries with a single replication, white grain color and distributed to more than 35 locations. (2014)"
+  uri <- "hdl:11529/10548433"
+  group <- "varieties_wheat"
+  ff <- carobiner::get_data(uri, path, group)
+  
+  meta <- data.frame(
+    carobiner::read_metadata(uri, path, group, major=1, minor=0),
+    data_institute = "CIMMYT",
+    publication= NA,
+    project="Harvest Plus Advanced Nursery",
+    data_type= "experiment",
+    response_vars = "yield",
+    treatment_vars = "variety_code",
+    carob_contributor= "Fredy Chimire",
+    carob_date="2024-11-01"
+  )
+  
+  proc_wheat <- carobiner::get_function("proc_wheat", path, group)
+  d <- proc_wheat(ff)
+  
+  carobiner::write_files(path, meta, d)
+}
