fba18e77567592ac680337f58dd6bdede2b8a2a7
rhijmans <r.hijmans@gmail.com>
2024-12-04 15:41:15 -0800

    for #608

--- scripts/varieties_potato/doi_10.21223_P3_FFORMJ.R
+++ scripts/varieties_potato/doi_10.21223_P3_FFORMJ.R
@@ -1,9 +1,5 @@
 # R script for "carob"
 
-## ISSUES
-# ....
-
-
 carob_script <- function(path) {
   
 "The B3C3 population came from crossing the elite clones of B3C2 population, in 2011 under quarantine greenhouses at La Molina. For this, 30,000 genotypes (60 families with 500 seeds each) were planted. At harvest 21685 genotypes, were selected (72%). These selected clones were planted in La Victoria - Huancayo, to increase the number of tubers and make selection for agronomic attributes, where 3123 clones (10.41% of the original population) were selected. This dataset is part of the following selection where selected clones were evaluated for three seasons (2012, 2013 and 2014), in Oxapampa where the environmental conditions (rain, relative humidity, temperature) are great to have a high pressure of late blight allowing us to select clones with resistance to this disease. In 2014-2015 at Oxapampa, the third evaluation of resistance to late blight was conducted. 370 clones were planted in plots of 10 plants with two replications. Furthermore, variety Kory, semiresistant Amarilis and Yungay, a susceptible variety, were used as controls to the resistant clones. The selected clones showed lower values in the scale of suceptibility to late blight, than the susceptible witness Yungay. 94 clones with marketable and total tuber yield above controls and with scale values of susceptibility to late blight lower than Control yungay were selected, but all clones were tested again for resistance to late blight, because that year the presence of late blight was very low."
@@ -31,10 +27,8 @@ carob_script <- function(path) {
   d <- lapply(f, process, addvars=c("AUDPC","rAUDPC","TTWP","MTWP"))
   d <- do.call(rbind, d)
   
-  d$planting_date <- as.Date(as.numeric(d$planting_date), origin="1899-12-30") |> 
-      as.character()
-  d$harvest_date <- as.Date(as.numeric(d$harvest_date), origin="1899-12-30") |> 
-      as.character()
+  d$planting_date <- as.Date(as.numeric(d$planting_date), origin="1899-12-30") |> as.character()
+  d$harvest_date <- as.Date(as.numeric(d$harvest_date), origin="1899-12-30") |> as.character()
   
   d$on_farm <- TRUE
   d$is_survey <- FALSE
@@ -45,12 +39,6 @@ carob_script <- function(path) {
   d$geo_from_source <- TRUE
   d$N_fertilizer <- d$P_fertilizer <- d$K_fertilizer <- as.numeric(NA)
   
-  carobiner::write_files(path = path,
-                         metadata = meta,
-                         records = d)
-  
+  carobiner::write_files(path = path, metadata = meta, records = d)
 }
 
-## now test your function in a _clean_ R environment (no packages loaded, no other objects available)
-# path <- _____
-# carob_script(path)
--- scripts/varieties_potato/doi_10.21223_P3_LCGD4P.R
+++ scripts/varieties_potato/doi_10.21223_P3_LCGD4P.R
@@ -1,12 +1,8 @@
 # R script for "carob"
 
-## ISSUES
-# ....
-
-
 carob_script <- function(path) {
   
-    "The B3C3 population came from crossing the elite clones of B3C2 population, in 2011 under quarantine greenhouses at La Molina. For this, 30,000 genotypes (60 families with 500 seeds each) were planted. At harvest 21685 genotypes, were selected (72%). These selected clones were planted in La Victoria, Huancayo, to increase the number of tubers and make selection for agronomic attributes. Here, 3123 clones (10.41% of the original population) were selected. \r\nThis dataset is part of the following selection where the selected clones were evaluated for three seasons (2012, 2013 and 2014), in Oxapampa where the environmental conditions (rain, relative humidity, temperature) are great to have a high pressure of late blight allowing us to select clones with resistance to this disease. In 2013-2014 at Oxapampa, the second evaluation of resistance to late blight was conducted. 507 clones were planted in plots of 10 plants with two replications. Furthermore, Variety Kory, semiresistant Amarilis and Yungay, a susceptible variety, were used as controls to the resistant clones. In this experiment, 370 clones were selected, they showed values lower than controls, according to the scale of susceptibility to late blight."
+"The B3C3 population came from crossing the elite clones of B3C2 population, in 2011 under quarantine greenhouses at La Molina. For this, 30,000 genotypes (60 families with 500 seeds each) were planted. At harvest 21685 genotypes, were selected (72%). These selected clones were planted in La Victoria, Huancayo, to increase the number of tubers and make selection for agronomic attributes. Here, 3123 clones (10.41% of the original population) were selected. \r\nThis dataset is part of the following selection where the selected clones were evaluated for three seasons (2012, 2013 and 2014), in Oxapampa where the environmental conditions (rain, relative humidity, temperature) are great to have a high pressure of late blight allowing us to select clones with resistance to this disease. In 2013-2014 at Oxapampa, the second evaluation of resistance to late blight was conducted. 507 clones were planted in plots of 10 plants with two replications. Furthermore, Variety Kory, semiresistant Amarilis and Yungay, a susceptible variety, were used as controls to the resistant clones. In this experiment, 370 clones were selected, they showed values lower than controls, according to the scale of susceptibility to late blight."
   
   uri <- "doi:10.21223/P3/LCGD4P"
   group <- "varieties_potato"
@@ -31,10 +27,8 @@ carob_script <- function(path) {
   d <- lapply(f, process, addvars=c("AUDPC","rAUDPC","TTWP","MTWP"))
   d <- do.call(rbind, d)
   
-  d$planting_date <- as.Date(as.numeric(d$planting_date),origin="1899-12-30") |> 
-      as.character()
-  d$harvest_date <- as.Date(as.numeric(d$harvest_date),origin="1899-12-30") |> 
-      as.character()
+  d$planting_date <- as.Date(as.numeric(d$planting_date), origin="1899-12-30") |> as.character()
+  d$harvest_date <- as.Date(as.numeric(d$harvest_date), origin="1899-12-30") |> as.character()
   
   d$on_farm <- TRUE
   d$is_survey <- FALSE
@@ -45,13 +39,5 @@ carob_script <- function(path) {
   d$geo_from_source <- TRUE
   d$N_fertilizer <- d$P_fertilizer <- d$K_fertilizer <- as.numeric(NA)
   
-  carobiner::write_files(path = path,
-                         metadata = meta,
-                         records = d)
-  
-  
+  carobiner::write_files(path = path, metadata = meta, records = d)
 }
-
-## now test your function in a _clean_ R environment (no packages loaded, no other objects available)
-# path <- _____
-# carob_script(path)
