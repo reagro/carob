cae79c9d18c8aa68dc6e111d1acea91915908ea1
rhijmans <r.hijmans@gmail.com>
2025-05-14 17:14:25 -0700

    m

---
+++ scripts/lsms/doi_10.48529_bp16-s524.R
@@ -0,0 +1,24 @@
+# R script for "carob"
+
+
+carob_script <- function(path) {
+
+"Niger: National Survey on Household Living Conditions and Agriculture 2011"
+
+	uri <- "doi:10.48529/bp16-s524"
+	group <- "LSMS"
+	ff  <- carobiner::get_data(uri, path, group)
+
+	meta <- data.frame(
+		carobiner::read_metadata(uri, path, group, major=1, minor=0),
+		publication = NA,
+		carob_contributor = "Robert Hijmans",
+		carob_date = "2025-05-14"
+	)
+
+	return(TRUE)
+	carobiner::write_files(path, meta, d)
+}
+
+
+
