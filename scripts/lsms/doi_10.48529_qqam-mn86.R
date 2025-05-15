8fafc95d3d6577d2af2bb5f14037709b61608167
rhijmans <r.hijmans@gmail.com>
2025-05-14 17:21:45 -0700

    m

---
+++ scripts/lsms/doi_10.48529_qqam-mn86.R
@@ -0,0 +1,24 @@
+# R script for "carob"
+
+
+carob_script <- function(path) {
+
+"Mali: Enquête Agricole de Conjoncture Intégrée 2014"
+
+	uri <- "doi:10.48529/qqam-mn86"
+	group <- "LSMS"
+	ff  <- carobiner::get_data(uri, path, group)
+
+	meta <- data.frame(
+		carobiner::read_metadata(uri, path, group, major=1, minor=0),
+		publication = NA,
+		carob_contributor = "Robert Hijmans",
+		carob_date = "2025-05-14"
+	)
+
+	return(TRUE)
+	carobiner::write_files(path, meta, d)
+}
+
+
+
