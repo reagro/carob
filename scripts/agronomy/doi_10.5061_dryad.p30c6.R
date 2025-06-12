2f5dd0553fa00cd0d8eee890984ddfb7bd957ad3
cedric Ngakou <cedric.ngakou@aims-cameroon.org>
2025-06-12 11:11:33 +0100

    meta

--- scripts/agronomy/doi_10.5061_dryad.p30c6.R
+++ scripts/agronomy/doi_10.5061_dryad.p30c6.R
@@ -6,7 +6,7 @@ carob_script <- function(path) {
    group <- "agronomy"
    ff <- carobiner::get_data(uri, path, group)
    
-   meta <- carobiner::get_metadata(uri, path, group, major=2, minor=0,
+   meta <- carobiner::get_metadata(uri, path, group, major=2, minor=NA,
                                    data_organization = "UNL", 
                                    publication="doi:10.2134/agronj2008.0103x", 
                                    project=NA, 
